b356aa8df0ca6b4f58b3108b1f99f80fc96e11c0
Wouter Saelens <wouter.saelens@gmail.com>
2017-09-14 16:53:26 +0200

    grouped rules, added rules, added perturbations, added comparison with grouped cells

--- analysis/analyses/score_rules/rules.R
+++ analysis/analyses/score_rules/rules.R
@@ -22,7 +22,7 @@ check_direct_gs_comparison <- function(scores_summary) {
   scores_summary %>%
     filter(!(perturbator_id %in% gs_perturbator_ids)) %>%
     group_by(score_id) %>%
-    summarise(rule = all(diff < 0))
+    summarise(rule = all(diff < 1e-10))
 }
 
 plot_direct_gs_comparison <- function(scores_summary) {
@@ -35,13 +35,14 @@ plot_direct_gs_comparison <- function(scores_summary) {
 
 # indirect_gs_comparison -----------------------------------
 check_indirect_gs_comparison <- function(scores_summary) {
-  scores2_summary %>%
+  scores_summary %>%
     mutate(is_gs = (perturbator_id %in% gs_perturbator_ids)) %>%
     group_by(score_id) %>%
     summarise(
       maxdiff = max(score[!is_gs]) - min(score[is_gs]),
       rule = maxdiff <= 1e-10
-    )
+    ) %>%
+    select(-maxdiff)
 }
 
 plot_indirect_gs_comparison <- function(scores_summary) {
@@ -74,8 +75,16 @@ check_join_linear <- function(scores_summary) {
     summarise(rule = all(diff < 0))
 }
 
-# shuffle_two_vs_shuffle_all --------------------
-check_shuffle_two_vs_shuffle_all <- function(scores_summary) {
+# switch all cells ------------------------------
+check_switch_cells <- function(scores_summary) {
+  scores_summary %>%
+    filter(perturbator_id == "switch_all_cells") %>%
+    group_by(score_id) %>%
+    summarise(rule = all(diff < 0))
+}
+
+# switch_two_vs_switch_all --------------------
+check_switch_two_vs_switch_all <- function(scores_summary) {
   scores_summary_largevssmall <- scores_summary %>%
     group_by(generator_id) %>%
     filter(perturbator_id %in% c("switch_two_cells", "switch_all_cells")) %>%
@@ -88,17 +97,26 @@ check_shuffle_two_vs_shuffle_all <- function(scores_summary) {
     summarise(
       maxdiff = max(score[!is_small]) - min(score[is_small]),
       rule = all(maxdiff < 0)
-    )
+    ) %>%
+    select(-maxdiff)
 }
 
-plot_shuffle_two_vs_shuffle_all <- function(scores_summary) {
+plot_switch_two_vs_switch_all <- function(scores_summary) {
   scores_summary_largevssmall %>% ggplot() +
     geom_boxplot(aes(toy_category, score, color=perturbator_id)) +
     facet_wrap(~score_id)
 }
 
-# hairy --------------------------
+# hairy -------------------------------
 check_hairy <- function(scores_summary) {
+  scores_summary %>%
+    filter(perturbator_id == "hairy") %>%
+    group_by(score_id) %>%
+    summarise(rule = all(diff < 0))
+}
+
+# hairy_small_vs_hairy_large --------------------------
+check_hairy_small_vs_hairy_large <- function(scores_summary) {
   scores_summary_largevssmall <- scores_summary %>%
     group_by(generator_id) %>%
     filter(perturbator_id %in% c("hairy_small", "hairy_large")) %>%
@@ -111,7 +129,7 @@ check_hairy <- function(scores_summary) {
     summarise(
       maxdiff = max(score[!is_small]) - min(score[is_small]),
       rule = all(maxdiff < 0)
-    )
+    ) %>% select(-maxdiff)
 }
 
 plot_hairy <- function(scores_summary) {
@@ -145,12 +163,13 @@ plot_warping <- function(scores_summary) {
 
 
 # hairy_large_effect --------------------------------
+# Changes in the milestone network should affect the scores, even if they do not greatly impact the ordering of the cells
 check_hairy_large_effect <- function(scores_summary) {
   scores_summary %>%
     group_by(score_id) %>%
-    mutate(maxscore = max(score)) %>%
-    filter(perturbator_id == "hairy") %>%
-    summarise(rule = all(score < maxscore/2))
+    mutate(maxscore = max(score, na.rm=TRUE), minscore = min(score, na.rm=TRUE)) %>%
+    filter(perturbator_id == "hairy_large") %>%
+    summarise(rule = all(score < quantile(c(maxscore, minscore), 0.5)))  ## how to define a "high" change??
 }
 
 
@@ -170,7 +189,7 @@ check_linear <- function(scores_summary) {
     mutate(perc = as.numeric(gsub("switch_(\\d*)", "\\1", perturbator_id))) %>%
     mutate(score = score + runif(n(), 0, 1e-10)) %>%
     group_by(score_id) %>%
-    summarise(cor=cor(perc, score), rule = cor < -0.9)
+    summarise(cor=cor(perc, score), rule = cor < -0.9) %>% select(-cor)
 }
 
 
@@ -185,7 +204,7 @@ check_remove_cells <- function(scores_summary) {
 
 
 
-# different_structure
+# different_structure ---------------------------
 check_different_structure <- function(scores_summary) {
   scores_summary %>%
     filter(perturbator_id == "add_distant_edge") %>%
@@ -194,8 +213,38 @@ check_different_structure <- function(scores_summary) {
 }
 
 
-toy <- toys %>% filter(perturbator_id=="hairy") %>% .$toy %>% first
-milestone_ids <- toy$milestone_percentages %>% group_by(cell_id) %>% summarise(list(milestone_id)) %>% pull(2) %>% keep(~length(.) > 1)
 
-map(milestone_ids, ~any((toy$milestone_network$from == .[[1]]) & (toy$milestone_network$to == .[[2]])))
-toy$milestone_network
+# change lengths -------------------------
+check_different_lengths <- function(scores_summary) {
+  scores_summary %>%
+    filter(perturbator_id == "change_lengths") %>%
+    group_by(score_id) %>%
+    summarise(rule=all(diff < -1e-5))
+}
+
+# change terminal lengths -------------------------
+check_different_terminal_lengths <- function(scores_summary) {
+  scores_summary %>%
+    filter(perturbator_id == "change_terminal_lengths") %>%
+    group_by(score_id) %>%
+    summarise(rule=all(diff < -1e-5))
+}
+
+# both position and structure
+check_structure_and_position <- function(scores_summary) {
+  scores_summary_both <- scores_summary %>%
+    filter(perturbator_id %in% c("structure_and_position", "add_distant_edge", "switch_all_cells")) %>%
+    group_by(generator_id) %>%
+    filter(length(unique(perturbator_id)) == 3) %>%
+    ungroup() %>%
+    mutate(is_both = (perturbator_id == "structure_and_position"))
+
+  scores_summary_both %>%
+    group_by(score_id) %>%
+    summarise(
+      meandiff = mean(diff),
+      maxdiff = max(score[is_both]) - min(score[!is_both]),
+      rule = all(maxdiff <= meandiff/10)
+    ) %>%
+    select(-meandiff, -maxdiff)
+}
