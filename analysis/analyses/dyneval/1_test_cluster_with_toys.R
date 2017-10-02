be32dc535fff50c88971fd59d15b7502d047fd0c
Robrecht Cannoodt <brechtus@gmail.com>
2017-10-02 18:27:25 +0200

    working on toy dataset script

--- analysis/analyses/dyneval/1_test_cluster_with_toys.R
+++ analysis/analyses/dyneval/1_test_cluster_with_toys.R
@@ -1,8 +1,13 @@
 library(dyneval)
 library(dyntoy)
+library(dynutils)
 library(tidyverse)
 
 derived_dir <- "analysis/data/derived_data/dyneval/1_test_cluster_with_toys/"
+
+# # remove previous output
+# unlink(derived_dir, recursive=TRUE)
+
 dir.create(derived_dir, recursive = T)
 
 # easy test
@@ -25,4 +30,10 @@ benchmark_suite_submit(
   num_init_params = 16
 )
 
-benchmark_suite_retrieve(derived_dir)
+outputs <- benchmark_suite_retrieve(derived_dir)
+
+succeeded <- outputs %>% list_as_tibble() %>% filter(!sapply(which_errored, any))
+
+all_outputs <- succeeded$outputs %>% map_df(function(x) x %>% map_df(~ .$eval_grp))
+all_outputs %>% select(ti_type, grid_i, param_i, fold_type, method_name, correlation, robbie_network_score)
+ggplot(all_outputs) + geom_point(aes())
