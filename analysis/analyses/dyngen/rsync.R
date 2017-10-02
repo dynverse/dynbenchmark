8550291a68b85e27495c9a4a86b10f13d4ba7d9f
Robrecht Cannoodt <brechtus@gmail.com>
2017-10-02 12:35:49 +0200

    add rsync script for dyngen

---
+++ analysis/analyses/dyngen/rsync.R
@@ -0,0 +1,3 @@
+
+#' update the local dyngen datasets from what is stored on the prism
+PRISM:::rsync_remote("prism", "/group/irc/shared/dyngen_results/4", "", "/home/rcannood/Workspace/dynverse/dynalysis/analysis/data/derived_data/dyngen/")
