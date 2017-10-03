da1c085c43fe9d690fae5db51587e67638fbaa15
Wouter Saelens <wouter.saelens@gmail.com>
2017-10-03 16:54:58 +0200

    update data loading on prism

--- R/dataset_loading.R
+++ R/dataset_loading.R
@@ -1,7 +1,7 @@
 #' @export
 load_dataset <- function(dataset_id, prefix="") {
   if(file.exists("/group/irc/shared")) {
-    read_rds(paste0("analysis/data/datasets/", prefix, "/", dataset_id, ".rds"))
+    read_rds(paste0("/group/irc/shared/dynalysis/analysis/data/datasets/", prefix, "/", dataset_id, ".rds"))
   } else {
     read_rds(paste0("analysis/data/datasets/", prefix, "/", dataset_id, ".rds"))
   }
