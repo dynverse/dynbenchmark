1ad052948eb1e67a43efd2cdb40141e6ddf0d9b5
Wouter Saelens <wouter.saelens@gmail.com>
2017-10-03 16:19:29 +0200

    added context sensitive data loading

---
+++ R/dataset_loading.R
@@ -0,0 +1,8 @@
+#' @export
+load_dataset <- function(dataset_id, prefix="") {
+  if(file.exists("/group/irc/shared")) {
+    read_rds(paste0("analysis/data/datasets/", prefix, "/", dataset_id, ".rds"))
+  } else {
+    read_rds(paste0("analysis/data/datasets/", prefix, "/", dataset_id, ".rds"))
+  }
+}
