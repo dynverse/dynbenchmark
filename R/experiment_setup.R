3b95a694e92ea3631ba3fdc157584ac61d0ca6e0
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-10-21 01:32:06 +0200

    add experiment setup helper

---
+++ R/experiment_setup.R
@@ -0,0 +1,62 @@
+#' Helper function for controlling experiments
+#'
+#' @param dirname Foldername for the experiment
+#' @param description TODO
+#' @param code_block A code block to execute
+#' @param auto_create_folders Automatically create folders
+#'  when one of the file functions is called.
+#'
+#' @importFrom glue glue
+#' @importFrom lazyeval lazy_eval
+#' @importFrom testthat expect_match
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' experiment(
+#'   dirname = "test_plots",
+#'   description = "this parameter does not do anything yet",
+#'   auto_create_folders = TRUE,
+#'   {
+#'     data <- matrix(runif(200), ncol = 2)
+#'     pdf(figure_file("testplot.pdf"), 5, 5)
+#'     plot(data)
+#'     dev.off()
+#'   }
+#' )
+#' }
+experiment <- function(dirname, description, code_block, auto_create_folders = TRUE) {
+  # substitute code block to stop from executing
+  code_block <- substitute(code_block)
+
+  # get environment
+  env <- environment(code_block)
+
+  # check whether the working directory is indeed the dynalysis folder
+  testthat::expect_match(getwd(), "/dynalysis/?$")
+
+  # create a few directories in which to store possible output
+  scratch_dir <- glue::glue("analysis/data/derived_data/{dirname}/")
+  figure_dir <- glue::glue("analysis/figures/{dirname}/")
+  result_dir <- glue::glue("analysis/results/{dirname}/")
+
+  # create a helper function
+  auto_create_fun <- function(path) {
+    function(...) {
+      if (!file.exists(path)) {
+        dir.create(path, recursive = TRUE)
+      }
+      paste(path, ..., collapse = "")
+    }
+  }
+  if (auto_create_folders) dir.create(c(scratch_dir, figure_dir, result_dir))
+
+  # create and expose functions
+  env$scratch_file <- auto_create_fun(scratch_dir)
+  env$figure_file <- auto_create_fun(figure_dir)
+  env$result_file <- auto_create_fun(result_dir)
+
+  # execute code block
+  eval(code_block, envir = env)
+}
+
