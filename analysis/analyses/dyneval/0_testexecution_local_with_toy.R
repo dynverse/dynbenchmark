73363fbdd887b410f213504a5cde1136d2d8f7b4
Robrecht Cannoodt <brechtus@gmail.com>
2017-10-23 12:06:35 +0200

    update figures with quicker setup times

--- analysis/analyses/dyneval/0_testexecution_local_with_toy.R
+++ analysis/analyses/dyneval/0_testexecution_local_with_toy.R
@@ -3,14 +3,11 @@ library(tidyverse)
 library(dynplot)
 library(glue)
 
-# get paths
-code <- "dyneval/0_testexecution_withtoy"
-deriveddata_dir <- glue("analysis/data/derived_data/{code}")
-figures_dir <- glue("analysis/figures/{code}")
-results_dir <- glue("analysis/results/{code}")
-dir.create(deriveddata_dir, recursive = T)
-dir.create(figures_dir, recursive = T)
-dir.create(results_dir, recursive = T)
+experiment(
+  dirname = "dyneval/0_testexecution_withtoy",
+  description = "Testing whether each method is able to run locally",
+  auto_create_folders = TRUE
+)
 
 # trying all methods
 methods <- dyneval::get_descriptions(F)
@@ -19,27 +16,24 @@ methods <- dyneval::get_descriptions(F)
 tasks <- dyntoy::toy_tasks[5,]
 task <- extract_row_to_list(tasks, 1)
 
-# # synthetic dataset
-# tasks <- readRDS(paste0("../dynalysis/analysis/data/derived_data/dyngen/tasks_v4.rds")) %>%
-#   filter(
-#     platform_id == "fluidigm_c1",
-#     takesetting_type == "snapshot")
-#
-# tasks <- tasks[5,]
+metrics <- c("auc_R_nx", "correlation")
+parameters <- list()
+timeout <- 300
 
+# run each method
 outs <- pbapply::pblapply(methods, function(method) {
   tryCatch({
-    score <- execute_evaluation(tasks, method, parameters = list(), metrics = "auc_R_nx", timeout = 300)
+    score <- execute_evaluation(tasks, method, parameters = parameters, metrics = metrics, timeout = timeout)
     summary <- attr(score,"extras")$.summary
     prediction <- attr(score,"extras")$.models[[1]]
     attr(score,"extras") <- NULL
     meth_plot <- method$plot_fun(prediction)
     default_plot <- plot_default(prediction)
-    strip_plot <- plot_strip(task, prediction)
+    strip_plot <- plot_strip_connections(prediction, task)
     lst(method, score, summary, prediction, meth_plot, default_plot, strip_plot)
   }, error = function(e) NULL)
 })
-save(outs, file = glue("{results_dir}/outs.rds"))
+save(outs, file = result_file("outs.rds"))
 
 check_df <- seq_along(outs) %>% map_df(function(i) {
   method <- methods[[i]]
@@ -54,9 +48,9 @@ check_df <- seq_along(outs) %>% map_df(function(i) {
 check_df %>% as.data.frame
 
 # rerun something
-# i <- 16
+# i <- 24
 # method <- methods[[i]]
-# score <- execute_evaluation(tasks, method, parameters = list(), metrics = "auc_R_nx", timeout = 300)
+# score <- execute_evaluation(tasks, method, parameters = parameters, metrics = metrics, timeout = timeout)
 # summary <- attr(score,"extras")$.summary
 # prediction <- attr(score,"extras")$.models[[1]]
 # attr(score,"extras") <- NULL
@@ -65,18 +59,18 @@ check_df %>% as.data.frame
 # outs[[i]] <- lst(method, score, summary, prediction, meth_plot, default_plot)
 # rerun stop
 
-plotlist <- outs[check_df$ggplot] %>% map(~ .$meth_plot)
+plotlist <- outs[check_df$produced_ggplot] %>% map(~ .$meth_plot)
 num_plots <- length(plotlist)
 ncol <- ceiling(sqrt(num_plots))
 nrow <- ceiling(num_plots / ncol)
 
-pdf(glue("{figures_dir}/0_theirplots.pdf"), ncol*8, nrow*8.4)
+pdf(figure_file("0_theirplots.pdf"), ncol*8, nrow*8.4)
 cowplot::plot_grid(plotlist = plotlist, ncol = ncol, align = "hv")
 dev.off()
 
-pdf(glue("{figures_dir}/1_multipleplots.pdf"), 24, 8.4)
+pdf(figure_file("1_multipleplots.pdf"), 24, 8.4)
 for (i in seq_along(outs)) {
-  if (check_df$ggplot[[i]]) {
+  if (check_df$produced_ggplot[[i]]) {
     ou <- outs[[i]]
     ou_name <- ou$method$name
     g <- cowplot::plot_grid(
@@ -100,7 +94,7 @@ timings <- summary %>%
   mutate(method_name_f = factor(method_name, levels = method_name)) %>%
   gather(part, time, -method_name, -method_name_f)
 
-pdf(glue("{figures_dir}/2_timings.pdf"), 24, 16)
+pdf(figure_file("2_timings.pdf"), 24, 16)
 ggplot(timings) +
   geom_bar(aes(method_name_f, time, fill = part), stat = "identity") +
   cowplot::theme_cowplot() +
@@ -112,12 +106,12 @@ dev.off()
 
 # scores
 scores <- summary %>%
-  select(method_name, num_files_created, num_setseed_calls, mean_R_nx, auc_R_nx, Q_global, Q_local) %>%
+  select(method_name, correlation, mean_R_nx, auc_R_nx, Q_global, Q_local, num_files_created, num_setseed_calls) %>%
   arrange(auc_R_nx) %>%
   mutate(method_name_f = factor(method_name, levels = method_name)) %>%
   gather(metric, score, -method_name, -method_name_f)
 
-pdf(glue("{figures_dir}/3_scores.pdf"), 24, 16)
+pdf(figure_file("3_scores.pdf"), 24, 16)
 ggplot(scores) +
   geom_bar(aes(method_name_f, score, fill = metric), stat = "identity") +
   cowplot::theme_cowplot() +
