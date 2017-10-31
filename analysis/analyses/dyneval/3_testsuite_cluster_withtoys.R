78d60916d9f5d0c9fb983a415f12f783fbe4ad3d
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-10-31 09:34:01 +0100

    initial results on toys

--- analysis/analyses/dyneval/3_testsuite_cluster_withtoys.R
+++ analysis/analyses/dyneval/3_testsuite_cluster_withtoys.R
@@ -56,14 +56,14 @@ time_df <- bind_rows(succeeded$eval_ind) %>% select(method_name, starts_with("ti
   gather(part, time, -method_name) %>% mutate(part = gsub("time_", "", part))
 
 method_name_u <- time_df$method_name %>% unique %>% sort(decreasing = T)
-# pdf(paste0(derived_dir, "/results_timings.pdf"), 10, 10)
+pdf(figure_file("/results_timings.pdf"), 10, 10)
 ggplot(time_df %>% mutate(method_name = factor(method_name, levels = method_name_u))) +
   geom_bar(aes(method_name, time, fill = method_name), stat = "identity") +
   facet_wrap(~part, scales = "free") +
   coord_flip() +
   theme(legend.position = "none") +
   labs(title = "Timings on toy datasets")
-# dev.off()
+dev.off()
 
 
 # summary %>%
@@ -72,19 +72,19 @@ ggplot(time_df %>% mutate(method_name = factor(method_name, levels = method_name
 #   mutate(method_name_f = factor(method_name, levels = method_name)) %>%
 #   gather(metric, score, -method_name, -method_name_f)
 
-eval_grp <- bind_rows(succeeded$eval_grp) #%>% mutate(harmonicmean = 2 * correlation * robbie_network_score / (correlation + robbie_network_score))
+eval_grp <- bind_rows(succeeded$eval_ind) #%>% mutate(harmonicmean = 2 * correlation * robbie_network_score / (correlation + robbie_network_score))
 param_sels <- eval_grp %>%
   filter(fold_type == "train") %>%
-  group_by(ti_type, task_group, method_name, fold_i) %>%
+  group_by(method_name, fold_i) %>%
   arrange(desc(auc_R_nx)) %>%
   slice(1) %>%
   ungroup() %>%
-  select(ti_type, task_group, method_name, fold_i, param_i)
+  select(method_name, fold_i, param_i)
 train_eval_grp <- eval_grp %>% filter(fold_type == "train") %>% inner_join(param_sels, by = colnames(param_sels))
 test_eval_grp <- eval_grp %>% filter(fold_type == "test") %>% inner_join(param_sels, by = colnames(param_sels))
 
-train_eval_grp_summ <- train_eval_grp %>% group_by(ti_type, task_group, method_name) %>% summarise_if(is.numeric, mean) %>% ungroup
-test_eval_grp_summ <- test_eval_grp %>% group_by(ti_type, task_group, method_name) %>% summarise_if(is.numeric, mean) %>% ungroup
+train_eval_grp_summ <- train_eval_grp %>% group_by(method_name) %>% summarise_if(is.numeric, mean) %>% ungroup
+test_eval_grp_summ <- test_eval_grp %>% group_by(method_name) %>% summarise_if(is.numeric, mean) %>% ungroup
 
 ordered_names <- test_eval_grp_summ %>% group_by(method_name) %>% summarise_if(is.numeric, mean) %>% ungroup() %>% arrange(auc_R_nx) %>% .$method_name
 train_eval_grp_summ <- train_eval_grp_summ %>% mutate(method_name_f = factor(method_name, levels = ordered_names))
@@ -93,7 +93,6 @@ test_eval_grp_summ <- test_eval_grp_summ %>% mutate(method_name_f = factor(metho
 g <- cowplot::plot_grid(
   ggplot(test_eval_grp_summ) +
     geom_bar(aes(method_name_f, auc_R_nx, fill = method_name_f), stat = "identity") +
-    facet_wrap(~ti_type, ncol = 1) +
     coord_flip() +
     theme(legend.position = "none"),
   # ggplot(test_eval_grp_summ) +
@@ -114,6 +113,6 @@ g <- cowplot::plot_grid(
   nrow = 1
 )
 
-# pdf(paste0(derived_dir, "/results_metrics_test.pdf"), 15, 15)
+pdf(figure_file("/results_metrics_test.pdf"), 15, 15)
 g
-# dev.off()
+dev.off()
