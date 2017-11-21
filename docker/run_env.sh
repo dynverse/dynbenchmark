f849761ee8cfd444246fcdbe014912227050dfe9
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-11-21 10:24:31 +0100

    don't write to log

--- docker/run_env.sh
+++ docker/run_env.sh
@@ -1,1 +1,1 @@
-docker build -t dynalysis_env . --file docker/Dockerfile_env --build-arg GITHUB_PAT=$GITHUB_PAT > log
+docker build -t dynalysis_env . --file docker/Dockerfile_env --build-arg GITHUB_PAT=$GITHUB_PAT
