2806f2f5cc144d8fe380c057f1479738b408c179
Wouter Saelens <wouter.saelens@gmail.com>
2017-11-23 14:20:37 +0100

    allow rendering

--- scripts/update_paper_from_google_drive.R
+++ scripts/update_paper_from_google_drive.R
@@ -1,6 +1,9 @@
 library(googledrive)
 library(tidyverse)
+library(rmarkdown)
 
 drive <- drive_download(as_id("1BCCaP21N2PXfzhj9H09yEpZz9lLY2HXd_LxTSsJ_wro"), type="text/plain", overwrite=TRUE, path = "local.txt")
 system(glue::glue("sed -i '1s/^.//' {drive$local_path}")) # remove first character, because this is some strang eunicode character
 system(glue::glue("cat analysis/paper/paper_base.Rmd {drive$local_path} > analysis/paper/paper.Rmd"))
+
+render("analysis/paper/paper.Rmd", "html_document")
