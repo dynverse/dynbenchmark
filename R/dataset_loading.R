65dc5083a6dc0cc3348206f752ca9632fb4de913
Wouter Saelens <wouter.saelens@gmail.com>
2017-11-23 14:19:38 +0100

    added dataset_location

--- R/dataset_loading.R
+++ R/dataset_loading.R
@@ -1,8 +1,14 @@
 #' @export
-load_dataset <- function(dataset_id, prefix="") {
+dataset_location <- function(dataset_id, prefix="") {
   if(file.exists("/group/irc/shared")) {
-    read_rds(paste0("/group/irc/shared/dynalysis/analysis/data/datasets/", prefix, "/", dataset_id, ".rds"))
+    paste0("/group/irc/shared/dynalysis/analysis/data/datasets/", prefix, "/", dataset_id, ".rds")
   } else {
-    read_rds(paste0("analysis/data/datasets/", prefix, "/", dataset_id, ".rds"))
+    paste0("analysis/data/datasets/", prefix, "/", dataset_id, ".rds")
   }
+}
+
+
+#' @export
+load_dataset <- function(dataset_id, prefix="") {
+  read_rds(dataset_location(dataset_id, prefix))
 }
