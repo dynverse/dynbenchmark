958b4cfebeec5af6091408a37a149877dbcf34ca
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-11-23 15:40:04 +0100

    move function

--- R/dataset_loading.R
+++ R/dataset_loading.R
@@ -6,3 +6,8 @@ load_dataset <- function(dataset_id, prefix="") {
     read_rds(paste0("analysis/data/datasets/", prefix, "/", dataset_id, ".rds"))
   }
 }
+
+#' @export
+save_dataset <- function(dataset) {
+  write_rds(dataset, paste0("analysis/data/datasets/real/", dataset$info$id, ".rds"))
+}
