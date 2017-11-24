47b086bdc3f8d2df50090de74503e02d53e95288
Robrecht Cannoodt <brechtus@gmail.com>
2017-11-24 11:16:34 +0100

    update epidermis hair joost

--- analysis/analyses/1-datasets/real/epidermis_hair_joost.R
+++ analysis/analyses/1-datasets/real/epidermis_hair_joost.R
@@ -1,52 +1,62 @@
 rm(list=ls())
 library(tidyverse)
+library(dynalysis)
 options('download.file.method.GEOquery'='curl')
 
+id <- "epidermis_hair_spatial_joost"
+dataset_preprocessing("real", id)
+
 txt_web_location <- "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE67602&format=file&file=GSE67602%5FJoost%5Fet%5Fal%5Fexpression%2Etxt%2Egz"
-txt_location <- "analysis/data/datasets_preproc/GSE67602_Joost_et_al_expression.txt.gz"
+txt_location <- dataset_preproc_file("GSE67602_Joost_et_al_expression.txt.gz")
 
 if (!file.exists(txt_location)) {
-  download.file(paste0(txt_web_location), txt_location, method="libcurl") # libcurl muuuuuuuuuch faster, usually
+  download.file(txt_web_location, txt_location, method = "libcurl")
 }
 
 counts_all <- read_tsv(txt_location) %>% filter(!startsWith(`Gene\\Cell`, "ERCC")) %>% tibble::column_to_rownames("Gene\\Cell") %>% as.matrix() %>% t
 
-geo <- GEOquery::getGEO("GSE67602", destdir="analysis/data/datasets_preproc/")
-cell_info_all <- geo[[1]] %>% Biobase::phenoData() %>% as("data.frame") %>% select(title, characteristics_ch1) %>% rename(cell_id=title, milestone_id=characteristics_ch1) %>% mutate(milestone_id=gsub("cell type level 1: (.*)", "\\1", milestone_id)) %>% mutate_all(funs(as.character))
+geo <- GEOquery::getGEO("GSE67602", destdir = dataset_preproc_file())
+cell_info_all <- geo[[1]] %>%
+  Biobase::phenoData() %>%
+  as("data.frame") %>%
+  select(title, characteristics_ch1) %>%
+  rename(cell_id = title, milestone_id = characteristics_ch1) %>%
+  mutate(milestone_id=gsub("cell type level 1: (.*)", "\\1", milestone_id)) %>%
+  mutate_all(funs(as.character))
 
 settings <- list(
   list(
+    # this ordering is different than in the paper,
+    # but spatially speaking, OB is first and IB second, so this makes much more sense
     milestone_network = tribble(
-      ~from, ~to,
-      "OB", "IB", # this ordering is different than in the paper, but spatially speaking, OB is first and IB second, so this makes much more sense
-      "IB", "uHF-I",
-      "uHF-I", "IFE-B"
+      ~from, ~to, ~length, ~directed,
+      "OB", "IB", 1, TRUE,
+      "IB", "uHF-I", 1, TRUE,
+      "uHF-I", "IFE-B", 1, TRUE
     ),
     id = "epidermis_hair_spatial_joost"
   ),
   list(
     milestone_network = tribble(
-      ~from, ~to,
-      "IFE-B", "IFE-DI",
-      "IFE-DI", "IFE-DII",
-      "IFE-DII", "IFE-KI",
-      "IFE-KI", "IFE-KII"
+      ~from, ~to, ~length, ~directed,
+      "IFE-B", "IFE-DI", 1, TRUE,
+      "IFE-DI", "IFE-DII", 1, TRUE,
+      "IFE-DII", "IFE-KI", 1, TRUE,
+      "IFE-KI", "IFE-KII", 1, TRUE
     ),
     id = "epidermis_hair_IFE_joost"
   ),
   list(
     milestone_network = tribble(
-      ~from, ~to,
-      "uHF-I", "uHF-II",
-      "uHF-II", "uHF-III"
+      ~from, ~to, ~length, ~directed,
+      "uHF-I", "uHF-II",  1, TRUE,
+      "uHF-II", "uHF-III",  1, TRUE
     ),
     id = "epidermis_hair_uHF_joost"
   )
 )
 
 for (setting in settings) {
-  info <- list(id=setting$id)
-
   milestone_network <- setting$milestone_network
 
   milestone_ids <- unique(c(milestone_network$from, milestone_network$to))
@@ -59,12 +69,24 @@ for (setting in settings) {
   cell_grouping <- cell_info %>% select(cell_id, milestone_id) %>% rename(group_id = milestone_id)
   milestone_percentages <- cell_grouping %>% rename(milestone_id=group_id) %>% mutate(percentage=1)
 
-  gene_info <- tibble(id=colnames(counts))
-  gene_ids <- gene_info$id
+  feature_info <- tibble(feature_id = colnames(counts))
 
+  # TODO: use dynutils normalisation
   expression <- log2(counts + 1)
 
-  dataset <- lst(gene_info, cell_info, cell_grouping, cell_ids, gene_ids, counts, expression, milestone_network, milestone_ids, milestone_percentages, info)
+  dataset <- wrap_ti_task_data(
+    ti_type = "real",
+    id = setting$id,
+    counts = counts,
+    expression = expression,
+    cell_ids = cell_ids,
+    milestone_ids = milestone_ids,
+    milestone_network = milestone_network,
+    milestone_percentages = milestone_percentages,
+    cell_grouping = cell_grouping,
+    cell_info = cell_info,
+    feature_info = feature_info
+  )
 
-  dynalysis:::save_dataset(dataset)
+  save_dataset(dataset)
 }
