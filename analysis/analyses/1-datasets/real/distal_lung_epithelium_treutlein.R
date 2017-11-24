1777c2db2eac10be43f0538f897cf33d523c7d80
Robrecht Cannoodt <brechtus@gmail.com>
2017-11-24 11:17:01 +0100

    clean up ocortical_interneuron_differentiation and distal_lung_epithelium

--- analysis/analyses/1-datasets/real/distal_lung_epithelium_treutlein.R
+++ analysis/analyses/1-datasets/real/distal_lung_epithelium_treutlein.R
@@ -9,12 +9,12 @@ txt_web_location <- "http://www.nature.com/nature/journal/v509/n7500/extref/natu
 txt_location <- dataset_preproc_file("nature13173-s4.txt")
 
 if (!file.exists(txt_location)) {
-  download.file(paste0(txt_web_location), txt_location, method="libcurl")
+  download.file(txt_web_location, txt_location, method = "libcurl")
 }
 
-expr <- read_tsv(txt_location)
-expression <- expr[, -c(1:4)] %>% as.matrix() %>% magrittr::set_rownames(expr$cell_name)
-cell_info <- expr[, c(1:4)] %>% as.data.frame() %>% magrittr::set_rownames(expr$cell_name) %>%
+df <- read_tsv(txt_location, col_types = cols(cell_name = "c", time_point = "c", sample = "c", putative_cell_type = "c", .default = "d"))
+expression <- df[, -c(1:4)] %>% as.matrix() %>% magrittr::set_rownames(expr$cell_name)
+cell_info <- df[, c(1:4)] %>% as.data.frame() %>% magrittr::set_rownames(expr$cell_name) %>%
   rename(
     cell_id = cell_name,
     milestone_id = putative_cell_type
@@ -35,8 +35,7 @@ cell_ids <- cell_info$cell_id
 cell_grouping <- cell_info %>% select(cell_id, milestone_id) %>% rename(group_id = milestone_id)
 milestone_percentages <- cell_grouping %>% rename(milestone_id=group_id) %>% mutate(percentage=1)
 
-gene_info <- tibble(id=colnames(expression))
-gene_ids <- gene_info$id
+feature_info <- tibble(feature_id = colnames(expression))
 
 # TODO: check whether the original counts exist
 counts <- round(2^expression - 1)
@@ -52,7 +51,7 @@ dataset <- wrap_ti_task_data(
   milestone_percentages = milestone_percentages,
   cell_grouping = cell_grouping,
   cell_info = cell_info,
-  gene_info = gene_info
+  feature_info = feature_info
 )
 
 save_dataset(dataset)
