dfee2f792daf1e58368ba7afa8279aed49493503
Robrecht Cannoodt <brechtus@gmail.com>
2017-11-24 10:30:01 +0100

    adapt first script

--- analysis/analyses/1-datasets/real/cortical_interneuron_differentiation_frazer.R
+++ analysis/analyses/1-datasets/real/cortical_interneuron_differentiation_frazer.R
@@ -1,33 +1,39 @@
 rm(list=ls())
+library(dynalysis)
 library(tidyverse)
 options('download.file.method.GEOquery'='curl')
 
+id <- "cortical_interneuron_differentiation_frazer"
+dataset_preprocessing("real", id)
+
 txt_web_location <- "https://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE90860&format=file&file=GSE90860%5FSupplementaryData1%5Frevised%5Fjulien%2Etsv%2Egz"
-txt_location <- "analysis/data/datasets_preproc/GSE90860_SupplementaryData1_revised_julien.tsv.gz"
+txt_location <- dataset_preproc_file("GSE90860_SupplementaryData1_revised_julien.tsv.gz")
 
 if (!file.exists(txt_location)) {
   download.file(paste0(txt_web_location), txt_location, method="libcurl") # libcurl muuuuuuuuuch faster, usually
 }
 
-counts <- read_tsv(txt_location) %>% column_to_rownames("X1") %>% as.matrix() %>% t
+counts_df <- read_tsv(txt_location)
+counts <- counts_df %>% select(-X1) %>% as.matrix %>% t
+colnames(counts) <- counts_df$X1
 
-geo <- GEOquery::getGEO(GEO="GSE90860", destdir="analysis/data/datasets_preproc/")
+geo_folder <- dataset_preproc_file("geo")
+dir.create(geo_folder, recursive = TRUE)
+geo <- GEOquery::getGEO(GEO = "GSE90860", destdir = geo_folder)
 cell_info <- geo[[1]] %>% Biobase::phenoData() %>% as("data.frame") %>% mutate(cell_id=rownames(counts))
 cell_info <- cell_info %>%
-  mutate(milestone_id=paste0(gsub("age: (.*)", "\\1", characteristics_ch1), "#", gsub("assigned_subgroup: (.*)", "\\1", characteristics_ch1.1))) %>%
+  mutate(milestone_id = paste0(gsub("age: (.*)", "\\1", characteristics_ch1), "#", gsub("assigned_subgroup: (.*)", "\\1", characteristics_ch1.1))) %>%
   select(cell_id, milestone_id) %>%
   mutate_all(funs(as.character))
 
-info <- list(id="cortical_interneuron_differentiation_frazer")
-
 milestone_network <- tribble(
-  ~from, ~to, ~length,
-  "E18#0", "P2#1", 4,
-  "E18#0", "P2#2", 4,
-  "E18#0", "P2#3", 4,
-  "P2#1", "P5#1", 3,
-  "P2#2", "P5#2", 3,
-  "P2#3", "P5#3", 3
+  ~from, ~to, ~length, ~directed,
+  "E18#0", "P2#1", 4, TRUE,
+  "E18#0", "P2#2", 4, TRUE,
+  "E18#0", "P2#3", 4, TRUE,
+  "P2#1", "P5#1", 3, TRUE,
+  "P2#2", "P5#2", 3, TRUE,
+  "P2#3", "P5#3", 3, TRUE
 )
 milestone_ids <- unique(c(milestone_network$from, milestone_network$to))
 
@@ -43,6 +49,17 @@ gene_ids <- gene_info$id
 
 expression <- log2(counts + 1)
 
-dataset <- lst(gene_info, cell_info, cell_grouping, cell_ids, gene_ids, expression, counts, milestone_network, milestone_ids, milestone_percentages, info)
+dataset <- wrap_ti_task_data(
+  ti_type = "real",
+  id = id,
+  counts = counts,
+  cell_ids = cell_ids,
+  milestone_ids = milestone_ids,
+  milestone_network = milestone_network,
+  milestone_percentages = milestone_percentages,
+  cell_grouping = cell_grouping,
+  cell_info = cell_info,
+  gene_info = gene_info
+)
 
-dynalysis:::save_dataset(dataset)
+save_dataset(dataset)
