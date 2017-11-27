f82bb89fe76a22ff53ddc9d134d694c2877f81ec
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2017-11-27 15:53:30 +0100

    update margues script

--- analysis/analyses/1-datasets/real/oligodendrocyte_differentiation_marques.R
+++ analysis/analyses/1-datasets/real/oligodendrocyte_differentiation_marques.R
@@ -1,28 +1,28 @@
 rm(list=ls())
+library(dynalysis)
 library(tidyverse)
 options('download.file.method.GEOquery'='curl')
 
-txt_web_location <- "http://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE75330&format=file&file=GSE75330%5FMarques%5Fet%5Fal%5Fmol%5Fcounts2%2Etab%2Egz"
-txt_location <- "analysis/data/datasets_preproc/Marques_mol_counts.tab.gz"
+dataset_preprocessing("real", "oligodendrocyte_differentiation_marques")
 
-if (!file.exists(txt_location)) {
-  download.file(paste0(txt_web_location), txt_location, method="libcurl")
-}
+txt_location <- download_dataset_file(
+  "http://www.ncbi.nlm.nih.gov/geo/download/?acc=GSE75330&format=file&file=GSE75330%5FMarques%5Fet%5Fal%5Fmol%5Fcounts2%2Etab%2Egz",
+  "Marques_mol_counts.tab.gz"
+)
 
-allcounts <- read_tsv(txt_location) %>% tibble::column_to_rownames("cellid") %>% as.matrix() %>% t
+allcounts <- read_tsv(txt_location) %>% as.data.frame %>% tibble::column_to_rownames("cellid") %>% as.matrix() %>% t
 
-geo <- GEOquery::getGEO("GSE75330", destdir="analysis/data/datasets_preproc/")
+geo <- GEOquery::getGEO("GSE75330", destdir=dataset_preproc_file())
 allcell_info <- geo[[1]] %>%
   Biobase::phenoData() %>%
   as("data.frame") %>%
   select(title, characteristics_ch1.4) %>%
-  rename(cell_id=title, milestone_id=characteristics_ch1.4) %>%
+  rename(cell_id = title, milestone_id = characteristics_ch1.4) %>%
   mutate(
-    cluster=gsub("inferred cell type: ([A-Z]*)\\d*", "\\1", milestone_id, perl=TRUE),
-    subcluster=gsub("inferred cell type: ([A-Z1-9]*)", "\\1", milestone_id)) %>%
+    cluster = gsub("inferred cell type: ([A-Z]*)\\d*", "\\1", milestone_id, perl=TRUE),
+    subcluster = gsub("inferred cell type: ([A-Z1-9]*)", "\\1", milestone_id)
+  ) %>%
   mutate_all(funs(as.character))
-
-info <- list(id="oligodendrocyte_differentiation_marques")
 
 settings <- list(
   list(
@@ -41,7 +41,7 @@ settings <- list(
       "MFOL2", "MOL4",
       "MFOL2", "MOL5",
       "MFOL2", "MOL6"
-    )
+    ) %>% mutate(length = 1, directed = TRUE)
   ),
   list(
     id = "oligodendrocyte_differentiation_marques_clusters",
@@ -51,12 +51,13 @@ settings <- list(
       "OPC", "COP",
       "COP", "NFOL",
       "NFOL", "MOL"
-    )
+    ) %>% mutate(length = 1, directed = TRUE)
   )
 )
 
 for (setting in settings) {
-  info <- list(id = setting$id)
+  dataset_preprocessing("real", setting$id)
+
   milestone_network <- setting$milestone_network
   cell_info <- allcell_info
   cell_info$milestone_id <- cell_info[[setting$milestone_source]]
@@ -70,12 +71,24 @@ for (setting in settings) {
   cell_grouping <- cell_info %>% select(cell_id, milestone_id) %>% rename(group_id = milestone_id)
   milestone_percentages <- cell_grouping %>% rename(milestone_id = group_id) %>% mutate(percentage = 1)
 
-  gene_info <- tibble(id = colnames(counts))
-  gene_ids <- gene_info$id
+  feature_info <- tibble(feature_id = colnames(counts))
 
+  # todo: use dynutils normalisation
   expression <- log2(counts + 1)
 
-  dataset <- lst(gene_info, cell_info, cell_grouping, cell_ids, gene_ids, expression, counts, milestone_network, milestone_ids, milestone_percentages, info)
+  dataset <- wrap_ti_task_data(
+    ti_type = "real",
+    id = datasetpreproc_getid(),
+    counts = counts,
+    expression = expression,
+    cell_ids = cell_ids,
+    milestone_ids = milestone_ids,
+    milestone_network = milestone_network,
+    milestone_percentages = milestone_percentages,
+    cell_grouping = cell_grouping,
+    cell_info = cell_info,
+    feature_info = feature_info
+  )
 
-  dynalysis:::save_dataset(dataset)
+  save_dataset(dataset)
 }
