8d619f870389531663c8bc0b4300eb4546558921
Wouter Saelens <wouter.saelens@gmail.com>
2017-11-27 10:59:15 +0100

    add pritt

--- scripts/update_paper_from_google_drive.R
+++ scripts/update_paper_from_google_drive.R
@@ -1,6 +1,7 @@
 library(googledrive)
 library(tidyverse)
 library(rmarkdown)
+library(dynutils)
 
 drive <- drive_download(as_id("1BCCaP21N2PXfzhj9H09yEpZz9lLY2HXd_LxTSsJ_wro"), type="text/plain", overwrite=TRUE, path = tempfile())
 system(pritt("sed -i '1s/^.//' {drive$local_path}")) # remove first character, because this is some strang eunicode character
