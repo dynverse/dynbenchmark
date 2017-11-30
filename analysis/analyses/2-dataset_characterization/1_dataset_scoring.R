b494da1ec4db2a081ce498ee68a82b55a617d0ec
Wouter Saelens <wouter.saelens@gmail.com>
2017-11-30 10:09:35 +0100

    fix dataset scoring

--- analysis/analyses/2-dataset_characterization/1_dataset_scoring.R
+++ analysis/analyses/2-dataset_characterization/1_dataset_scoring.R
@@ -6,14 +6,17 @@ library(dynalysis)
 
 dataset_infos <- gs_title("Real datasets") %>%
   gs_read(ws = "datasets", col_types = cols(date = col_date())) %>%
-  separate_rows(id, sep=",")
+  separate_rows(id, sep=",\n") %>%
+  mutate(id = paste0("real/", id))
 
 dataset_ids <- dataset_infos$id
 
-# do an export DYNALYSIS_PATH="/group/irc/shared/dynalysis" on prism beforehand
-stop("read above comment")
 map_prism <- function (x, f) PRISM::qsub_lapply(x, f, qsub_environment = list2env(list()), qsub_config = PRISM::override_qsub_config(memory="4G"))
-map_local <- function(x, f) parallel::mclapply(x, f, mc.cores=6)
+map_local <- function(x, f) pbapply::pblapply(x, f, cl=1)
+map_local_parallel <- function(x, f) pbapply::pblapply(x, f, cl=8)
+
+
+paste0("real/", list.files("analysis/data/derived_data/datasets/real/"))[!(paste0("real/", list.files("analysis/data/derived_data/datasets/real/")) %in% dataset_ids)]
 
 
 # Check the datasets ------------------------------------
@@ -24,24 +27,30 @@ dataset_checks <- map_local(dataset_ids, function(dataset_id) {
   print(paste0("-------------", dataset_id))
   print("loading")
 
-  stop("The function below does not exist anymore, use ")
-  dataset <- load_dataset("real", dataset_id)
-  dataset %>% list2env(.GlobalEnv)
+  if (!file.exists(dataset_file("dataset.rds", dataset_id = dataset_id))) {
+    return(list(id = dataset_id, found = FALSE))
+  }
 
-  all_milestones_represented <- all(milestone_ids %in% cell_grouping$group_id)
+  #stop("The function below does not exist anymore, use ")
+  dataset <- readRDS(dataset_file("dataset.rds", dataset_id = dataset_id))
 
-  lst(all_milestones_represented, id=info$id) %>% as_tibble()
+  all_milestones_represented <- all(dataset$milestone_ids %in% dataset$cell_grouping$group_id)
+
+  lst(all_milestones_represented, id=dataset_id, n_cells = nrow(dataset$cell_info), n_genes = nrow(dataset$feature_info)) %>% as_tibble()
 })
 dataset_checks <- dataset_checks %>% bind_rows()
+dataset_checks
+
+dataset_ids_filtered <- dataset_checks %>% filter(all_milestones_represented) %>% pull(id)
 
 # Score the datasets ---------------------------------
-# dataset_scores <- map_local(dataset_ids, function(dataset_id) {
-dataset_scores <- map_prism(dataset_ids, function(dataset_id) {
+dataset_scores <- map_local(dataset_ids_filtered, function(dataset_id) {
+# dataset_scores <- map_prism(dataset_ids, function(dataset_id) {
   library(tidyverse)
   library(dynalysis)
   print(paste0("-------------", dataset_id))
   print("loading")
-  dataset <- load_dataset("real", dataset_id)
+  dataset <- readRDS(dataset_file("dataset.rds", dataset_id = dataset_id))
   dataset %>% list2env(.GlobalEnv)
 
   Coi <- rowMeans(dataset$expression) %>% {(. > (median(.) - IQR(., 0.5)*2)) & (. < (median(.) + IQR(., 0.5)*2))} %>% keep(~.) %>% names
@@ -83,7 +92,7 @@ dataset_scores <- map_prism(dataset_ids, function(dataset_id) {
     score_aggregation(dataset$expression[Coi, Goi], dataset$cell_grouping)
   )
 
-  scores$dataset_id <- dataset$info$id
+  scores$dataset_id <- dataset_id
 
   scores
 })
@@ -110,29 +119,19 @@ dataset_scores %>% .[c(score_ids[score_ids %in% colnames(dataset_scores)], "data
 
 
 ### Cell plotting ----------------
-cell_plots <- map_prism(dataset_ids, function(dataset_id) {
-# cell_plots <- map_local(dataset_ids, function(dataset_id) {
+# cell_plots <- map_prism(dataset_ids, function(dataset_id) {
+cell_plots <- map_local(dataset_ids, function(dataset_id) {
   library(tidyverse)
   plots <- list()
 
-  dataset <- load_dataset("real", dataset_id)
+  dataset <- readRDS(dataset_file("dataset.rds", dataset_id = dataset_id))
 
-  gene_sds <- apply(dataset$expression, 2, sd) %>% sort(decreasing = TRUE)
-  Goi <- gene_sds %>% head(5000) %>% names()
-  Coi <- sample(rownames(dataset$expression), min(nrow(dataset$expression), 500))
-  Coi <- Coi[dataset$expression[Coi, Goi] %>% apply(1, sd) %>% {. > 0}]
+  source("../dynmodular/dimred_wrappers.R")
 
-  dimred_mds = function(x, ndim=3) {
-    space = SCORPIUS::reduce_dimensionality(SCORPIUS::correlation_distance(x),ndim = ndim)
-    space = as.matrix(space)
-    colnames(space) = paste0("Comp", 1:ncol(space))
-    space
-  }
-
-  space = dimred_mds(dataset$expression[Coi, Goi], ndim = 2)
+  space <- dimred_mds(dataset$expression)
 
   plotdata_cells <- bind_cols(space %>% as.data.frame() %>% tibble::rownames_to_column("cell_id")) %>% left_join(dataset$cell_info, by="cell_id") %>% left_join(dataset$cell_grouping, by="cell_id")
-  plotdata_cells$ncount <- rowMeans(dataset$expression[Coi, Goi])
+  plotdata_cells$libsize <- rowMeans(dataset$expression)
 
   plotdata_groups <- plotdata_cells %>% group_by(group_id) %>% summarise(Comp1=mean(Comp1), Comp2=mean(Comp2))
 
@@ -140,20 +139,19 @@ cell_plots <- map_prism(dataset_ids, function(dataset_id) {
     left_join(plotdata_groups %>% rename_at(vars(starts_with("Comp")), list(~paste0("from_", .))), by=c("from"="group_id")) %>%
     left_join(plotdata_groups %>% rename_at(vars(starts_with("Comp")), list(~paste0("to_", .))), by=c("to"="group_id"))
 
-  plots$plot_cells <- list(
-    ggplot() +
-    geom_point(aes(Comp1, Comp2, color=group_id), shape=21, stroke=4, data=plotdata_groups, size=3, fill="black") +
+  base_scater <- ggplot(plotdata_groups, aes(Comp1, Comp2)) +
+    geom_point(aes(Comp1, Comp2), shape=21, stroke=4, size=3, alpha=0.3) +
     geom_segment(aes(from_Comp1, from_Comp2, xend=to_Comp1, yend=to_Comp2), data=plotdata_network, arrow=arrow(), size=2, alpha=0.2) +
-    geom_point(aes(Comp1, Comp2, color=group_id), data=plotdata_cells) +
+    geom_point(aes(Comp1, Comp2, fill=group_id), shape=23, stroke=1, data=plotdata_groups, size=3, color="black") +
     theme(legend.position="bottom")
-  )
 
-  plots$plot_ncount <- list(
-    ggplot() +
-    geom_point(aes(Comp1, Comp2), shape=21, stroke=4, data=plotdata_groups, size=3, alpha=0.3) +
-    geom_segment(aes(from_Comp1, from_Comp2, xend=to_Comp1, yend=to_Comp2), data=plotdata_network, arrow=arrow(), size=2, alpha=0.2) +
-    geom_point(aes(Comp1, Comp2, color=ncount), data=plotdata_cells) +
-    viridis::scale_color_viridis()
+  plots$plot_cells <- list(
+    base_scater + geom_point(aes(Comp1, Comp2, color=group_id), data=plotdata_cells)
+  )
+  plots$plot_libsize <- list(
+    base_scater +
+      geom_point(aes(Comp1, Comp2, color=libsize), data=plotdata_cells) +
+      viridis::scale_color_viridis(option="A")
   )
 
   rm(dataset)
@@ -165,17 +163,29 @@ cell_plots <- cell_plots %>% bind_rows()
 grid <- cowplot::plot_grid(plotlist=cell_plots %>% pull(plot_cells))
 grid
 
+save_plot("grid.pdf", grid, base_height=20, base_width=20)
+
+
+grid <- cowplot::plot_grid(plotlist=cell_plots %>% pull(plot_libsize))
+save_plot("grid2.pdf", grid, base_height=30, base_width=30)
+
 
 ### Network plotting --------------------------
 library(ggnetwork)
-network_plots <- parallel::mclapply(dataset_ids, function(dataset_id) {
+network_plots <- map_local(dataset_ids_filtered, function(dataset_id) {
   library(tidyverse)
   print(paste0("-------------", dataset_id))
   print("loading")
-  dataset <- load_dataset("real", dataset_id)
+  dataset <- readRDS(dataset_file("dataset.rds", dataset_id = dataset_id))
 
   milestone_network <- dataset$milestone_network
   milestone_nodes <- dataset$cell_info %>% group_by(milestone_id) %>% summarise(n=n())
+
+  end_nodes <- (milestone_nodes$milestone_id %in% milestone_network$to) & !(milestone_nodes$milestone_id %in% milestone_network$from)
+  start_nodes <- !(milestone_nodes$milestone_id %in% milestone_network$to) & (milestone_nodes$milestone_id %in% milestone_network$from)
+  milestone_nodes$type <- ifelse(start_nodes, "start", ifelse(end_nodes, "end", "inner"))
+  type_shapes <- c("start"=17, "inner"=16, "end"=15)
+  type_colors <- c("start"="#0074D9", "inner"="#FF851B", "end"="#FF4136")
 
   graph <- igraph::graph_from_data_frame(milestone_network, TRUE, milestone_nodes)
 
@@ -186,39 +196,42 @@ network_plots <- parallel::mclapply(dataset_ids, function(dataset_id) {
     theme_blank() +
     scale_x_continuous(expand=c(0.2, 0)) +
     scale_y_continuous(expand=c(0.2, 0)) +
-    theme(legend.position="none")
+    theme(legend.position="none") +
+    geom_edges() +
+    geom_edges(aes(x=x, y=y, xend=xend - (xend - x)/3, yend = yend - (yend - y)/3), arrow=arrow(type="closed", length=unit(0.1, "inches"))) +
+    geom_edges(aes(x=x, y=y, xend=xend - (xend - x)/3*2, yend = yend - (yend - y)/3*2), arrow=arrow(type="closed", length=unit(0.1, "inches")))
 
   network_plot_labeled <- list(
     base_plot +
-      geom_edges() +
-      geom_edges(aes(x=x, y=y, xend=xend - (xend - x)/3, yend = yend - (yend - y)/3), arrow=arrow(type="closed", length=unit(0.1, "inches"))) +
-      geom_edges(aes(x=x, y=y, xend=xend - (xend - x)/3*2, yend = yend - (yend - y)/3*2), arrow=arrow(type="closed", length=unit(0.1, "inches"))) +
       geom_nodelabel(aes(label=vertex.names, fill=vertex.names), color="white")
   )
 
   network_plot = list(
     base_plot +
-      geom_edges() +
-      geom_edges(aes(x=x, y=y, xend=xend - (xend - x)/3, yend = yend - (yend - y)/3), arrow=arrow(type="closed", length=unit(0.1, "inches"))) +
-      geom_edges(aes(x=x, y=y, xend=xend - (xend - x)/3*2, yend = yend - (yend - y)/3*2), arrow=arrow(type="closed", length=unit(0.1, "inches"))) +
-      geom_nodes(aes(color=vertex.names), size=10)
+      geom_nodes(aes(color=vertex.names, shape = type), size=10) + scale_shape_manual(values=type_shapes)
+  )
+
+  network_plot_startstop = list(
+    base_plot +
+      geom_nodes(aes(color=type, shape = type), size=10) +
+      scale_shape_manual(values=type_shapes) +
+      scale_color_manual(values=type_colors)
   )
 
   network_plot_sizes = list(
     base_plot +
-      geom_edges() +
-      geom_edges(aes(x=x, y=y, xend=xend - (xend - x)/3, yend = yend - (yend - y)/3), arrow=arrow(type="closed", length=unit(0.1, "inches"))) +
-      geom_edges(aes(x=x, y=y, xend=xend - (xend - x)/3*2, yend = yend - (yend - y)/3*2), arrow=arrow(type="closed", length=unit(0.1, "inches"))) +
       geom_nodes(aes(color=vertex.names, size=n)) +
       scale_size_continuous(range=c(3, 20), trans="log10", limits=c(1, 100000))
   )
 
   lst(
-    id = dataset$info$id,
+    id = dataset_id,
     network_plot_labeled,
-    network_plot
+    network_plot,
+    network_plot_sizes,
+    network_plot_startstop
   ) %>% as_tibble()
-}, mc.cores=8)
+})
 network_plots <- network_plots %>% bind_rows()
 
 library(ggnetwork)
@@ -226,7 +239,7 @@ network_characteristics <- parallel::mclapply(dataset_ids, function(dataset_id) 
   library(tidyverse)
   print(paste0("-------------", dataset_id))
   print("loading")
-  dataset <- load_dataset("real", dataset_id)
+  dataset <- readRDS(dataset_file("dataset.rds", dataset_id = dataset_id))
 
   contains_cycle <- FALSE
   tryCatch(
@@ -237,14 +250,14 @@ network_characteristics <- parallel::mclapply(dataset_ids, function(dataset_id) 
   contains_bifurcating <- dataset$milestone_network$from %>% table %>% {any(.>1)}
   contains_convergence <- dataset$milestone_network$to %>% table %>% {any(.>1)}
 
-  lst(contains_cycle, contains_bifurcating, contains_convergence, id=dataset$info$id)
+  lst(contains_cycle, contains_bifurcating, contains_convergence, id=dataset_id)
 }, mc.cores=8) %>% bind_rows()
 network_characteristics$topology_id <- network_characteristics %>% select_if(is.logical) %>% apply(1, function(x) sum((2^seq_along(x))[x]))
 
 
 datasets <- dataset_infos %>% left_join(network_characteristics, "id") %>% left_join(network_plots, "id")
 
-grid <- cowplot::plot_grid(plotlist=datasets %>% arrange(topology_id) %>% filter(standard=="gold") %>% pull(network_plot))
+grid <- cowplot::plot_grid(plotlist=datasets %>% arrange(topology_id) %>% filter(standard=="gold") %>% pull(network_plot_startstop))
 title <- ggdraw() + draw_label("Gold standard networks", fontface='bold')
 plot_grid(title, grid, ncol=1, rel_heights=c(0.1, 1)) # rel_heights values control title margins
 
