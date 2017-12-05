c97d11347d502568bb98d51a558e0731b428cda0
Wouter Saelens <wouter.saelens@gmail.com>
2017-12-05 10:23:31 +0100

    method characterization: trajecotry types

---
+++ analysis/analyses/4-method_characterization/create_trajectory_types.R
@@ -0,0 +1,8 @@
+boxes <- tibble(
+  id = c("linear", "single_bifurcation", "binary_tree", "single_multifurcation", "non_binary_tree", "single_cycle", "simple_graph", "single_bifurcation_single_convergence"), opacity = 1
+)
+map(boxes$id, function(id) {
+  boxes$opacity <- ifelse(boxes$id == id, 1, 0)
+
+  change_opacity(boxes, id, "analysis/figures/trajectory_types.svg", folder = "analysis/figures/trajectory_types/mini/", export = "--export-area-drawing", trim=TRUE, svg=TRUE, png=FALSE)
+})
---
+++ analysis/analyses/4-method_characterization/svg_helpers.R
@@ -0,0 +1,66 @@
+library(xml2)
+
+xml_find_multiple_ids <- function(xml, ids) {
+  ids_txt <- paste0("@id = \'", ids, "\'", collapse=" or ")
+  xpath <- glue("//svg:g[{ids_txt}]")
+  xml_find_all(xml, xpath)
+}
+
+change_opacity <- function(
+  boxes,
+  output,
+  svg_location,
+  folder = ".",
+  verbose = FALSE,
+  export = "--export-area-page",
+  trim = FALSE,
+  size = "100%",
+  png=TRUE,
+  svg=FALSE
+) {
+  if (!("opacity" %in% colnames(boxes))) {
+    stop("Need opacity")
+  }
+
+  xml <- read_xml(svg_location)
+
+  xml_find_multiple_ids <- function(xml, ids) {
+    ids_txt <- paste0("@id = \'", ids, "\'", collapse=" or ")
+    xpath <- glue::glue("//svg:g[{ids_txt}]")
+    xml_find_all(xml, xpath)
+  }
+
+  walk(boxes %>% split(boxes$opacity), function(x) {
+    nodes <- xml_find_multiple_ids(
+      xml,
+      x$id
+    )
+    xml_attr(nodes, "style") <- glue::glue("opacity:{x$opacity[[1]]};")
+
+    if (x$opacity[[1]] == 0) {
+      xml_remove(nodes)
+    }
+  })
+
+  write(as.character(xml), file=glue::glue("{folder}/{output}.svg"))
+
+  if(svg && trim) {
+    command <- glue::glue("inkscape --verb=FitCanvasToDrawing --verb=FileSave --verb=FileQuit {folder}/{output}.svg")
+    system(command, ignore.stdout = !verbose)
+  }
+
+  if (!svg) {
+    file.remove(glue::glue("{folder}/{output}.svg"))
+  }
+
+  if (png) {
+    if(trim) {
+      system(glue::glue("convert {folder}/{output}.png -trim {folder}/{output}.png"))
+    }
+    system(glue::glue("inkscape {folder}/{output}.svg --export-area-page --export-png={folder}/{output}.png"), ignore.stdout = !verbose)
+  }
+
+  glue::glue('{folder}/{output}.png')
+
+  #glue::glue("<img src='{folder}/{output}.png' style='max-width:{size};max-height:{size}' />")
+}
