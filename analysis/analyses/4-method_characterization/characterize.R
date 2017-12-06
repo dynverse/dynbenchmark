eb69d1c4ffa5fc5ddbbfa5732e78429182c949af
Wouter Saelens <wouter.saelens@gmail.com>
2017-12-06 14:30:34 +0100

    Remove lm, add smoothing

--- analysis/analyses/4-method_characterization/characterize.R
+++ analysis/analyses/4-method_characterization/characterize.R
@@ -77,7 +77,7 @@ method_df_evaluated <- read_rds(derived_file("method_df_evaluated.rds"))
 
 # Comparing number of citations, qc score, and time -------------------------------------
 g1 <- ggplot(method_df_evaluated, aes(date, qc_score)) +
-  geom_smooth(method="lm") +
+  geom_smooth(span=2) +
   geom_point() +
   ggrepel::geom_label_repel(aes(label = name)) +
   cowplot::theme_cowplot() +
