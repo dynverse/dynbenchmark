af646e4a65df407cd09ce32487cccc3bc6a09387
Robrecht Cannoodt <brechtus@gmail.com>
2017-12-12 16:25:21 +0100

    update script

--- analysis/analyses/5-optimise_parameters/8-evaluate_with_real_datasets.R
+++ analysis/analyses/5-optimise_parameters/8-evaluate_with_real_datasets.R
@@ -4,30 +4,41 @@ library(dynplot)
 
 experiment("5-optimise_parameters/8-evaluate_with_real_datasets")
 
+dataset_names <- list_datasets()
+tasks <- pbapply::pblapply(dataset_names, load_dataset)
+tasks[[31]]$trajectory_type <- "linear"
+tasks <- tasks %>% list_as_tibble()
+
 best_parm <- read_rds(result_file("best_params.rds", "5-optimise_parameters/7-train_parameters_with_synthetic_datasets"))
 
-methods <- get_descriptions(as_tibble = T)
+methods <- get_descriptions(as_tibble = F)
 metrics <- "auc_R_nx"
 timeout <- 300
 
-# start benchmark suite
-benchmark_suite_submit(
-  tasks,
-  task_group,
-  task_fold,
-  out_dir = derived_file("suite/"),
-  save_r2g_to_outdir = TRUE,
-  methods = methods,
+mn <- "SCORPIUS"
+
+param_sel <- best_parm %>% filter(method_name == mn) %>% dynutils::extract_row_to_list(1)
+method <- methods[[mn]]
+
+parameters <- param_sel$params
+parameters <- lapply(names(parameters), function(prnm) {
+  parset_par <- method$par_set$pars[[prnm]]
+  parv <- parameters[[prnm]]
+  if (!is.null(parset_par$trafo)) {
+    parv <- parset_par$trafo(parv)
+  }
+  parv
+}) %>% setNames(names(parameters))
+
+
+eval_out <- execute_evaluation(
+  tasks = tasks %>% slice(1),
+  method = method,
+  parameters = parameters,
   metrics = metrics,
   timeout = timeout,
-  memory = "11G",
-  num_cores = 1,
-  num_iterations = 100,
-  num_repeats = 4,
-  num_init_params = 200,
-  execute_before = "source /scratch/irc/shared/dynverse/module_load_R.sh; export R_MAX_NUM_DLLS=500",
-  r_module = NULL
+  output_model = T,
+  error_score = 0
 )
-
-
-outputs <- benchmark_suite_retrieve(derived_file("suite/"))
+extras <- attr(eval_out, "extras")
+extras$.summary$error[[1]]
