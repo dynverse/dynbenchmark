cb729e4077bec175ffcb70b9f00efd2e7a9be574
Wouter Saelens <wouter.saelens@gmail.com>
2017-12-13 12:04:03 +0100

    create datasets on cluster update

--- analysis/analyses/1-datasets/real/run_all_datasets.R
+++ analysis/analyses/1-datasets/real/run_all_datasets.R
@@ -1,4 +1,4 @@
-
+# Local
 dataset_scripts <- list.files(path = "analysis/analyses/1-datasets/real", pattern = "^dataset_.*\\.R", full.names = TRUE)
 
 for (scr in dataset_scripts) {
@@ -6,20 +6,13 @@ for (scr in dataset_scripts) {
 }
 
 
-
-
-
-
-
-
-
+## Need both GEOquery and MultiAssayExperiment remotely
 library(PRISM)
-qsub_conf <- override_qsub_config(memory = "30G", wait=FALSE, r_module = NULL, execute_before="module unload R")
+qsub_conf <- override_qsub_config(memory = "30G", wait=FALSE, r_module = NULL, execute_before="module unload R", stop_on_error=FALSE)
 
 handle <- qsub_lapply(dataset_scripts, function(dataset_script) {
-  wd <- getwd()
-  setwd(dynalysis::get_dynalysis_folder())
-  source(dataset_script)
-  setwd(wd)
+  source(paste0(dynalysis::get_dynalysis_folder(), "/", dataset_script))
+  TRUE
 }, qsub_environment = list2env(list()), qsub_config = qsub_conf)
-qsub_retrieve(handle)
+saveRDS(handle, "handle.rds")
+results <- qsub_retrieve(handle)
