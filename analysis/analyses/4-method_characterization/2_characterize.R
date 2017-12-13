c6b4348fd9abc7282b84ca96453d9eed73a80db7
Robrecht Cannoodt <brechtus@gmail.com>
2017-12-13 15:42:16 +0100

    add experiment name

--- analysis/analyses/4-method_characterization/2_characterize.R
+++ analysis/analyses/4-method_characterization/2_characterize.R
@@ -2,10 +2,13 @@ library(tidyverse)
 library(dynalysis)
 library(cowplot)
 
+experiment("method_characteristics")
+
 source("analysis/analyses/4-method_characterization/0_common.R")
 
 method_df <- read_rds(derived_file("method_df.rds"))
 method_df_evaluated <- read_rds(derived_file("method_df_evaluated.rds"))
+
 
 # Comparing number of citations, qc score, and time -------------------------------------
 g1 <- ggplot(method_df_evaluated, aes(date, qc_score)) +
