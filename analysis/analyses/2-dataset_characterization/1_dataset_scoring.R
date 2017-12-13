df5b01d99e325e515c6f37b6143b9ca464cda677
Wouter Saelens <wouter.saelens@gmail.com>
2017-12-13 12:04:11 +0100

    update on dataset scoring

--- analysis/analyses/2-dataset_characterization/1_dataset_scoring.R
+++ analysis/analyses/2-dataset_characterization/1_dataset_scoring.R
@@ -4,110 +4,122 @@ library(googlesheets)
 library(dynalysis)
 # devtools::load_all()
 
-dataset_infos <- gs_title("Real datasets") %>%
+task_infos <- gs_title("Real datasets") %>%
   gs_read(ws = "datasets", col_types = cols(date = col_date())) %>%
   separate_rows(id, sep=",\n") %>%
   mutate(id = paste0("real/", id))
 
-dataset_ids <- dataset_infos$id
+task_ids <- task_infos$id
+
+# tasks not online but found physically
+paste0("real/", list.files("analysis/data/derived_data/datasets/real/"))[!(paste0("real/", list.files("analysis/data/derived_data/datasets/real/")) %in% task_ids)]
+
+task_ids[!(task_ids %in% paste0("real/", list.files("analysis/data/derived_data/datasets/real/")))]
+task_ids <- task_ids[(task_ids %in% paste0("real/", list.files("analysis/data/derived_data/datasets/real/")))]
 
 map_prism <- function (x, f) PRISM::qsub_lapply(x, f, qsub_environment = list2env(list()), qsub_config = PRISM::override_qsub_config(memory="4G"))
 map_local <- function(x, f) pbapply::pblapply(x, f, cl=1)
 map_local_parallel <- function(x, f) pbapply::pblapply(x, f, cl=8)
 
-# datasets not online but found physically
-paste0("real/", list.files("analysis/data/derived_data/datasets/real/"))[!(paste0("real/", list.files("analysis/data/derived_data/datasets/real/")) %in% dataset_ids)]
-
-
 tasks_synthetic <- read_rds("analysis/data/derived_data/datasets/synthetic/v5.rds")
-tasks_real <- map_local(dataset_ids, function(dataset_id) {
-  dataset <- readRDS(dataset_file("dataset.rds", dataset_id = dataset_id))
+tasks_real <- map_local(task_ids, function(task_id) {
+  print(task_id)
+  task <- readRDS(dataset_file("dataset.rds", dataset_id = task_id))
 
-  dataset$expression <- function() {dataset_file("dataset.rds", dataset_id = dataset_id)$expression}
-  dataset$counts <- function() {dataset_file("dataset.rds", dataset_id = dataset_id)$counts}
+  print(task$trajectory_type)
 
-  dynutils::list_as_tibble(list(dataset))
+  task$expression <- function() {readRDS(dataset_file("dataset.rds", dataset_id = task_id))$expression}
+  task$counts <- function() {readRDS(dataset_file("dataset.rds", dataset_id = task_id))$counts}
+
+  dynutils::list_as_tibble(list(task))
 })
-tasks <- bind_rows(tasks_synthetic, tasks_real)
+tasks_toy <- dyntoy::toy_tasks
+tasks <- bind_rows(tasks_synthetic, tasks_real, toy_tasks)
+
+tasks$category <- ifelse(tasks$id %>% startsWith("real"), "real", ifelse(tasks$id %>% startsWith("toy"), "toy", "synthetic"))
 
 
-# Check the datasets ------------------------------------
-# dataset_checks <- map_prism(dataset_ids, function(dataset_id) {
-dataset_checks <- map_local(dataset_ids, function(dataset_id) {
+# Check the tasks ------------------------------------
+# task_checks <- map_prism(task_ids, function(task_id) {
+task_checks <- pmap(as.list(tasks), function(...) {
+  # task <- extract_row_to_list(tasks, 1)
+  task <- list(...)
   library(tidyverse)
-  # dataset_checks <- parallel::mclapply(mc.cores=1, dataset_ids, function(dataset_id) {
-  print(paste0("-------------", dataset_id))
+  print(paste0("-------------", task$id))
   print("loading")
 
-  if (!file.exists(dataset_file("dataset.rds", dataset_id = dataset_id))) {
-    return(list(id = dataset_id, found = FALSE))
+  all_milestones_represented <- all(task$milestone_ids %in% task$prior_information$grouping_assignment$group_id)
+
+  lst(all_milestones_represented, task_id = task$id, n_cells = nrow(task$cell_info), n_genes = nrow(task$feature_info)) %>% as_tibble()
+}) %>% bind_rows()
+
+task_checks %>% bind_cols(tasks) %>% ggplot() + geom_point(aes(n_genes, n_cells, color=category))
+
+task_ids_filtered <- task_checks %>% filter(all_milestones_represented) %>% pull(task_id)
+tasks_filtered <- tasks %>% filter(id %in% task_ids_filtered)
+
+load_expression <- function(expr) {
+  if(class(expr) == "function") {
+    expr()
+  } else {
+    expr
   }
+}
 
-  dataset <- readRDS(dataset_file("dataset.rds", dataset_id = dataset_id))
-
-  all_milestones_represented <- all(dataset$milestone_ids %in% dataset$cell_grouping$group_id)
-
-  lst(all_milestones_represented, id=dataset_id, n_cells = nrow(dataset$cell_info), n_genes = nrow(dataset$feature_info)) %>% as_tibble()
-})
-dataset_checks <- dataset_checks %>% bind_rows()
-dataset_checks
-
-dataset_ids_filtered <- dataset_checks %>% filter(all_milestones_represented) %>% pull(id)
-
-# Score the datasets ---------------------------------
-dataset_scores <- map_local(dataset_ids_filtered, function(dataset_id) {
-  # dataset_scores <- map_prism(dataset_ids, function(dataset_id) {
+# Score the tasks ---------------------------------
+task_scores <- pmap(as.list(tasks_filtered), function(...) {
+  # task <- extract_row_to_list(tasks, 20)
+  task <- list(...)
   library(tidyverse)
-  library(dynalysis)
-  print(paste0("-------------", dataset_id))
+  print(paste0("-------------", task$id))
   print("loading")
-  dataset <- readRDS(dataset_file("dataset.rds", dataset_id = dataset_id))
-  dataset %>% list2env(.GlobalEnv)
 
-  Coi <- rowMeans(dataset$expression) %>% {(. > (median(.) - IQR(., 0.5)*2)) & (. < (median(.) + IQR(., 0.5)*2))} %>% keep(~.) %>% names
+  task$expression <- load_expression(task$expression)
 
-  gene_sds <- apply(dataset$expression[Coi, ], 2, sd) %>% sort(decreasing = TRUE)
+  Coi <- rowMeans(task$expression) %>% {(. > (median(.) - IQR(., 0.5)*2)) & (. < (median(.) + IQR(., 0.5)*2))} %>% keep(~.) %>% names
+
+  gene_sds <- apply(task$expression[Coi, ], 2, sd) %>% sort(decreasing = TRUE)
   Goi <- gene_sds %>% names %>% head(5000)
 
   if(length(Goi) == 0) {stop("No genes selected")}
   if(length(Coi) == 0) {stop("No cells selected")}
   if(length(Coi) > 500) {Coi=sample(Coi, 500)}
 
-  Coi <- Coi[dataset$expression[Coi, Goi] %>% apply(1, sd) %>% {. > 0}]
+  Coi <- Coi[task$expression[Coi, Goi] %>% apply(1, sd) %>% {. > 0}]
 
   # make sure we select for every group at least one cell
-  Coi2 <- dataset$cell_grouping %>% group_by(group_id) %>% filter(row_number() == 1) %>% pull(cell_id)
+  Coi2 <- task$prior_information$grouping_assignment %>% group_by(group_id) %>% filter(row_number() == 1) %>% pull(cell_id)
   Coi <- union(Coi, Coi2)
 
-  expression_grouped <- dataset$expression[Coi, Goi] %>% dynutils::group_counts(dataset$cell_grouping)
+  expression_grouped <- task$expression[Coi, Goi] %>% dynutils::group_counts(task$prior_information$grouping_assignment)
   scores <- list()
 
   print("transitions")
   scores <- bind_cols(
     scores,
-    score_milestone_transitions(expression_grouped, dataset$milestone_ids, dataset$milestone_network)
+    score_milestone_transitions(expression_grouped, task$milestone_ids, task$milestone_network)
   )
   print("grouping")
   scores <- bind_cols(
     scores,
-    score_milestone_grouping(dataset$expression[Coi, Goi], dataset$cell_grouping)
+    score_milestone_grouping(task$expression[Coi, Goi], task$prior_information$grouping_assignment)
   )
   print("connectivity")
   scores <- bind_cols(
     scores,
-    score_milestone_connectivity(dataset$expression[Coi, Goi], dataset$cell_grouping, dataset$milestone_ids, dataset$milestone_network)
+    score_milestone_connectivity(task$expression[Coi, Goi], task$prior_information$grouping_assignment, task$milestone_ids, task$milestone_network)
   )
   print("aggregation")
   scores <- bind_cols(
     scores,
-    score_aggregation(dataset$expression[Coi, Goi], dataset$cell_grouping)
+    score_aggregation(task$expression[Coi, Goi], task$prior_information$grouping_assignment)
   )
 
-  scores$dataset_id <- dataset_id
+  scores$task_id <- task$id
 
   scores
 })
-dataset_scores <- dataset_scores %>% bind_rows()
+task_scores <- task_scores %>% bind_rows()
 
 score_ids <- c("aggregation_sd_frac", "transition_pval", "transition_frac", "grouping_asw", "connectivity")
 score_baselines <- tribble(
@@ -119,34 +131,38 @@ score_baselines <- tribble(
   "grouping_asw", 0, "hard",
   "connectivity", 0.2, "soft"
 )
-dataset_scores %>% .[c(score_ids[score_ids %in% colnames(dataset_scores)], "dataset_id")] %>% gather("score_id", "score", -dataset_id) %>%
+task_scores %>% .[c(score_ids[score_ids %in% colnames(task_scores)], "task_id")] %>% gather("score_id", "score", -task_id) %>%
   ggplot() +
-  geom_bar(aes(dataset_id, score), stat="identity") +
+  geom_bar(aes(task_id, score), stat="identity") +
   facet_wrap(~score_id, scales = "free_x", ncol=10) +
-  geom_point(aes(dataset_id, 0, alpha=is.na(score))) +
+  geom_point(aes(task_id, 0, alpha=is.na(score))) +
   geom_hline(aes(yintercept=baseline, color=type), data=score_baselines) +
   coord_flip()
 
 
 
 ### Cell plotting ----------------
-# cell_plots <- map_prism(dataset_ids, function(dataset_id) {
-cell_plots <- map_local(dataset_ids, function(dataset_id) {
+cell_plots <- pmap(as.list(tasks_filtered), function(...) {
+  # task <- extract_row_to_list(tasks, 20)
+  task <- list(...)
   library(tidyverse)
-  plots <- list()
+  print(paste0("-------------", task$id))
+  print("loading")
 
-  dataset <- readRDS(dataset_file("dataset.rds", dataset_id = dataset_id))
+  task$expression <- load_expression(task$expression)
 
   source("../dynmodular/dimred_wrappers.R")
 
-  space <- dimred_mds(dataset$expression)
+  plots <- list()
 
-  plotdata_cells <- bind_cols(space %>% as.data.frame() %>% tibble::rownames_to_column("cell_id")) %>% left_join(dataset$cell_info, by="cell_id") %>% left_join(dataset$cell_grouping, by="cell_id")
-  plotdata_cells$libsize <- rowMeans(dataset$expression)
+  space <- dimred_mds(task$expression)
+
+  plotdata_cells <- bind_cols(space %>% as.data.frame() %>% tibble::rownames_to_column("cell_id")) %>% left_join(task$cell_info, by="cell_id") %>% left_join(task$prior_information$grouping_assignment, by="cell_id")
+  plotdata_cells$libsize <- rowMeans(task$expression)
 
   plotdata_groups <- plotdata_cells %>% group_by(group_id) %>% summarise(Comp1=mean(Comp1), Comp2=mean(Comp2))
 
-  plotdata_network <- dataset$milestone_network %>%
+  plotdata_network <- task$milestone_network %>%
     left_join(plotdata_groups %>% rename_at(vars(starts_with("Comp")), list(~paste0("from_", .))), by=c("from"="group_id")) %>%
     left_join(plotdata_groups %>% rename_at(vars(starts_with("Comp")), list(~paste0("to_", .))), by=c("to"="group_id"))
 
@@ -165,7 +181,7 @@ cell_plots <- map_local(dataset_ids, function(dataset_id) {
       viridis::scale_color_viridis(option="A")
   )
 
-  rm(dataset)
+  rm(task)
 
   plots
 })
@@ -174,23 +190,63 @@ cell_plots <- cell_plots %>% bind_rows()
 grid <- cowplot::plot_grid(plotlist=cell_plots %>% pull(plot_cells))
 grid
 
-save_plot("grid.pdf", grid, base_height=20, base_width=20)
-
+save_plot("grid.pdf", grid, base_height=30, base_width=30)
 
 grid <- cowplot::plot_grid(plotlist=cell_plots %>% pull(plot_libsize))
 save_plot("grid2.pdf", grid, base_height=30, base_width=30)
 
+s <- pmap(as.list(tasks_filtered), function(...) {
+  # task <- extract_row_to_list(tasks, 20)
+  task <- list(...)
+  library(tidyverse)
+  print(paste0("-------------", task$id))
+  print("loading")
+
+  task$expression <- load_expression(task$expression)
+
+  source("../dynmodular/dimred_wrappers.R")
+
+  space <- dimred_mds(task$expression)
+
+  plotdata_cells <- bind_cols(space %>% as.data.frame() %>% tibble::rownames_to_column("cell_id")) %>% left_join(task$cell_info, by="cell_id") %>% left_join(task$prior_information$grouping_assignment, by="cell_id")
+  plotdata_cells$libsize <- rowMeans(task$expression)
+
+  plotdata_groups <- plotdata_cells %>% group_by(group_id) %>% summarise(Comp1=mean(Comp1), Comp2=mean(Comp2))
+
+  plotdata_network <- task$milestone_network %>%
+    left_join(plotdata_groups %>% rename_at(vars(starts_with("Comp")), list(~paste0("from_", .))), by=c("from"="group_id")) %>%
+    left_join(plotdata_groups %>% rename_at(vars(starts_with("Comp")), list(~paste0("to_", .))), by=c("to"="group_id"))
+
+  base_scater <- ggplot(plotdata_groups, aes(Comp1, Comp2)) +
+    geom_point(aes(Comp1, Comp2), shape=21, stroke=4, size=3, alpha=0.3) +
+    geom_segment(aes(from_Comp1, from_Comp2, xend=to_Comp1, yend=to_Comp2), data=plotdata_network, arrow=arrow(), size=2, alpha=0.2) +
+    geom_point(aes(Comp1, Comp2, fill=group_id), shape=23, stroke=1, data=plotdata_groups, size=3, color="black") +
+    theme(legend.position="bottom")
+
+  plots$plot_cells <- list(
+    base_scater + geom_point(aes(Comp1, Comp2, color=group_id), data=plotdata_cells)
+  )
+  plots$plot_libsize <- list(
+    base_scater +
+      geom_point(aes(Comp1, Comp2, color=libsize), data=plotdata_cells) +
+      viridis::scale_color_viridis(option="A")
+  )
+
+  rm(task)
+
+  plots
+
 
 ### Network plotting --------------------------
 library(ggnetwork)
-network_plots <- map_local(dataset_ids_filtered, function(dataset_id) {
+network_plots <- map_local(task_ids_filtered, function(task_id) {
   library(tidyverse)
-  print(paste0("-------------", dataset_id))
+  print(paste0("-------------", task_id))
   print("loading")
-  dataset <- readRDS(dataset_file("dataset.rds", dataset_id = dataset_id))
+  task <- readRDS(dataset_file("dataset.rds", dataset_id = task_id))
 
-  milestone_network <- dataset$milestone_network
-  milestone_nodes <- dataset$cell_info %>% group_by(milestone_id) %>% summarise(n=n())
+  milestone_network <- task$milestone_network
+  milestone_nodes <- task$cell_info %>% group_by(milestone_id) %>% summarise(n=n())
 
   end_nodes <- (milestone_nodes$milestone_id %in% milestone_network$to) & !(milestone_nodes$milestone_id %in% milestone_network$from)
   start_nodes <- !(milestone_nodes$milestone_id %in% milestone_network$to) & (milestone_nodes$milestone_id %in% milestone_network$from)
@@ -236,7 +292,7 @@ network_plots <- map_local(dataset_ids_filtered, function(dataset_id) {
   )
 
   lst(
-    id = dataset_id,
+    id = task_id,
     network_plot_labeled,
     network_plot,
     network_plot_sizes,
@@ -246,33 +302,33 @@ network_plots <- map_local(dataset_ids_filtered, function(dataset_id) {
 network_plots <- network_plots %>% bind_rows()
 
 library(ggnetwork)
-network_characteristics <- parallel::mclapply(dataset_ids, function(dataset_id) {
+network_characteristics <- parallel::mclapply(task_ids, function(task_id) {
   library(tidyverse)
-  print(paste0("-------------", dataset_id))
+  print(paste0("-------------", task_id))
   print("loading")
-  dataset <- readRDS(dataset_file("dataset.rds", dataset_id = dataset_id))
+  task <- readRDS(dataset_file("dataset.rds", dataset_id = task_id))
 
   contains_cycle <- FALSE
   tryCatch(
-    {igraph::graph_from_data_frame(dataset$milestone_network) %>% igraph::topo_sort()}
+    {igraph::graph_from_data_frame(task$milestone_network) %>% igraph::topo_sort()}
     , warning=function(w) contains_cycle <<- TRUE
   )
 
-  contains_bifurcating <- dataset$milestone_network$from %>% table %>% {any(.>1)}
-  contains_convergence <- dataset$milestone_network$to %>% table %>% {any(.>1)}
+  contains_bifurcating <- task$milestone_network$from %>% table %>% {any(.>1)}
+  contains_convergence <- task$milestone_network$to %>% table %>% {any(.>1)}
 
-  lst(contains_cycle, contains_bifurcating, contains_convergence, id=dataset_id)
+  lst(contains_cycle, contains_bifurcating, contains_convergence, id=task_id)
 }, mc.cores=8) %>% bind_rows()
 network_characteristics$topology_id <- network_characteristics %>% select_if(is.logical) %>% apply(1, function(x) sum((2^seq_along(x))[x]))
 
 
-datasets <- dataset_infos %>% left_join(network_characteristics, "id") %>% left_join(network_plots, "id")
+tasks <- task_infos %>% left_join(network_characteristics, "id") %>% left_join(network_plots, "id")
 
-grid <- cowplot::plot_grid(plotlist=datasets %>% arrange(topology_id) %>% filter(standard=="gold") %>% pull(network_plot_startstop))
+grid <- cowplot::plot_grid(plotlist=tasks %>% arrange(topology_id) %>% filter(standard=="gold") %>% pull(network_plot_startstop))
 title <- ggdraw() + draw_label("Gold standard networks", fontface='bold')
 plot_grid(title, grid, ncol=1, rel_heights=c(0.1, 1)) # rel_heights values control title margins
 
 
-grid <- cowplot::plot_grid(plotlist=datasets %>% arrange(topology_id) %>% filter(standard=="silver") %>% pull(network_plot))
+grid <- cowplot::plot_grid(plotlist=tasks %>% arrange(topology_id) %>% filter(standard=="silver") %>% pull(network_plot))
 title <- ggdraw() + draw_label("Silver standard networks", fontface='bold')
 plot_grid(title, grid, ncol=1, rel_heights=c(0.1, 1)) # rel_heights values control title margins
