fde69e532fd41146779b80562d4676f148163056
Wouter Saelens <wouter.saelens@gmail.com>
2018-01-18 10:36:18 +0100

    large update to paper generation. Includes supplementary figures at the end. Fix references. Text and figure update

--- analysis/paper/update_paper_from_google_drive.R
+++ analysis/paper/update_paper_from_google_drive.R
@@ -4,8 +4,9 @@ library(rmarkdown)
 library(dynutils)
 
 drive <- drive_download(as_id("1BCCaP21N2PXfzhj9H09yEpZz9lLY2HXd_LxTSsJ_wro"), type="text/plain", overwrite=TRUE, path = tempfile())
-system(pritt("sed -i '1s/^.//' {drive$local_path}")) # remove first character, because this is some strang eunicode character
-system(pritt("cat analysis/paper/paper_base.Rmd {drive$local_path} > analysis/paper/paper.Rmd"))
+system(pritt("sed -i '1s/^.//' {drive$local_path}")) # remove first character, because this is some strange unicode character
+system(pritt("cat {drive$local_path} > analysis/paper/paper.Rmd"))
 
-render("analysis/paper/paper.Rmd", "html_document")
+file.remove("analysis/paper/paper.html")
+render("analysis/paper/paper.Rmd", "html_document", output_dir = "analysis/paper/", output_file = "paper.html")
 browseURL("analysis/paper/paper.html")
