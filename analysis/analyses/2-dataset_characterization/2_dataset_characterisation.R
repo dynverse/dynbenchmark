0bfd2f24767a8e44f79749110356c4154df6311e
Wouter Saelens <wouter.saelens@gmail.com>
2018-01-25 12:58:47 +0100

    update dataset characterisation

--- analysis/analyses/2-dataset_characterization/2_dataset_characterisation.R
+++ analysis/analyses/2-dataset_characterization/2_dataset_characterisation.R
@@ -18,8 +18,10 @@ pie <- function(tasks, what = "technology") {
     fill_scale <- scale_fill_manual(values=c("gold"="#ffeca9", "silver"="#e3e3e3"))
   } else if (what == "technology") {
     fill_scale <- scale_fill_manual(values=technology_colors)
+  } else if (what == "trajectory_type") {
+    fill_scale <- scale_fill_manual(values=trajectory_type_background_colors)
   } else {
-    fill_scale <-scale_fill_grey(start = 0.6)
+    fill_scale <- scale_fill_grey(start = 0.6, end = 0.9)
   }
 
   tasks$variable_of_interest <- tasks[[what]]
@@ -32,27 +34,41 @@ pie <- function(tasks, what = "technology") {
     mutate(start = lag(cumsum(n), 1, 0), end = cumsum(n), mid = start + (end - start)/2) %>%
     ggplot(aes(ymin=0, ymax=1)) +
     geom_rect(aes(xmin=start, xmax=end, fill=variable_of_interest), stat="identity", color="white", size=0.5) +
-    geom_text(aes(mid, odd, label = pritt("{label_short(variable_of_interest)}"), vjust=1-odd), color="black", hjust=0.5, size=5) +
-    geom_text(aes(mid, 0.5, label = pritt("{n}")), color="black", hjust=0.5, size = 5) +
+    geom_text(aes(mid, 0.5, label = pritt("{label_short(variable_of_interest, 15)}: {n}"), vjust=0.5), color="black", hjust=0.5, size=5) +
     fill_scale +
     ggtitle(label_long(what)) +
     theme_void() +
     theme(legend.position = "none") +
-    scale_x_continuous(expand=c(0.08, 0.08)) +
-    scale_y_continuous(limits=c(-0.5, 1.5))
+    scale_x_continuous(expand=c(0, 0)) +
+    scale_y_continuous(expand=c(0, 0)) +
+    coord_flip()
 }
 
-dataset_characterisation_pies <- c("technology", "organism", "standard", "trajectory_type") %>% map(pie, tasks = tasks_real) %>% cowplot::plot_grid(plotlist=., ncol =1)
+dataset_characterisation_pies <- c("technology", "organism", "standard", "trajectory_type") %>% map(pie, tasks = tasks_real) %>% cowplot::plot_grid(plotlist=., nrow =1)
 dataset_characterisation_pies
 
 
+
 ## Distributions -------------------------------------
-dataset_characterisation_distributions <- c("ncells", "ngenes") %>% map(function(what) {
-  tasks_real %>% mutate(what = map_int(normalisation_info, ~last(.$normalisation_steps[[what]]))) %>%
+tasks_real$ngenes <- map_int(tasks_real$normalisation_info, ~last(.$normalisation_steps[["ngenes"]]))
+tasks_real$ncells <- map_int(tasks_real$normalisation_info, ~last(.$normalisation_steps[["ncells"]]))
+dataset_characterisation_distributions <- c("ncells", "ngenes", "date") %>% map(function(what) {
+  nbins <- 40
+
+  if (what %in% c("ncells", "ngenes")) {
+    limits <- c(10, 10000)
+    x_scale <- scale_x_log10(label_long(what), breaks=c(10, 100, 100, 1000, 10000), limits=limits)
+    limits <- log10(limits)
+  } else {
+    limits <- c(as.Date("2014-01-01"), as.Date("2018-01-01"))
+    x_scale <- scale_x_date(label_long(what), limits = limits, date_breaks="1 year", date_labels="%Y")
+  }
+  tasks_real$what <- tasks_real[[what]]
+
+  tasks_real %>%
     ggplot(aes(what, fill=technology, color=technology)) +
-    annotation_logticks(sides = "b", color = "#333333") +
-    geom_dotplot(binwidth=0.025, method = "histodot", binpositions="all", stackgroups=TRUE, stackratio=1.5, dotsize=0.7) +
-    scale_x_log10(paste0(label_long(what), " after filtering"), breaks=c(10, 20, 50, 100, 200, 500, 100, 1000, 2000, 5000, 10000), limits=c(50, 10000)) +
+    geom_dotplot(binwidth=as.numeric(max(limits) - min(limits)) / nbins, method = "histodot", binpositions="all", stackgroups=TRUE, stackratio=1.5, dotsize=0.7) +
+    x_scale +
     scale_y_continuous(NULL, breaks=NULL, expand=c(0.02, 0)) +
     scale_fill_manual(values=technology_colors) +
     scale_color_manual(values=technology_colors) +
@@ -63,12 +79,12 @@ dataset_characterisation_distributions
 
 ## Final figure -----------------------
 dataset_characterisation_plot <- cowplot::plot_grid(
-  dataset_characterisation_pies,
   dataset_characterisation_distributions,
-  rel_heights = c(0.6, 0.35),
-  ncol=1
+  dataset_characterisation_pies,
+  rel_widths = c(0.35, 0.68),
+  ncol=2
 )
 dataset_characterisation_plot
 
 
-cowplot::save_plot(figure_file("dataset_characterisation.svg"), dataset_characterisation_plot, base_height = 6, base_width = 6)
+cowplot::save_plot(figure_file("dataset_characterisation.svg"), dataset_characterisation_plot, base_height = 6, base_width = 9)
