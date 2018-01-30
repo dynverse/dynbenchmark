1e110a9050a3b027703a8e50b041b276e9d354e8
Wouter Saelens <wouter.saelens@gmail.com>
2018-01-30 15:34:09 +0100

    move data upload download to analysis

--- scripts/data_upload_download.R
+++
@@ -1,7 +0,0 @@
-## Sync to prism
-PRISM:::rsync_remote("", "analysis/data/", "prism", "/group/irc/shared/dynalysis/analysis/data")
-PRISM:::rsync_remote("", "analysis/data/derived_data/datasets/", "prism", "/group/irc/shared/dynalysis/analysis/data/derived_data/datasets")
-
-## Download from prism
-PRISM:::rsync_remote("prism", "/group/irc/shared/dynalysis/analysis/data/", "", "analysis/data")
-PRISM:::rsync_remote("prism", "/group/irc/shared/dynalysis/analysis/data/datasets/real", "", "analysis/data/datasets/")
