c9e787a7813885210eb6b0c8b65b13258e7e2f03
Wouter Saelens <wouter.saelens@gmail.com>
2018-01-31 12:06:36 +0100

    paper statistics, including word counts (but at the same time also some readability stuff :p )

---
+++ analysis/paper/paper_stats.R
@@ -0,0 +1,28 @@
+library(wordcountaddin)
+library(xml2)
+library(tidyverse)
+
+paper <- read_html("analysis/paper/paper.html")
+body <- paper %>% xml_child("body")
+
+
+sections <- body %>%
+  xml_find_first(".//section") %>%
+  xml_find_all("./div")
+
+pieces <- tibble(
+  text = sections %>% xml_text,
+  section = sections %>% xml_attr("id")
+)
+counts <- pieces$text %>% map(wordcountaddin:::text_stats_fn_) %>% bind_rows()
+readibilities <- pieces$text %>% map(wordcountaddin:::readability_fn_)
+
+stats <-
+  bind_cols(counts, pieces) %>%
+  gather("statistic", "value", -text, -section)
+
+
+stats %>% ggplot(aes(section, value)) +
+  geom_bar(stat="identity") +
+  facet_wrap(~statistic, scales = "free") +
+  coord_flip()
