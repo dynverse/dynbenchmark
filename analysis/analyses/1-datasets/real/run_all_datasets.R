cfb165139bcea7e17454681cd103962171ee0653
Robrecht Cannoodt <brechtus@gmail.com>
2018-02-01 13:10:55 +0100

    reformat; add experiment tag

--- analysis/analyses/1-datasets/real/run_all_datasets.R
+++ analysis/analyses/1-datasets/real/run_all_datasets.R
@@ -1,18 +1,35 @@
-# Local
-dataset_scripts <- list.files(path = "analysis/analyses/1-datasets/real", pattern = "^dataset_.*\\.R", full.names = TRUE)
-
-for (scr in dataset_scripts) {
-  do.call(source, list(scr))
-}
-
-
-## Need both GEOquery and MultiAssayExperiment remotely
+library(dynalysis)
 library(PRISM)
-qsub_conf <- override_qsub_config(memory = "30G", wait=FALSE, r_module = NULL, execute_before="module unload R", stop_on_error=FALSE)
 
-handle <- qsub_lapply(dataset_scripts, function(dataset_script) {
-  source(paste0(dynalysis::get_dynalysis_folder(), "/", dataset_script))
-  TRUE
-}, qsub_environment = list2env(list()), qsub_config = qsub_conf)
-saveRDS(handle, "handle.rds")
-results <- qsub_retrieve(handle)
+experiment("1-datasets/real/run_all_datasets")
+
+# # Local
+# dataset_scripts <- list.files(path = "analysis/analyses/1-datasets/real", pattern = "^dataset_.*\\.R", full.names = TRUE)
+#
+# for (scr in dataset_scripts) {
+#   do.call(source, list(scr))
+# }
+
+## Need both GEOquery and MultiAssayExperiment installed on PRISM
+handle <- qsub_lapply(
+  X = dataset_scripts,
+  qsub_environment = list2env(list()),
+  qsub_config = override_qsub_config(
+    memory = "30G",
+    wait = FALSE,
+    r_module = NULL,
+    execute_before = "",
+    stop_on_error = FALSE
+  ),
+  qsub_packages = c("GEOquery", "MultiAssayExperiment", "tidyverse", "dynalysis"),
+  FUN = function(dataset_script) {
+    setwd("/group/irc/shared/dynalysis/")
+    source(paste0(dynalysis::get_dynalysis_folder(), "/", dataset_script))
+    TRUE
+  }
+)
+
+write_rds(handle, derived_file("handle.rds"))
+
+results <- qsub_retrieve(read_rds(derived_file("handle.rds")))
+file.remove("handle.rds")
