eac0c1b52e9d36245d92291aa5be90c344c15bd3
Wouter Saelens <wouter.saelens@gmail.com>
2018-02-01 17:42:11 +0100

    update trajectory types tree

--- analysis/analyses/2-dataset_characterisation/trajectory_types.R
+++ analysis/analyses/2-dataset_characterisation/trajectory_types.R
@@ -1,4 +1,5 @@
 library(dynalysis)
+library(tidygraph)
 library(ggraph)
 library(tidyverse)
 
@@ -7,11 +8,16 @@ experiment("dataset_characterisation/trajectory_types")
 trajectory_type_tree <- trajectory_type_dag %>%
   activate(nodes) %>%
   filter(name != "unknown") %>%
+  mutate_at(vars(directedness), label_long) %>%
+  activate(edges) %>%
+  mutate_at(vars(type), label_long) %>%
   ggraph(layout = "tree") +
-  geom_edge_link() +
-  geom_edge_link(aes(xend = x+(xend-x)/2, yend = y+(yend - y)/2, filter = ), arrow=arrow(type="closed")) +
+  geom_edge_link(aes(edge_linetype = type)) +
+  geom_edge_link(aes(xend = x+(xend-x)/2, yend = y+(yend - y)/2, edge_linetype = type), arrow=arrow(type="closed", length=unit(0.1, "inches"))) +
   geom_node_label(aes(label=label_long(name), fill=directedness)) +
-  theme_graph()
-
+  scale_edge_linetype_manual(values=c(Directedness = "dotted", Generalisation = "solid")) +
+  theme_graph() +
+  theme(legend.position="top")
+trajectory_type_tree
 
 trajectory_type_tree %>% write_rds(figure_file("trajectory_type_tree.rds"))
