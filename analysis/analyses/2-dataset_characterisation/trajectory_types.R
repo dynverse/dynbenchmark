f674fa1a106ea14fdf3ef2effcb315101b27e677
Wouter Saelens <wouter.saelens@gmail.com>
2018-02-02 10:34:40 +0100

    added trajectory type tree plotting, added prop changestree, added binary_tree

--- analysis/analyses/2-dataset_characterisation/trajectory_types.R
+++ analysis/analyses/2-dataset_characterisation/trajectory_types.R
@@ -5,19 +5,73 @@ library(tidyverse)
 
 experiment("dataset_characterisation/trajectory_types")
 
-trajectory_type_tree <- trajectory_type_dag %>%
+
+
+#   ____________________________________________________________________________
+#   Trajectory type trees                                                   ####
+
+trajectory_type_tree_data <- trajectory_type_dag %>%
   activate(nodes) %>%
   filter(name != "unknown") %>%
-  mutate_at(vars(directedness), label_long) %>%
   activate(edges) %>%
-  mutate_at(vars(type), label_long) %>%
-  ggraph(layout = "tree") +
-  geom_edge_link(aes(edge_linetype = type)) +
-  geom_edge_link(aes(xend = x+(xend-x)/2, yend = y+(yend - y)/2, edge_linetype = type), arrow=arrow(type="closed", length=unit(0.1, "inches"))) +
-  geom_node_label(aes(label=label_long(name), fill=directedness)) +
-  scale_edge_linetype_manual(values=c(Directedness = "dotted", Generalisation = "solid")) +
-  theme_graph() +
-  theme(legend.position="top")
-trajectory_type_tree
+  mutate(
+    directed_change = map_lgl(prop_changes, ~"undirected" %in% .),
+    directed_change = ifelse(directed_change,"Add directed", "Other generalisation")
+  )
 
-trajectory_type_tree %>% write_rds(figure_file("trajectory_type_tree.rds"))
+
+##  ............................................................................
+##  Changes                                                                 ####
+label_prop_changes <- function(prop_changes) {
+  format_prop_changes <- function(prop_changes) {
+    gsub("==", "=", prop_changes) %>% gsub("num", "number_of", .) %>% label_long()
+  }
+  map(prop_changes, format_prop_changes) %>% map_chr(glue::collapse, "\n")
+}
+
+trajectory_type_tree_changes <- trajectory_type_tree_data %>%
+  activate(nodes) %>%
+  filter(directedness == "undirected") %>%
+  ggraph(layout = "tree") +
+  geom_edge_link() +
+  geom_edge_link(aes(xend = x+(xend-x)/1.3, yend = y+(yend - y)/1.3), arrow=arrow(type="closed", length=unit(0.1, "inches"))) +
+  geom_node_label(aes(label=label_long(name) %>% gsub(" ", "\n", .), fill=name)) +
+  ggrepel::geom_label_repel(aes(x=x+(xend-x)/2, y = y+(yend - y)/2, label=label_prop_changes(prop_changes)), data = get_edges(), min.segment.length=Inf) +
+  scale_fill_manual(values=set_names(trajectory_types$color, trajectory_types$id)) +
+  theme_graph() +
+  theme(legend.position="none") +
+  scale_x_continuous(expand=c(0.2, 0.2))
+trajectory_type_tree_changes
+
+trajectory_type_tree_changes %>% write_rds(figure_file("trajectory_type_tree_changes.rds"))
+
+
+##  ............................................................................
+##  Overall tree                                                            ####
+trajectory_type_tree_overall <- trajectory_type_tree_data %>%
+  ggraph(layout = "tree") +
+  geom_edge_link(aes(edge_linetype = directed_change)) +
+  geom_edge_link(aes(xend = x+(xend-x)/2, yend = y+(yend - y)/2, edge_linetype = directed_change), arrow=arrow(type="closed", length=unit(0.1, "inches"))) +
+  geom_node_label(aes(label=label_long(name) %>% gsub(" ", "\n", .), fill=directedness)) +
+  theme_graph() +
+  theme(legend.position="top") +
+  scale_x_continuous(expand=c(0.2, 0.2)) +
+  scale_fill_discrete("", labels=label_long) +
+  scale_edge_linetype_discrete("", labels=label_long)
+trajectory_type_tree_overall
+
+trajectory_type_tree_overall %>% write_rds(figure_file("trajectory_type_tree_overall.rds"))
+
+
+
+
+##  ............................................................................
+##  Combined tree plot                                                      ####
+trajectory_type_trees <- cowplot::plot_grid(
+  trajectory_type_tree_changes,
+  trajectory_type_tree_overall,
+  ncol=2,
+  labels="auto"
+)
+trajectory_type_trees
+write_rds(trajectory_type_trees, figure_file("trajectory_type_trees.rds"))
