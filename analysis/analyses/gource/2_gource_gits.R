d9c6d0def5f737a934a6d319ab243f8ddb9925fa
Robrecht Cannoodt <robrecht.cannoodt@ugent.be>
2018-02-04 04:29:36 +0100

    uniformise code

--- analysis/analyses/gource/2_gource_gits.R
+++ analysis/analyses/gource/2_gource_gits.R
@@ -8,34 +8,42 @@ output_folder <- derived_file("git_output/")
 
 dir.create(output_folder)
 
-gitmodules <- readr::read_lines("../.gitmodules") %>%
+git_df <-
+  readr::read_lines("../.gitmodules") %>%
   keep(~ grepl("submodule", .)) %>%
-  gsub(".*\"(.*)\".*", "\\1", .)
+  gsub(".*\"(.*)\".*", "\\1", .) %>%
+  data_frame(path = ., prefix = paste0("/", .), filename = str_replace(., "/", "_"), gitmodule = TRUE) %>%
+  add_row(path = "..", prefix = "", filename = "dynverse", gitmodule = FALSE) %>%
+  add_row(path = "../../dyndocs", prefix = "/dyndocs", filename = "dyndocs", gitmodule = FALSE)
 
 unlink(gitlinks_folder, recursive = T)
-for (gitmodule in gitmodules) {
+for (i in seq_len(nrow(git_df))) {
+  path <- git_df$path[[i]]
+  prefix <- git_df$prefix[[i]]
+  filename <- git_df$filename[[i]]
+  gitmodule <- git_df$gitmodule[[i]]
+
   gource_output <- paste0(
     output_folder, "/",
-    gsub("/", "_", gitmodule),
+    filename,
     ".txt"
   )
 
-  dir.create(paste0(gitlinks_folder, gitmodule), recursive = T, showWarnings = F)
-  file.symlink(
-    normalizePath(paste0("../.git/modules/", gitmodule)),
-    paste0(gitlinks_folder, gitmodule, "/.git")
-  )
-  cmd <- paste0("gource --output-custom-log ", gource_output, " ", gitlinks_folder, gitmodule)
+  if (gitmodule) {
+    dir.create(paste0(gitlinks_folder, path), recursive = T, showWarnings = F)
+    file.symlink(
+      normalizePath(paste0("../.git/modules/", path)),
+      paste0(gitlinks_folder, path, "/.git")
+    )
+    git_location <- paste0(gitlinks_folder, path)
+  } else {
+    git_location <- path
+  }
+  cmd <- paste0("gource --output-custom-log ", gource_output, " ", git_location)
   system(cmd)
 
   read_lines(gource_output) %>%
-    gsub("(\\|[AMD]\\|)", paste0("\\1/", gitmodule), .) %>%
+    str_replace("\\|/", paste0("|", prefix, "/")) %>%
     write_lines(gource_output)
 }
 unlink(gitlinks_folder, recursive = T)
-
-cmd <- paste0("gource --output-custom-log ", output_folder, "/dynverse.txt ..")
-system(cmd)
-
-cmd <- paste0("gource --output-custom-log ", output_folder, "/dyndocs.txt ../../dyndocs")
-system(cmd)
