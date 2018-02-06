379dd49b86bd4a9428d5cc725116e9fa4c083b8f
Wouter Saelens <wouter.saelens@gmail.com>
2018-02-06 10:16:09 +0100

    fix paper labelling sufixes

--- analysis/paper/setup.R
+++ analysis/paper/setup.R
@@ -22,7 +22,7 @@ ref <- function(ref_type, ref_id, suffix="", anchor=FALSE) {
   }
   ref <- refs %>% filter(ref_id == !!ref_id) %>%
     pull(name) %>%
-    {pritt("[**{.}{suffix}**](#{ref_type}_{ref_id})")}
+    {pritt("[**{.}{glue::collapse(suffix, ',')}**](#{ref_type}_{ref_id})")}
 
   if (anchor)  ref <- pritt("{ref}{anchor(ref_type, ref_id)}")
   ref
@@ -31,11 +31,11 @@ anchor <- function(ref_type, ref_id) {
   pritt("<a name='{ref_type}_{ref_id}'></a>")
 }
 create_names <- list(
-  sfig = function(i) pritt("Supplementary Figure {glue::collapse(i, ',')}"),
-  fig = function(i) pritt("Figure {glue::collapse(i, ',')}"),
-  snote = function(i) pritt("Supplementary Note {glue::collapse(i, ',')}"),
-  table = function(i) pritt("Table {glue::collapse(i, ',')}"),
-  stable = function(i) pritt("Supplementary Table {glue::collapse(i, ',')}")
+  sfig = function(i) pritt("Supplementary Figure {i}"),
+  fig = function(i) pritt("Figure {i}"),
+  snote = function(i) pritt("Supplementary Note {i}"),
+  table = function(i) pritt("Table {i}"),
+  stable = function(i) pritt("Supplementary Table {i}")
 )
 sfigs <- tibble(ref_id = character(), fig = list(), caption = character(), width=numeric(), height=numeric())
 add_sfig <- function(fig, ref_id, caption, width=15, height=10) {
