664c13abefb7dc1d50f92f48c926fb5c8d2f9965
Wouter Saelens <wouter.saelens@gmail.com>
2018-02-07 16:18:31 +0100

    add user input non inclusion reason

--- R/common_non_inclusion_reasons.R
+++ R/common_non_inclusion_reasons.R
@@ -10,5 +10,6 @@ non_inclusion_reasons <- tribble(
   "unwrappable", "Unresolved errors during wrapping",
   "speed", "Too slow (requires more than one hour on a 100x100 dataset)",
   "no_ordering", "Doesn't return an ordering",
+  "user_input", "Requires additional user input during the algorithm (not prior information)",
   "date", "Published later than 2017-05-01 to be included in the current version of the evaluation"
 ) %>% mutate(footnote = seq_along(id))
