b1e3664c56df49abc3be6069a60e0624df400830
Robrecht Cannoodt <brechtus@gmail.com>
2018-02-09 16:54:43 +0100

    update scripts and figures

---
+++ analysis/analyses/5-optimise_parameters/3a-evaluate_parameters-submit_jobs.R
@@ -0,0 +1,49 @@
+library(dynalysis)
+library(tidyverse)
+library(dynplot)
+
+experiment("5-optimise_parameters/3-evaluate_parameters")
+
+# settings
+methods <- get_descriptions()
+metrics <- c("correlation", "rf_mse", "edge_flip")
+extra_metrics <- c()
+eval_timeout <- 60 * 60
+optim_timeout <- 7 * 24 * 60 * 60
+num_repeats <- 4
+
+# read tasks
+tasks <- read_rds(derived_file("tasks.rds", "5-optimise_parameters/0-process_tasks")) %>%
+  mutate(nrow = map_int(expression, nrow), ncol = map_int(expression, ncol)) %>%
+  filter(nrow < 2000)
+
+# extract the default parameters
+designs <- lapply(methods$short_name, function(mn) {
+  par_set <- methods %>% filter(short_name == mn) %>% .$par_set %>% .[[1]]
+  defaults <- ParamHelpers::generateDesignOfDefaults(par_set)
+  # best <- ...
+  # bind_rows(defaults, best)
+  defaults
+}) %>% setNames(methods$short_name)
+
+# save benchmark configuration and start it
+write_rds(lst(methods, designs, metrics, extra_metrics, num_repeats, tasks), derived_file("config.rds"))
+benchmark_suite_submit(
+  tasks = tasks,
+  task_group = tasks$task_group,
+  task_fold = rep(1, nrow(tasks)),
+  out_dir = derived_file("suite/"),
+  remote_dir = paste0("/scratch/irc/shared/dynverse_derived/", getOption("dynalysis_experiment_id"), "/"),
+  methods = methods,
+  designs = designs,
+  metrics = metrics,
+  extra_metrics = extra_metrics,
+  memory = "10G",
+  num_cores = 4,
+  num_iterations = 1,
+  num_repeats = num_repeats,
+  num_init_params = num_init_params,
+  execute_before = "source /scratch/irc/shared/dynverse/module_load_R.sh; export R_MAX_NUM_DLLS=500",
+  r_module = NULL,
+  output_model = TRUE
+)
