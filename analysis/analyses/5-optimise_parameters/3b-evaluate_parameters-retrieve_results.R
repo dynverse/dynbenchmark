30da0fe65e7245fcddb0580708f09cb1c91c9529
Robrecht Cannoodt <brechtus@gmail.com>
2018-02-15 18:25:58 +0100

    update scripts and figures

--- analysis/analyses/5-optimise_parameters/3b-evaluate_parameters-retrieve_results.R
+++ analysis/analyses/5-optimise_parameters/3b-evaluate_parameters-retrieve_results.R
@@ -8,10 +8,15 @@ experiment("5-optimise_parameters/3-evaluate_parameters")
 ##########################################################
 
 # fetch results from cluster
-bs_fetch_results(derived_file("suite/"))
+paramoptim_fetch_results(derived_file("suite/"))
 
 # bind results in one data frame (without models)
-outputs <- bs_bind_results(derived_file("suite/"), load_models = FALSE)
+outputs <- paramoptim_bind_results(derived_file("suite/"), load_models = FALSE)
+
+# load tasks info
+list2env(read_rds(derived_file("config.rds")), environment())
+tasks_info <- tasks %>% select(task_id = id, type, trajectory_type, task_source)
+rm(tasks)
 
 # print task errors
 task_errors <- outputs %>%
@@ -37,26 +42,24 @@ print(job_errors)
 write_tsv(job_errors, figure_file("errors_method.tsv"))
 
 required_outputs <- nrow(tasks_info) * num_repeats
-outputs %>% filter(!is.na(task_id)) %>% group_by(method_name) %>% summarise(n = n()) %>% filter(n != required_outputs)
+outputs %>% filter(!is.na(task_id)) %>% group_by(method_name) %>% summarise(n = n()) %>% filter(n != required_outputs) %>% mutate(pass = n > .9 * required_outputs)
 
 ###################################################
 ############### CREATE AGGREGATIONS ###############
 ###################################################
-list2env(read_rds(derived_file("config.rds")), environment())
-tasks_info <- tasks %>% select(task_id = id, type, trajectory_type, task_source)
-rm(tasks)
+
 
 outputs_ind <- outputs %>%
   filter(!is.na(task_id)) %>%
   group_by(method_name) %>%
-  filter(n() == required_outputs) %>%
+  filter(n() > .9 * required_outputs) %>%
   ungroup() %>%
   left_join(tasks_info, by = "task_id") %>%
   mutate(
     param_group = c("default", "optimised")[param_i],
     pct_errored = (error_message != "") + 0,
     prior_str = sapply(prior_df, function(prdf) ifelse(is.null(prdf) || nrow(prdf) == 0, "", paste(prdf$prior_names, collapse = ";"))),
-    trajectory_type_f = factor(trajectory_type, levels = intersect(unique(trajectory_type), dynalysis::trajectory_types$id))
+    trajectory_type_f = factor(trajectory_type, levels = intersect(dynalysis::trajectory_types$id, unique(trajectory_type)))
   ) %>%
   group_by(task_id) %>%
   mutate(
@@ -128,38 +131,38 @@ outputs_summtrajtype_totalsx2 <- bind_rows(
   outputs_summmethod_totals %>% mutate(trajectory_type = "overall"),
   outputs_summtrajtype_totals
 ) %>%
-  mutate(trajectory_type_f = factor(trajectory_type, levels = c("overall", intersect(unique(trajectory_type), dynalysis::trajectory_types$id))))
+  mutate(trajectory_type_f = factor(trajectory_type, levels = c("overall", intersect(dynalysis::trajectory_types$id, unique(trajectory_type)))))
 
 # save data structures
 to_save <- environment() %>% as.list()
 to_save <- to_save[str_detect(names(to_save), "^outputs_")]
 write_rds(to_save, derived_file("outputs_postprocessed.rds"))
 
-# Upload ---------------------
-PRISM:::rsync_remote(
-  remote_dest = "prism",
-  path_dest = paste0("/group/irc/shared/dynalysis/analysis/data/derived_data/", getOption("dynalysis_experiment_id"), "/"),
-  remote_src = "",
-  path_src = derived_file("config.rds")
-)
-PRISM:::rsync_remote(
-  remote_dest = "prism",
-  path_dest = paste0("/group/irc/shared/dynalysis/analysis/data/derived_data/", getOption("dynalysis_experiment_id"), "/"),
-  remote_src = "",
-  path_src = derived_file("outputs_postprocessed.rds")
-)
-method_names <- list.dirs(derived_file("suite"), recursive = FALSE, full.names = FALSE)
-for (method_name in method_names) {
-  cat("Syncing method output for ", method_name, "\n", sep = "")
-  path_dest <- paste0("/group/irc/shared/dynalysis/analysis/data/derived_data/", getOption("dynalysis_experiment_id"), "/suite/", method_name, "/")
-
-  if (length(list.files(paste0(derived_file("suite/"), method_name, "/"), pattern = "output_*")) > 0) {
-    PRISM:::mkdir_remote(path_dest, remote = "prism")
-    PRISM:::rsync_remote(
-      remote_dest = "prism",
-      path_dest = path_dest,
-      remote_src = "",
-      path_src = paste0(derived_file("suite/"), method_name, "/output_*.rds")
-    )
-  }
-}
+# # Upload ---------------------
+# PRISM:::rsync_remote(
+#   remote_dest = "prism",
+#   path_dest = paste0("/group/irc/shared/dynalysis/analysis/data/derived_data/", getOption("dynalysis_experiment_id"), "/"),
+#   remote_src = "",
+#   path_src = derived_file("config.rds")
+# )
+# PRISM:::rsync_remote(
+#   remote_dest = "prism",
+#   path_dest = paste0("/group/irc/shared/dynalysis/analysis/data/derived_data/", getOption("dynalysis_experiment_id"), "/"),
+#   remote_src = "",
+#   path_src = derived_file("outputs_postprocessed.rds")
+# )
+# method_names <- list.dirs(derived_file("suite"), recursive = FALSE, full.names = FALSE)
+# for (method_name in method_names) {
+#   cat("Syncing method output for ", method_name, "\n", sep = "")
+#   path_dest <- paste0("/group/irc/shared/dynalysis/analysis/data/derived_data/", getOption("dynalysis_experiment_id"), "/suite/", method_name, "/")
+#
+#   if (length(list.files(paste0(derived_file("suite/"), method_name, "/"), pattern = "output_*")) > 0) {
+#     PRISM:::mkdir_remote(path_dest, remote = "prism")
+#     PRISM:::rsync_remote(
+#       remote_dest = "prism",
+#       path_dest = path_dest,
+#       remote_src = "",
+#       path_src = paste0(derived_file("suite/"), method_name, "/output_*.rds")
+#     )
+#   }
+# }
