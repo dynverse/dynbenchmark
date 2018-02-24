547af5019fc917a2c9412613f731164027fc5fb6
Robrecht Cannoodt <brechtus@gmail.com>
2018-02-24 21:45:23 +0100

    debug methods

--- analysis/analyses/5-optimise_parameters/3d-debug_runs.R
+++ analysis/analyses/5-optimise_parameters/3d-debug_runs.R
@@ -9,17 +9,36 @@ experiment("5-optimise_parameters/3-evaluate_parameters")
 list2env(read_rds(derived_file("outputs_postprocessed.rds")), environment())
 list2env(read_rds(derived_file("config.rds")), environment())
 
-model_short_name <- "scoup"
-debug_out <- outputs_ind %>% filter(method_short_name == model_short_name)
+tasks <- map_df(paste0(local_tasks_folder, "/", task_ids, ".rds"), read_rds)
 
-i <- 1
+
+# msn <- "tscan"
+msn <- "mnclddr"
+debug_out <- outputs_ind %>% filter(method_short_name == msn) %>% arrange(desc(rank_correlation)) %>% filter(repeat_i == 1)
+
+i <- 4
 model_id <- debug_out$model_id[[i]]
 task_id <- debug_out$task_id[[i]]
 
-model <- load_dyneval_model(method_short_name = model_short_name, model_id = model_id)[[1]]
+model <- load_dyneval_model(method_short_name = msn, model_id = model_id)[[1]]
 task <- tasks %>% filter(id == task_id) %>% extract_row_to_list(1)
 
-plot_default(task)
-plot_default(model)
+wp_cells <- unique(c(model$waypoint_cells, task$waypoint_cells))
+task_geo <- dynutils::compute_tented_geodesic_distances(task, wp_cells)
+model_geo <- dynutils::compute_tented_geodesic_distances(model, wp_cells)
 
-model$milestone_percentages
+if (task$task_source != "real") {
+  ggplot() + geom_jitter(aes(as.vector(task_geo), as.vector(model_geo)), width = .05, height = 0.05, alpha = .5) + cowplot::theme_cowplot()
+} else {
+  ggplot() + geom_violin(aes(factor(as.vector(task_geo)), as.vector(model_geo))) + cowplot::theme_cowplot()
+}
+
+cowplot::plot_grid(
+  plot_default(task),
+  plot_default(model),
+  plot_combined(model, task),
+  plot_combined(task, model),
+  ncol = 2
+)
+
+plot_strip_connections(task, model)
