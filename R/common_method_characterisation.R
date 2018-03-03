77f6fe2c2bbaada36247f6e589afa08dbfc91a3e
Robrecht Cannoodt <brechtus@gmail.com>
2018-03-03 08:46:37 +0100

    add error type colours to package; clean scripts

--- R/common_method_characterisation.R
+++ R/common_method_characterisation.R
@@ -9,3 +9,12 @@ topinf <- tibble(
 #' Topology inference colours
 #' @export
 topinf_colours <- setNames(topinf$colour, topinf$name)
+
+#' Errors
+#' @export
+#' @importFrom RColorBrewer brewer.pal
+errors <- tibble(
+  name = c("pct_memory_exceeded", "pct_time_exceeded", "pct_allerrored", "pct_stochastic"),
+  label = c("Memory limit exceeded", "Time limit exceeded", "Dataset-specific error", "Stochastic error"),
+  colour = RColorBrewer::brewer.pal(4, "Set3")
+)
