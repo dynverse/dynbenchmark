100a21f7a8403acf8b28bac95a07666df0962d61
Wouter Saelens <wouter.saelens@gmail.com>
2018-03-05 17:25:15 +0100

    small update

--- analysis/analyses/8-topology_detection/3-topology_complexity.R
+++ analysis/analyses/8-topology_detection/3-topology_complexity.R
@@ -173,3 +173,4 @@ topology_complexity_comparison <- plot_grid(
 
 topology_complexity_comparison
 topology_complexity_comparison %>% ggsave(figure_file("topology_complexity_comparison.svg"), ., width=12, height=8)
+
