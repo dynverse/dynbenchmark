b79c59d9591e92eeb06d8d70b5c01269bec7783e
Wouter Saelens <wouter.saelens@gmail.com>
2018-03-05 11:50:47 +0100

    remove not published non inclusion reason

--- R/common_non_inclusion_reasons.R
+++ R/common_non_inclusion_reasons.R
@@ -11,6 +11,5 @@ non_inclusion_reasons <- tribble(
   "speed", "Too slow (requires more than one hour on a 100x100 dataset)",
   "no_ordering", "Doesn't return an ordering",
   "user_input", "Requires additional user input during the algorithm (not prior information)",
-  "date", "Published later than 2017-05-01 to be included in the current version of the evaluation",
-  "not_published", "This method is not published in preprint or a peer-reviewed journal"
+  "date", "Published later than 2017-05-01 to be included in the current version of the evaluation"
 ) %>% mutate(footnote = letters[seq_along(id)])
