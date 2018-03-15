6d520a0b3ec52852a26c5cb47887573fcdf768e8
Robrecht Cannoodt <brechtus@gmail.com>
2018-03-15 11:15:59 +0100

    remove metrics comparison again

--- analysis/analyses/11-evaluation_robustness/create_figure.R
+++ analysis/analyses/11-evaluation_robustness/create_figure.R
@@ -5,10 +5,10 @@ experiment("11-evaluation_robustness")
 
 robustness_overview <- cowplot::plot_grid(
   read_rds(figure_file("real_synthetic_comparison.rds")),
-  read_rds(figure_file("metrics_comparison.rds")),
+  # read_rds(figure_file("metrics_comparison.rds")),
   read_rds(figure_file("performance_dataset_variability_combined.rds")),
   ncol = 1,
   labels = "auto",
-  rel_heights = c(1, 2, 2)
+  rel_heights = c(1,  2)
 )
-ggsave(figure_file("robustness.svg"), robustness_overview, width = 16, height = 14)
+ggsave(figure_file("robustness.svg"), robustness_overview, width = 16, height = 10)
