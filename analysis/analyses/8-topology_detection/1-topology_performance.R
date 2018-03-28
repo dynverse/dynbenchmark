dcf4aa070d6fe743a93ccd9f139395260136ac0e
Wouter Saelens <wouter.saelens@gmail.com>
2018-03-28 17:12:32 +0200

    major topology performance update

--- analysis/analyses/8-topology_detection/1-topology_performance.R
+++ analysis/analyses/8-topology_detection/1-topology_performance.R
@@ -7,69 +7,24 @@ library(ggraph)
 
 experiment("8-compare_topology")
 
-methods <- read_rds(derived_file("methods.rds", experiment_id = "4-method_characterisation"))
-outputs_list <- read_rds(derived_file("outputs_postprocessed.rds", "5-optimise_parameters/3-evaluate_parameters"))
+read_rds(derived_file("evaluation_algorithm.rds", "5-optimise_parameters/10-aggregations")) %>% list2env(.GlobalEnv)
 
-overall_scores <- outputs_list$outputs_summmethod_totals %>% filter(task_source=="mean") %>% rename(method_id = method_short_name)
-trajtype_scores <- outputs_list$outputs_summtrajtype_totals %>% filter(task_source=="mean") %>% rename(method_id = method_short_name)
-indrep_scores <- outputs_list$outputs_summrepl %>%
-  rename(method_id = method_short_name) %>%
-  filter(method_id %in% methods$method_id)
-ind_scores <- outputs_list$outputs_ind %>%
-  rename(method_id = method_short_name) %>%
-  filter(method_id %in% methods$method_id)
-method_order <- outputs_list$outputs_summmethod_totals %>%
-  rename(method_id = method_short_name) %>%
-  filter(task_source=="mean") %>%
-  arrange(-harm_mean) %>%
-  filter(method_id %in% methods$method_id) %>%
-  pull(method_id)
-
-label_facet_methods <- function(x) {tibble(method_id=methods$method_name[match(x$method_id, methods$method_id)])}
-
-
-##  ............................................................................
-##  Topology freedom tesjtjes                                               ####
-#
-# method_topology_freedom <- methods %>%
-#   gather(trajectory_type, can_handle_trajectory_type, !!trajectory_types$id[trajectory_types$directedness == "directed"]) %>%
-#   inner_join(overall_scores, "method_id") %>%
-#   filter(can_handle_trajectory_type)
-#
-# method_topology_freedom <- methods %>%
-#   inner_join(overall_scores, "method_id") %>%
-#   rename(trajectory_type = maximal_trajectory_type)
-#
-#
-# method_topology_freedom %>%
-#   left_join(trajectory_types, by=c("trajectory_type"="id")) %>%
-#   mutate(trajectory_type = factor(trajectory_type, trajectory_types$id)) %>%
-#   gather("score_id", "score", harm_mean, rank_edge_flip) %>%
-#   ggplot(aes(trajectory_type, score)) +
-#   geom_violin(aes(color=color, fill=background_color)) +
-#   geom_point(aes(color=color)) +
-#   scale_color_identity() +
-#   scale_fill_identity() +
-#   scale_x_discrete(label_long("trajectory_type"), labels=label_long) +
-#   scale_y_continuous(label_long("rank_edge_flip")) +
-#   facet_wrap(~score_id)
-
-
+label_facet_methods <- function(x) {tibble(method_short_name=methods$method_name[match(x$method_short_name, methods$method_short_name)])}
 
 ##  ............................................................................
 ##  Performance on traj type vs can handle traj type                        ####
 method_trajtypes <- methods %>%
   gather(trajectory_type, can_handle_trajectory_type, !!trajectory_types$id[trajectory_types$directedness == "directed"])
 
-scores <- c("harm_mean", "rank_edge_flip", "rank_correlation", "rank_rf_mse")
+scores <- c("harm_mean", "norm_edge_flip", "norm_correlation", "norm_rf_mse")
 
 method_trajtypes_scores <- left_join(
   trajtype_scores,
   method_trajtypes,
-  c("method_id", "trajectory_type")
+  c("method_short_name", "trajectory_type")
 ) %>%
   mutate(trajectory_type = factor(trajectory_type, trajectory_types$id)) %>%
-  select(method_id, trajectory_type, can_handle_trajectory_type, !!scores) %>%
+  select(method_short_name, trajectory_type, can_handle_trajectory_type, !!scores) %>%
   drop_na() %>%
   gather(score_id, score_value, !!scores)
 
@@ -90,7 +45,7 @@ trajtype_handle_comparison <- method_trajtypes_scores %>%
     aes(
       can_handle_trajectory_type,
       score_value,
-      label= label_short(set_names(methods$method_name, methods$method_id)[method_id], 10),
+      label= label_short(set_names(methods$method_name, methods$method_short_name)[method_short_name], 10),
       color = get_violin_color(trajectory_type, can_handle_trajectory_type)
     ),
     method_trajtypes_scores %>%
@@ -114,11 +69,9 @@ trajtype_handle_comparison %>% ggsave(figure_file("trajtype_handle_comparison.sv
 
 ##  ............................................................................
 ##  Balance between ordering and topology                                   ####
-
-scores <- c("harm_mean", "rank_edge_flip", "rank_correlation", "rank_rf_mse")
+scores <- c("harm_mean", "norm_edge_flip", "norm_correlation", "norm_rf_mse")
 # scores <- c("harm_mean", "edge_flip", "correlation", "rf_mse")
 linear_split_scores <- indrep_scores %>%
-  left_join(methods, by="method_id") %>%
   mutate(
     is_linear_restricted = ifelse(maximal_trajectory_type %in% c("directed_cycle", "directed_linear"), "linear_method", "non-linear_method"),
     trajectory_type_linear = ifelse(trajectory_type %in% c("directed_cycle", "directed_linear"), "linear_dataset", "more_complex_than_linear_dataset")
@@ -166,10 +119,10 @@ ggsave(figure_file("linear_split_comparison.svg"), linear_split_comparison, widt
 ##  Edge flip distributions                                                 ####
 bw = 0.05
 edge_flip_distributions <- ind_scores %>%
-  filter(method_id %in% method_order) %>%
-  mutate(method_id = factor(method_id, rev(method_order))) %>%
+  filter(method_short_name %in% method_order) %>%
+  mutate(method_short_name = factor(method_short_name, rev(method_order))) %>%
   mutate(trajectory_type = factor(trajectory_type, trajectory_types$id)) %>%
-  ggplot(aes(edge_flip, method_id)) +
+  ggplot(aes(edge_flip, method_short_name)) +
   ggridges::geom_density_ridges2(
     aes(fill=trajectory_type),
     stat="density_ridges",
@@ -180,7 +133,7 @@ edge_flip_distributions <- ind_scores %>%
   ) +
   scale_fill_manual(values=set_names(trajectory_types$color, trajectory_types$id)) +
   facet_grid(.~trajectory_type, labeller=label_facet(label_simple_trajectory_types)) +
-  scale_y_discrete(label_long("method_id"), expand = c(0, 0), labels=set_names(methods$method_name, methods$method_id)) +
+  scale_y_discrete(label_long("method_short_name"), expand = c(0, 0), labels=set_names(methods$method_name, methods$method_short_name)) +
   scale_x_continuous(label_long("edge_flip"), expand = c(0, 0), breaks=c(0, 0.5, 1), label=round) +
   theme(legend.position = "none", axis.text.y=element_text(vjust=0))
 edge_flip_distributions
