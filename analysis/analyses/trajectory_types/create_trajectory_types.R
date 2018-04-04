fb810a18ab2a173b6c14948cccacf13c9d1eb7b1
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-04 13:34:14 +0200

    clean up trajectory types script and minis

--- analysis/analyses/trajectory_types/create_trajectory_types.R
+++ analysis/analyses/trajectory_types/create_trajectory_types.R
@@ -7,72 +7,30 @@ experiment("trajectory_types")
 library(xml2)
 
 xml_find_multiple_ids <- function(xml, ids) {
-  ids_txt <- paste0("@id = \'", ids, "\'", collapse=" or ")
-  xpath <- glue("//svg:g[{ids_txt}]")
+  ids_txt <- paste0(glue::glue("@id = \'"), ids, "\'", collapse=" or ")
+  xpath <- glue::glue(".//svg:g[{ids_txt}]")
+
   xml_find_all(xml, xpath)
 }
 
-change_opacity <- function(
-  boxes,
-  output,
-  svg_location,
-  folder = ".",
-  verbose = FALSE,
-  size = "100%",
-  identifier = "g",
-  attribute = "id"
-) {
-  if (!("opacity" %in% colnames(boxes))) {
-    stop("Need opacity")
-  }
+xml <- read_xml(figure_file("trajectory_types.svg"))
 
-  xml <- read_xml(svg_location)
+# Check traj types are in the xml
+trajtype_ids <- xml %>% xml_find_multiple_ids(trajectory_types$id) %>% xml_attr("id")
 
-  xml_find_multiple_ids <- function(xml, ids) {
-    ids_txt <- paste0(glue::glue("@{attribute} = \'"), ids, "\'", collapse=" or ")
-    xpath <- glue::glue(".//svg:{identifier}[{ids_txt}]")
+walk(trajtype_ids, function(id) {
+  mini_name <- figure_file(glue::glue("mini/{id}.svg"))
 
-    print(xpath)
-    xml_find_all(xml, xpath)
-  }
+  xml_mini <- read_xml(figure_file("trajectory_types.svg"))
 
-  walk(boxes %>% split(boxes$opacity), function(x) {
-    nodes <- xml_find_multiple_ids(
-      xml,
-      x$id
-    )
-    xml_attr(nodes, "style") <- glue::glue("opacity:{x$opacity[[1]]};")
+  remove_ids <- setdiff(trajtype_ids, id) %>% c("layer2", "layer3")
 
-    if (x$opacity[[1]] == 0) {
-      xml_remove(nodes)
-    }
-  })
+  xml_mini %>%
+    xml_find_multiple_ids(remove_ids) %>%
+    xml_remove()
 
-  write(as.character(xml), file=glue::glue("{folder}/{output}.svg"))
+  write(as.character(xml_mini), file = mini_name)
 
-  command <- glue::glue("inkscape --verb=FitCanvasToDrawing --verb=FileVacuum --verb=FileSave --verb=FileQuit {folder}/{output}.svg")
-  system(command, ignore.stdout = !verbose)
-
-
-  glue::glue('{folder}/{output}.svg')
-}
-
-
-
-
-# Check traj types
-trajectory_types$id
-
-boxes <- tibble(
-  id = trajectory_types$id, opacity = 1
-)
-map(boxes$id, function(id) {
-  boxes$opacity <- ifelse(boxes$id == id, 1, 0)
-
-  change_opacity(boxes, id, figure_file("trajectory_types.svg"), folder = figure_file("mini"))
+  command <- glue::glue("inkscape --verb=FitCanvasToDrawing --verb=FileVacuum --verb=FileSave --verb=FileQuit {mini_name}")
+  system(command, ignore.stdout = F)
 })
-
-boxes$base64 <- boxes$id %>% map_chr(function(x) {base64enc::base64encode(glue::glue("analysis/figures/trajectory_types/mini/{x}.svg"))})
-
-write_rds(boxes, figure_file("trajectory_type_boxes.rds"))
-
