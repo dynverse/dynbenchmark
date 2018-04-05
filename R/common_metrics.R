6c56b9da9b99cf986b731386a15da5e13e923eb1
Wouter Saelens <wouter.saelens@gmail.com>
2018-04-05 12:56:29 +0200

    update metrics

--- R/common_metrics.R
+++ R/common_metrics.R
@@ -1,8 +1,9 @@
 #' Common metric information
 #' @export
 metrics <- tribble(
-  ~metric_id, ~descriptive_name, ~name,
-  "correlation", "Correlation", "Ordering",
-  "rf_mse", "RF MSE", "Neighbourhood",
-  "edge_flip", "Edge flip", "Topology"
+  ~metric_id, ~descriptive_name, ~name, ~perfect,
+  "correlation", "Correlation", "Ordering", 1,
+  "rf_mse", "RF MSE", "Neighbourhood", 0,
+  "rf_mse_inv", "RF MSE inverted", "Neighbourhood", 1,
+  "edge_flip", "Edge flip", "Topology",1
 )
