6135c39411ba36e52fc1f0f8a1c017068c4e7673
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-18 12:13:10 +0200

    prior is in dynwrap now

--- analysis/analyses/1-datasets/real/fix_datasets.R
+++ analysis/analyses/1-datasets/real/fix_datasets.R
@@ -59,7 +59,7 @@ for (i in seq_along(dataset_ids)) {
 
   dataset$trajectory_type <- dynutils::classify_milestone_network(dataset$milestone_network)$network_type
   # dataset$geodesic_dist <- dynutils::compute_tented_geodesic_distances(dataset)
-  # dataset$prior_information <- dynnormaliser::generate_prior_information(dataset$milestone_ids, dataset$milestone_network, dataset$progressions, dataset$milestone_percentages, dataset$counts, dataset$feature_info, dataset$cell_info)
+  # dataset$prior_information <- dynwrap::generate_prior_information(dataset$milestone_ids, dataset$milestone_network, dataset$progressions, dataset$milestone_percentages, dataset$counts, dataset$feature_info, dataset$cell_info)
   # dataset$progressions <- with(dataset, dynutils::convert_milestone_percentages_to_progressions(cell_ids, milestone_ids, milestone_network, milestone_percentages))
 
   write_rds(dataset, dataset_file(dataset_id = dataset_id, filename = "dataset.rds"))
