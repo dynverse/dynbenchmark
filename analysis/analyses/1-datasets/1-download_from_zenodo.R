b21ebbf0ba2040b4c44649fcbfba36c9636887d9
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-19 12:04:44 +0200

    lazy loading of the counts and expression

--- analysis/analyses/1-datasets/1-download_from_zenodo.R
+++ analysis/analyses/1-datasets/1-download_from_zenodo.R
@@ -14,18 +14,33 @@ unzip(dataset_file, exdir = derived_file(""))
 file.remove(dataset_file)
 
 # temporary fix; the classes were accidentally removed from each dataset before submission to zenodo
-traj_files <- list.files(derived_file(""), pattern = ".rds", recursive = TRUE, full.names = TRUE)
+traj_files <- list.files(derived_file(""), pattern = ".rds", recursive = TRUE, full.names = FALSE)
 for (file in traj_files) {
-  traj <- read_rds(file) %>%
+  traj <- read_rds(derived_file(file)) %>%
     add_class(paste0("dynwrap::", c("data_wrapper", "with_expression", "with_prior", "with_trajectory"))) %>%
     dynwrap::add_cell_waypoints_to_wrapper() %>%
     .[names(.) != "milenet_spr"]
-  write_rds(traj, file)
+  write_rds(traj, derived_file(file))
 }
 
-# load all tasks and perform visual check
-tasks <- list_as_tibble(map(traj_files, read_rds))
-tasks
+# make one big tasks tibble, with count and expression as functions.
+tasks <- list_as_tibble(map(traj_files, function(task_file) {
+  task <- read_rds(derived_file(task_file))
+  for (col in c("expression", "counts")) {
+    env <- new.env(baseenv())
+    assign("task_file", task_file, env)
+    assign("col", col, env)
+    task[[col]] <- function() {
+      readr::read_rds(dynalysis::derived_file(task_file, "1-datasets"))[[col]]
+    }
+    environment(task[[col]]) <- env
+  }
+  task
+}))
+write_rds(tasks, derived_file("tasks.rds"))
+
+# check the size of tasks, in MB
+tasks %>% map_int(~ pryr::object_size(.) %>% as.integer) %>% sort(decreasing = T) %>% {. / 1e6} %>% head(10)
 
 # upload to remote, if relevant
 # this assumes you have a gridengine cluster at hand
