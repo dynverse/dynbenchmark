a20cede40098ddffd59d7c9cbba706c1db380677
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-19 15:46:16 +0200

    add documentation on different data objects

--- R/data.R
+++ R/data.R
@@ -47,3 +47,21 @@
 #'
 #' @docType data
 "qc_categories"
+
+
+#' Metadata on the trajectory types
+#'
+#' @docType data
+"trajectory_types"
+
+
+#' A DAG of trajectory types
+#'
+#' @docType data
+"trajectory_type_dag"
+
+
+#' Metadata on simplified trajectory types
+#'
+#' @docType data
+"trajectory_types_simplified"
