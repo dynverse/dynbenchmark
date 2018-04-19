302b3c0d96a0f4197d0a8a8c18ac3807cfbf37a3
Robrecht Cannoodt <brechtus@gmail.com>
2018-04-19 14:08:19 +0200

    move around data objects

--- R/generate_prior_mini.R
+++ R/generate_prior_mini.R
@@ -1,30 +1,3 @@
-#' The priors
-#' @export
-priors <- tribble(
-  ~prior_id, ~prior_name,
-  "start_id", "Start cell",
-  "end_id", "End cell(s)",
-  "end_n", "# end states",
-  "states_id", "Cell clustering",
-  "states_n", "# states",
-  "states_network", "State network",
-  "time_id", "Time course",
-  "genes_id", "Marker genes"
-)
-labels <- bind_rows(
-  labels,
-  priors %>% mutate(id=prior_id, long=prior_name, short=prior_name) %>% select(id, long, short)
-)
-
-#' The prior usages
-#' @export
-prior_usages <- tribble(
-  ~prior_usage, ~color,
-  "optional", "#0074D9",
-  "no", "#EEEEEE",
-  "required", "#FF4136"
-)
-
 #' Creates a prior mini svg
 #' @param method_priors Dataframe containing a prior_id and a prior_usage
 #' @param svg The base svg of the priors
