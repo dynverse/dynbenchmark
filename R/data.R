9e6aa2b4fb2b1c1bdc3e12f7965c10c739a8f2a4
zouter <wouter.saelens@gmail.com>
2018-05-16 20:22:07 +0200

    move trajectory types to dynwrap

--- R/data.R
+++ R/data.R
@@ -47,21 +47,3 @@
 #'
 #' @docType data
 "qc_categories"
-
-
-#' Metadata on the trajectory types
-#'
-#' @docType data
-"trajectory_types"
-
-
-#' A DAG of trajectory types
-#'
-#' @docType data
-"trajectory_type_dag"
-
-
-#' Metadata on simplified trajectory types
-#'
-#' @docType data
-"trajectory_types_simplified"
