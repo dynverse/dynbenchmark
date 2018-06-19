21d09458dc2dfb757c143a64fc0981fd23f322e4
zouter <wouter.saelens@gmail.com>
2018-06-19 16:18:37 +0200

    update methods

--- analysis/analyses/4-method_characterisation/4_process_combined.R
+++ analysis/analyses/4-method_characterisation/4_process_combined.R
@@ -11,25 +11,16 @@ methods_tidy <- read_rds(derived_file("methods_tidy.rds"))
 implementation_qc_scores <- readRDS(derived_file("implementation_qc_scores.rds"))
 
 # merge qc scores with implementations tibble
-implementations_tidy <- implementations_tidy %>%
+implementations <- implementations_tidy %>%
   select(-matches("qc_score")) %>%
   left_join(implementation_qc_scores, "implementation_id")
 
-# merge implementations with methods
 methods <- methods_tidy %>%
-  left_join(implementations_tidy, "implementation_id")
-
-implementations <- implementations_tidy %>%
-  left_join(methods_tidy, "implementation_id") %>%
-  group_by(implementation_id) %>%
-  filter(row_number() == n()) %>%
-  ungroup()
-
-# add method_short_name
-methods$method_short_name <- methods$method_id
+  select(-matches("qc_score")) %>%
+  left_join(implementation_qc_scores, "implementation_id")
 
 # filter evaluated
-methods$evaluated <- (methods$wrapper == "Done") %>% coalesce(FALSE)
+methods$evaluated <- (methods$evaluation_inclusion == "TRUE") %>% coalesce(FALSE)
 implementations$evaluated <- (implementations$evaluation_inclusion == "TRUE") %>% coalesce(FALSE)
 
 methods_evaluated <- methods %>%
