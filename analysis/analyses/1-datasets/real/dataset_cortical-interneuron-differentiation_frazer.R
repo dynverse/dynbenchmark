c2368434a58ffc197f660199fdb9ef565886c0fc
zouter <wouter.saelens@gmail.com>
2018-06-29 14:24:18 +0200

    adapt preprocess dataset

--- analysis/analyses/1-datasets/real/dataset_cortical-interneuron-differentiation_frazer.R
+++ analysis/analyses/1-datasets/real/dataset_cortical-interneuron-differentiation_frazer.R
@@ -36,20 +36,12 @@ milestone_ids <- unique(c(milestone_network$from, milestone_network$to))
 
 cell_info <- cell_info %>% filter(milestone_id %in% milestone_ids)
 counts <- counts[cell_info$cell_id, ]
-cell_ids <- cell_info$cell_id
 
-cell_grouping <- cell_info %>% select(cell_id, milestone_id) %>% rename(group_id = milestone_id)
-milestone_percentages <- cell_grouping %>% rename(milestone_id=group_id) %>% mutate(percentage=1)
-
-feature_info <- tibble(feature_id = colnames(counts))
+grouping <- cell_info %>% select(cell_id, milestone_id) %>% deframe()
 
 preprocess_dataset(
   counts = counts,
-  cell_ids = cell_ids,
-  milestone_ids = milestone_ids,
+  grouping = grouping,
   milestone_network = milestone_network,
-  milestone_percentages = milestone_percentages,
-  cell_grouping = cell_grouping,
-  cell_info = cell_info,
-  feature_info = feature_info
+  cell_info = cell_info
 )
