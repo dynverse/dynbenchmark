528b63dcc67f006ea4d9b5dd7acd1e992a32094c
zouter <wouter.saelens@gmail.com>
2018-07-04 16:43:21 +0200

    fix test dynbenchmark

--- package/tests/testthat/test-labels.R
+++ package/tests/testthat/test-labels.R
@@ -5,6 +5,6 @@ context("Testing plotting_labels")
 test_that("Plotting labels works", {
   expect_equal(label_short("one_hundred_and_seven_thousand", width = 10), "One\nhundred\nand seven\nthousand")
   expect_equal(label_long("one_hundred_and_seven_thousand"), "One hundred and seven thousand")
-  expect_equal(label_simple_trajectory_types("directed_linear"), "linear")
+  expect_equal(label_simple_trajectory_types("directed_linear"), "Linear")
   expect_equal(label_extrema(c(0, 0.5, 1)), c("0", "", "1"))
 })
