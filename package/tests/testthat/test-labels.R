3948b3473d9b044f8495dad4221372b9f604a234
zouter <wouter.saelens@gmail.com>
2018-07-04 16:11:11 +0200

    fixy fixy fix

--- package/tests/testthat/test-labels.R
+++ package/tests/testthat/test-labels.R
@@ -2,7 +2,7 @@ context("Testing plotting_labels")
 
 
 
-testthat("Plotting labels works", {
+test_that("Plotting labels works", {
   expect_equal(label_short("one_hundred_and_seven_thousand", width = 10), "One\nhundred\nand seven\nthousand")
   expect_equal(label_long("one_hundred_and_seven_thousand"), "One hundred and seven thousand")
   expect_equal(label_simple_trajectory_types("directed_linear"), "linear")
