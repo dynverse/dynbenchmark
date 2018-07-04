a5f8cc8e45e04483d04ed1ebfae3a95c2b747709
zouter <wouter.saelens@gmail.com>
2018-07-04 13:18:21 +0200

    simplify trajectory type -> dynwrap

--- package/R/simplify_trajectory_type.R
+++
@@ -1,6 +0,0 @@
-#' Convert trajectory type
-#' @param trajectory_types_oi trajectory types to simplify
-#' @export
-simplify_trajectory_type <- function(trajectory_types_oi) {
-  set_names(trajectory_types$simplified, trajectory_types$id)[trajectory_types_oi]
-}
