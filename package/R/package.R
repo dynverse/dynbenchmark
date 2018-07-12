9703ca1fd0fdcf665d53dc16ea274af00644847e
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-12 13:40:58 +0200

    import map2_df

--- package/R/package.R
+++ package/R/package.R
@@ -8,7 +8,7 @@
 #'
 #' @import tibble
 #' @import dplyr
-#' @importFrom purrr %>% map set_names map_dbl map2_dbl map_df walk
+#' @importFrom purrr %>% map set_names map_dbl map2_dbl map_df map2_df walk
 #' @importFrom stringr str_replace_all str_replace
 #'
 #' @import dynutils
