f3c4bff8a77e7b578a476dbcab0863b565b13d98
Robrecht Cannoodt <rcannood@gmail.com>
2018-07-19 16:23:54 +0200

    update scaling

--- scripts/05-scaling/1-submit_jobs.R
+++ scripts/05-scaling/1-submit_jobs.R
@@ -3,11 +3,61 @@ library(tidyverse)
 
 experiment("05-scaling")
 
-# define datasets
-dataset_ids <- c("synthetic/linear_5", "synthetic/linear_6")
+# define dataset function. two datasets with the same seed but different
+# dimensionalities should have the same values in overlapping columns and rows.
+generate_dataset <- function(lnrow, lncol, seed = 1) {
+  prev_seed <- .Random.seed[[1]]
+  nrow <- ceiling(10 ^ lnrow)
+  ncol <- ceiling(10 ^ lncol)
+  expression <- sapply(seq_len(ncol), function(i) {
+    set.seed(i * seed)
+    runif(nrow)
+  })
+  expression[expression <= .9] <- 0
+  counts <- round(2^expression) + 1
+
+  cell_ids <- paste0("Cell", seq_len(nrow))
+  gene_ids <- paste0("Gene", seq_len(nrow))
+  dimnames(counts) <- dimnames(expression) <- list(cell_ids, gene_ids)
+
+  set.seed(seed)
+  dataset <-
+    dynwrap::wrap_data(
+      id = paste0("scaling_", lnrow, "_", lncol),
+      cell_ids = cell_ids
+    ) %>%
+    dynwrap::add_trajectory(
+      milestone_network = data_frame(from = "A", to = "B", length = 1, directed = FALSE),
+      progressions = data_frame(cell_id = cell_ids, from = "A", to = "B", percentage = runif(length(cell_ids)))
+    ) %>%
+    dynwrap::add_expression(
+      counts = counts,
+      expression = expression
+    ) %>%
+    dynwrap::add_prior_information() %>%
+    dynwrap::add_cell_waypoints() %>%
+    dynwrap::add_root("Cell1")
+
+  set.seed(prev_seed)
+
+  dataset
+}
+
+dim_test <- seq(log10(100), log10(100000), by = log10(10) / 4)
+dim_df <- crossing(
+  nrow = dim_test,
+  ncol = dim_test
+)
+dataset_funs <- lapply(seq_len(nrow(dim_df)), function(i) {
+  fun <- generate_dataset
+  formals(fun)$lnrow <- dim_df$nrow[[i]]
+  formals(fun)$lncol <- dim_df$ncol[[i]]
+  fun
+})
+
 
 # define methods
-method_ids <- get_ti_methods(ti_packages = c("dynmethods"))$method_id # removed dynwrap for now
+method_ids <- get_ti_methods()$method_id
 
 # create design
 design <- generate_benchmark_design(
