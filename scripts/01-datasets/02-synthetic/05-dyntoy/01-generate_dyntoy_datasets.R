9853ef7b758b827c54a9afcee7fff164c67c3c7e
zouter <wouter.saelens@gmail.com>
2018-07-25 22:32:04 +0200

    rename toy data generation

--- scripts/01-datasets/02-synthetic/05-dyntoy/01-generate_dyntoy_datasets.R
+++ scripts/01-datasets/02-synthetic/05-dyntoy/01-generate_dyntoy_datasets.R
@@ -3,6 +3,6 @@ library(dynbenchmark)
 toy_datasets <- dyntoy::toy_datasets
 
 datasets <- mapdf(toy_datasets, as.list)
-dataset_ids <- toy_datasets$id
+dataset_ids <- paste0("synthetic/dyn", toy_datasets$id)
 
 walk2(datasets, dataset_ids, save_dataset)
