a0c0794230da4ffe10cd8ec9f90b45336a676e9e
zouter <wouter.saelens@gmail.com>
2018-07-26 15:25:29 +0200

    cleanup dyngen generation :relieved:

--- scripts/01-datasets/02-synthetic/02-dyngen/0_common.R
+++
@@ -1,7 +0,0 @@
-library(tidyverse)
-library(qsub)
-library(dynbenchmark)
-
-dataset_id <- "synthetic/v7"
-dataset_preprocessing(dataset_id)
-remote_folder <- "/group/irc/shared/dynbenchmark/" # dynbenchmark on prism
