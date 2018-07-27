7473d0553400a4e776e5011fdfbe6535d8ce6f1e
zouter <wouter.saelens@gmail.com>
2018-07-27 09:06:49 +0200

    include simulator design

--- scripts/01-datasets/02-synthetic/05-dyntoy/01-generate_dyntoy_datasets.R
+++ scripts/01-datasets/02-synthetic/05-dyntoy/01-generate_dyntoy_datasets.R
@@ -2,6 +2,13 @@ library(dynbenchmark)
 
 toy_datasets <- dyntoy::toy_datasets
 toy_datasets$dataset_source <- "synthetic/dyntoy"
+toy_datasets$simulation_design <- map(toy_datasets$model, function(model) {
+  list(
+    simulator = "dyntoy",
+    simulator_version = devtools::session_info()$packages %>% filter(package %in% c("dyntoy", "dynbenchmark")),
+    model = model
+  )
+})
 
 datasets <- mapdf(toy_datasets, as.list)
 dataset_ids <- paste0("synthetic/dyn", toy_datasets$id)
