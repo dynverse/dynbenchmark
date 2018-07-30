537ca217cdbe7515292f474ca72b8045dcd0d9c7
zouter <wouter.saelens@gmail.com>
2018-07-30 17:03:31 +0200

    upate download from sources with longer time

--- scripts/01-datasets/01-real/01-download_from_sources.R
+++ scripts/01-datasets/01-real/01-download_from_sources.R
@@ -6,14 +6,14 @@ library(dynbenchmark)
 experiment("01-datasets/real/run_all_datasets")
 
 dataset_scripts <- list.files(path = "scripts/01-datasets/01-real/01-download_from_sources-helpers/", pattern = "^dataset_.*\\.R", full.names = TRUE)
-# dataset_scripts <- dataset_scripts[!str_detect(dataset_scripts, "mouse-cell-atlas")]
+# dataset_scripts <- dataset_scripts[str_detect(dataset_scripts, "mouse-cell-atlas") | str_detect(dataset_scripts, "plass")]
 
 script_contents <- map_chr(dataset_scripts, read_file)
-
 
 qsub_config <- qsub::override_qsub_config(
   name = "dynreal",
   memory = "30G",
+  max_wall_time = "02:00:00",
   wait = FALSE,
   stop_on_error = FALSE
 )
@@ -35,6 +35,9 @@ handle <- qsub::qsub_lapply(
 
 write_rds(handle, "handle.rds")
 
+##
+
+handle <- read_rds("handle.rds")
 results <- qsub::qsub_retrieve(handle, wait = TRUE)
 
 names(results) <- dataset_scripts
