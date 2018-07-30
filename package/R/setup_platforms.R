3e16d753b47f7d333a5b66de659c64891beb72f5
zouter <wouter.saelens@gmail.com>
2018-07-30 12:57:02 +0200

    add estimate_platform to package

--- package/R/setup_platforms.R
+++ package/R/setup_platforms.R
@@ -47,3 +47,59 @@ select_platforms <- function(n_platforms) {
   # return platforms
   platforms %>% keep(~.$platform_id %in% platform_ids)
 }
+
+
+# fix for splatEstDroupout function to avoid convergence errors in dropout estimation
+splatEstDropout <- function(norm.counts, params) {
+  means <- rowMeans(norm.counts)
+  x <- log(means)
+  obs.zeros <- rowSums(norm.counts == 0)
+  y <- obs.zeros / ncol(norm.counts)
+  df <- data.frame(x, y)
+  fit <- nls(y ~ splatter:::logistic(x, x0 = x0, k = k), data = df,
+             start = list(x0 = 4, k = -1)) # this initial condition has been changed, to avoid convergence errors
+  mid <- summary(fit)$coefficients["x0", "Estimate"]
+  shape <- summary(fit)$coefficients["k", "Estimate"]
+  params <- splatter::setParams(params, dropout.mid = mid, dropout.shape = shape)
+  return(params)
+}
+
+#' Estimate a platform
+#' @param dataset_id The dataset_id from which the platform will be estimated, using the files in datasets_preproc/raw
+#' @export
+estimate_platform <- function(dataset_id) {
+  requireNamespace("splatter")
+  requireNamespace("Seurat")
+
+  platform_location <- derived_file(paste0(dataset_id, ".rds"), experiment_id = "01-datasets_preproc/platforms")
+  platform <- load_or_generate(
+    platform_location,
+    {
+      assignInNamespace("splatEstDropout", dynbenchmark:::splatEstDropout, pos = "package:splatter")
+
+      dataset_raw <- read_rds(dataset_raw_file(dataset_id))
+
+      counts <- dataset_raw$counts
+
+      # determine how many features change between trajectory stages
+      seurat <- Seurat::CreateSeuratObject(t(dataset_raw$counts))
+      seurat@ident <- factor(dataset_raw$grouping)
+      changing <- FindAllMarkers(seurat, logfc.treshold = 1, min.pct = 0.4, test.use = "t")
+      n_changing <- changing %>% pull(gene) %>% unique() %>% length()
+      trajectory_dependent_features <- n_changing / ncol(counts)
+
+      # estimate splatter params
+      estimate <- splatEstimate(t(counts[sample(nrow(counts), min(nrow(counts), 500)), ]))
+      class(estimate) <- "TheMuscularDogBlinkedQuietly." # change the class, so scater won't get magically loaded when the platform is loaded
+
+      # create platform object
+      platform <- lst(
+        estimate,
+        trajectory_dependent_features,
+        n_cells = nrow(counts),
+        n_features = ncol(counts),
+        platform_id = dataset_id
+      )
+    }
+  )
+}
