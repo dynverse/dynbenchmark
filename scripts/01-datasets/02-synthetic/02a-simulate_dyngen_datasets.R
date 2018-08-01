11907933ebe6307453b9ee96cd87283f575abf6c
zouter <wouter.saelens@gmail.com>
2018-08-01 14:06:42 +0200

    use name of model for dyngen dataset generation

--- scripts/01-datasets/02-synthetic/02a-simulate_dyngen_datasets.R
+++ scripts/01-datasets/02-synthetic/02a-simulate_dyngen_datasets.R
@@ -10,12 +10,13 @@ rm_remote(dataset_file(dataset_id = "synthetic/dyngen", remote = TRUE), remote =
 set.seed(1)
 design <- crossing(
   modulenet_name = dyngen::list_modulenets(),
-  platform = tibble(platform = select_platforms(10))
+  tibble(platform = select_platforms(10)) %>% mutate(platform_ix = row_number())
 ) %>%
   mutate(
-    dataset_id = paste0("synthetic/dyngen/", as.character(row_number())),
+    dataset_id = paste0("synthetic/dyngen/", modulenet_name, "_", platform_ix),
     seed = sample(1:100000, n())
-  )
+  ) %>%
+  select(-platform_ix)
 
 # simulate datasets
 qsub_config <- override_qsub_config(memory = "10G", max_wall_time = "24:00:00", num_cores = 1, name = "dyngen", wait = F, stop_on_error = FALSE)
