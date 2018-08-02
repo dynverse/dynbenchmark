194956438dfe7458e9de146eede70f3fb4db0636
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-02 15:08:04 +0200

    rework error parsing strategy

--- package/R/suite_benchmark_fetch_results.R
+++ package/R/suite_benchmark_fetch_results.R
@@ -40,58 +40,39 @@ benchmark_fetch_results <- function() {
         outputs <- map_df(seq_len(nrow(subdesign$crossing)), function(i) {
           out <- output[[i]]
 
-          if (length(out) != 1 || !is.na(out)) {
-            # hooray, the benchmark suite ran fine!
-            out
+          # if the method has errored and no data was generated at all,
+          # try to find an error message and return it in the right format
+          if (length(out) == 1 && is.na(out)) {
+            stderr <- attr(out, "qsub_error")
 
-          } else {
-            # if qacct is empty or the correct taskid cannot be found, then
-            # this job never ran
-            if (is.null(qacct_out) || !any(qacct_out$taskid == i)) {
-              qsub_error <- "Job cancelled by user"
-            } else {
-              qacct_filt <-
-                qacct_out %>%
-                filter(taskid == i) %>%
-                arrange(desc(row_number_i)) %>%
-                slice(1)
-
-              qacct_memory <- process_qsub_memory(qacct_filt$maxvmem)
-              qacct_exit_status <- qacct_filt$exit_status %>% str_replace(" +", " ")
-              qacct_exit_status_number <- qacct_exit_status %>% str_replace(" .*", "")
-              qacct_user_time <- qacct_filt$ru_stime %>% str_replace("s$", "") %>% as.numeric
-
-              qsub_memory <- process_qsub_memory(qacct_filt$maxvmem)
-              qsub_user_time <- qsub_handle$max_wall_time
-
-              memory_messages <- c("cannot allocate vector of size", "MemoryError", "OOM when allocating tensor", "error writing to connection")
-              is_memory_problem <- function(message) {
-                any(map_lgl(memory_messages, ~grepl(., message)))
-              }
-
-              qsub_error <-
-                if (qacct_exit_status_number %in% c("134", "139") || is_memory_problem(attr(out, "qsub_error"))) {
-                  "Memory limit exceeded"
-                } else if (qacct_exit_status_number %in% c("137", "140", "9", "64")) {
-                  "Time limit exceeded"
-                } else if (qacct_exit_status_number != "0") {
-                  paste0("Error status ", qacct_exit_status, "\n", attr(out, "qsub_error"))
-                } else {
-                  attr(out, "qsub_error")
-                }
+            # if qacct is empty or the correct taskid cannot be found,
+            # then this job never ran
+            if (is.null(stderr) && (is.null(qacct_out) || !any(qacct_out$taskid == i))) {
+              stderr <- "Job cancelled by user"
             }
 
-            # "rerun" the evaluation in error mode, in order to generate the expected output except with
-            # the default fail-scores for each of the metrics
-            benchmark_run_evaluation(
+            # use benchmark_run_evaluation to generate the expected output format and
+            # errored metric scores
+            out <- benchmark_run_evaluation(
               i = i,
               subdesign = metadata$subdesign,
               metrics = metadata$metrics,
               verbose = FALSE,
-              error_mode = qsub_error,
+              error_mode = stderr,
               output_models = metadata$output_models
             )
           }
+
+          out$job_exit_status <- extract_job_exit_status(qacct_out, i)
+
+          out$error_status <- with(out, extract_error_status(
+            stdout = stdout,
+            stderr = stderr,
+            job_exit_status = job_exit_status,
+            produced_model = is.null(model) || identical(model, FALSE)
+          ))
+
+          out
         })
 
         # save models separately
@@ -145,6 +126,49 @@ benchmark_fetch_results <- function() {
 }
 
 
+extract_job_exit_status <- function(qacct_out, task_id) {
+  if (!is.null(qacct_out) && any(qacct_out$taskid == task_id)) {
+    qacct_out %>%
+      filter(taskid == task_id) %>%
+      arrange(desc(row_number_i)) %>%
+      slice(1) %>%
+      pull(exit_status) %>%
+      str_replace(" +", " ") %>%
+      str_replace(" .*", "")
+  } else {
+    NA
+  }
+}
+
+extract_error_status <- function(stdout, stderr, job_exit_status, produced_model) {
+  memory_messages <- c(
+    "cannot allocate vector of size", # R
+    "MemoryError", # python
+    "OOM when allocating tensor", # tensorflow
+    "nullptr != block->mem", # tensorflow,
+    "std::bad_alloc", # tensorflow
+    "Bus error", # stemid 2 -> clustexpr
+    "what\\(\\):  Resource temporarily unavailable", # grandprix
+    "error writing to connection"
+  )
+  is_memory_problem <- function(message) {
+    any(map_lgl(memory_messages, ~grepl(., message)))
+  }
+
+  case_when(
+    job_exit_status %in% c("134", "139") ~ "memory_limit",
+    is_memory_problem(stderr) ~ "memory_limit",
+    is_memory_problem(error_message) ~ "memory_limit",
+    is_memory_problem(stdout) ~ "memory_limit",
+    job_exit_status %in% c("137", "140", "9", "64") ~ "time_limit",
+    job_exit_status != "0" ~ "execution_error",
+    !produced_model ~ "method_error",
+    TRUE ~ "no_error"
+  )
+}
+
+
+
 #' Gather and bind the results of the benchmark jobs
 #'
 #' @param load_models Whether or not to load the models as well.
@@ -158,7 +182,7 @@ benchmark_bind_results <- function(load_models = FALSE) {
   files <- list.files(local_output_folder, pattern = "output_metrics.rds", recursive = TRUE, full.names = TRUE)
 
   # process each method separately
-  output <- as_tibble(map_df(files, function(output_metrics_file) {
+  as_tibble(map_df(files, function(output_metrics_file) {
     name <- output_metrics_file %>% gsub(paste0(local_output_folder, "/"), "", ., fixed = TRUE) %>% gsub("/output_metrics.rds", "", ., fixed = TRUE)
     output_models_file <- gsub("output_metrics.rds", "output_models.rds", output_metrics_file, fixed = TRUE)
 
@@ -178,56 +202,6 @@ benchmark_bind_results <- function(load_models = FALSE) {
       NULL
     }
   }))
-
-  output$error_status <- purrr::pmap_chr(output, extract_error_status)
-
-  output
 }
 
 
-
-#' Extract the error status from an evaluation
-#'
-#' @param stdout The standard output
-#' @param stderr The standard error
-#' @param error_message The error message
-#' @param ... Other columns in the output, ignored
-extract_error_status <- function(stdout, stderr, error_message, ...) {
-  memory_messages <- c(
-    "cannot allocate vector of size", # R
-    "MemoryError", # python
-    "OOM when allocating tensor", # tensorflow
-    "nullptr != block->mem", # tensorflow,
-    "std::bad_alloc", # tensorflow
-    "Bus error", # stemid 2 -> clustexpr
-    "what\\(\\):  Resource temporarily unavailable" # grandprix
-  )
-  is_memory_problem <- function(message) {
-    any(map_lgl(memory_messages, ~grepl(., message)))
-  }
-
-  case_when(
-    error_message == "Memory limit exceeded" ~ "memory_limit",
-    is_memory_problem(stderr) ~ "memory_limit",
-    is_memory_problem(error_message) ~ "memory_limit",
-    is_memory_problem(stdout) ~ "memory_limit",
-    stringr::str_detect(error_message, "Time limit exceeded") ~ "time_limit",
-    stringr::str_detect(stderr, "Time limit exceeded") ~ "time_limit",
-    stringr::str_detect(error_message, "^Error status [0-9]*.*") ~ "execution_error",
-    stringr::str_detect(stderr, "^Error status [0-9]*.*") ~ "execution_error",
-    nchar(error_message) > 0 ~ "method_error",
-    TRUE ~ "no_error"
-  )
-}
-
-
-
-process_qsub_memory <- function(qsub_memory) {
-  if (str_detect(qsub_memory, "[0-9\\.]*MB")) {
-    as.numeric(str_replace_all(qsub_memory, "([0-9\\.]*)MB", "\\1")) / 1024
-  } else if (str_detect(qsub_memory, "[0-9\\.]*GB")) {
-    as.numeric(str_replace_all(qsub_memory, "([0-9\\.]*)GB", "\\1"))
-  } else {
-    stop("Cannot process qsub memory: ", qsub_memory)
-  }
-}
