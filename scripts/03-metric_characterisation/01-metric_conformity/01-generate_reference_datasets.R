933c45d5b37285a833df68ee8c0cb02a291fea44
zouter <wouter.saelens@gmail.com>
2018-08-03 17:27:41 +0200

    update metric characterisation

--- scripts/03-metric_characterisation/01-metric_conformity/01-generate_reference_datasets.R
+++ scripts/03-metric_characterisation/01-metric_conformity/01-generate_reference_datasets.R
@@ -4,31 +4,39 @@ library(tidyverse)
 library(dynbenchmark)
 library(dyntoy)
 
-experiment("03-metric_characterisation/01-reference_vs_perturbation")
+experiment("03-metric_characterisation/01-metric_conformity")
 
-dataset_design <- crossing(
-  tribble(
-    ~topology_id, ~topology_model,
-    "linear", model_linear(num_milestones = 5),
-    "bifurcation", model_bifurcating(max_degree = 3)
-  ),
-  num_cells = c(2, 10, 50, 100, 200, 500),
-  repeat_ix = 1
-) %>%
+source(scripts_file("helper-topologies.R"))
+
+# the topologies are defined here because they need to have the same number of edges for perturb_change_topology
+dataset_design <-
+  crossing(
+    bind_rows(
+      topologies %>% enframe("topology_id", "topology_model"),
+      tribble(
+        ~topology_id, ~topology_model,
+        "bifurcation_simple", dyntoy::model_bifurcating(max_degree = 1)
+      )
+    ),
+    num_cells = c(10, 50, 100, 200, 500),
+    repeat_ix = 1
+  ) %>%
   mutate(
-    dataset_id = str_glue("{topology_id}_{num_cells}_{repeat_ix}")
+    dataset_id = as.character(str_glue("{topology_id}_{num_cells}_{repeat_ix}"))
   )
 
 datasets <- pmap(dataset_design, function(dataset_id, topology_model, num_cells, ...) {
   generate_dataset(
-    unique_id = dataset_id,
+    id = dataset_id,
     model = topology_model,
+    num_cells = num_cells,
+    num_features = 20,
     allow_tented_progressions = FALSE,
     add_prior_information = FALSE,
     normalise = FALSE
   )
-})
+}) %>% set_names(dataset_design$dataset_id)
 datasets_suite <- datasets %>% process_datasets_design()
 
 write_rds(dataset_design, derived_file("dataset_design.rds"))
-write_rds(datasets_suite, derived_file("datasets_suite.rds"))
+write_rds(datasets, derived_file("datasets.rds"))
--- scripts/03-metric_characterisation/01-metric_conformity/02-evaluate_perturbations.R
+++ scripts/03-metric_characterisation/01-metric_conformity/02-evaluate_perturbations.R
@@ -1,10 +1,10 @@
 library(tidyverse)
 library(dynbenchmark)
 
-experiment("03-metric_characterisation/01-reference_vs_perturbation")
+experiment("03-metric_characterisation/01-metric_conformity")
 
 dataset_design <- read_rds(derived_file("dataset_design.rds"))
-datasets_suite <- read_rds(derived_file("datasets_suite.rds"))
+datasets <- read_rds(derived_file("datasets.rds"))
 
 # load perturbations = dynwrap::ti_methods
 source(scripts_file("helper-02-perturbations.R"))
@@ -13,7 +13,19 @@ methods_suite <- perturbation_methods %>% process_methods_design()
 # load rules
 source(scripts_file("helper-02-rules.R"))
 
-# create the actual crossing, and use it to construct the design
+dataset <- datasets$linear_10_1
+model <- perturb_remove_cells(datasets$linear_10_1, 1)
+
+write_rds(dataset, "~/dataset.rds")
+write_rds(model, "~/model.rds")
+
+calculate_metrics(dataset, model)
+
+
+##  ............................................................................
+##  Define which combinations of methods and datasets should be run         ####
+
+# create the crossing first
 crossing <- mapdf(rules, function(rule) {
   crossing <- rule$crossing
 
@@ -33,9 +45,12 @@ parameters <- map(methods_suite$id, function(method_id) {
         tibble(id = character())
       }
     }) %>% bind_rows()
+  parameters <- parameters[!duplicated(parameters),]
   parameters
 }) %>% set_names(methods_suite$id)
 parameters_suite <- process_parameters_design(methods_suite, parameters)
+
+datasets_suite <- datasets %>% process_datasets_design()
 
 design <- lst(
   datasets = datasets_suite,
@@ -46,7 +61,9 @@ design <- lst(
   num_repeats = 1
 )
 
-# define metrics
+
+##  ............................................................................
+##  Run the crossing                                                        ####
 metrics <- c("correlation", "rf_mse", "rf_rsq", "rf_nmse", "lm_mse", "lm_rsq", "lm_nmse", "edge_flip")
 
 # run evaluation on cluster
@@ -57,19 +74,26 @@ metrics <- c("correlation", "rf_mse", "rf_rsq", "rf_nmse", "lm_mse", "lm_rsq", "
 
 # run evaluation locally
 benchmark_submit_check(design, metrics)
-results <- map_df(seq_len(nrow(design$crossing)), benchmark_run_evaluation, subdesign = design, metric = metrics, verbose = TRUE) %>%
+results <- pbapply::pblapply(cl=8, seq_len(nrow(design$crossing)), benchmark_run_evaluation, subdesign = design, metric = metrics, verbose = TRUE) %>%
+  bind_rows() %>%
   mutate(error_status = "no_error")
+
+if (any(results$error_message != "")) stop("Errors: ", results %>% filter(error_message != "") %>% pull(method_id) %>% unique() %>% glue::glue_collapse(", "))
 
 # extract scores from successful results
 scores <- results %>%
   filter(error_status == "no_error") %>%
   gather("metric_id", "score", intersect(metrics, colnames(results))) %>%
-  select(method_id, dataset_id, param_id, metric_id, score, model)
+  select(method_id, dataset_id, param_id, metric_id, score)
+
+models <- results %>%
+  select(method_id, dataset_id, param_id, model)
 
 write_rds(scores, derived_file("scores.rds"))
+write_rds(models, derived_file("models.rds"))
 ##
-
-split(scores, scores$metric_id) %>% map(function(scores) {
-  scores %>%
-    ggplot(aes(dataset_id, paste0(method_id, "_", param_id))) + geom_raster(aes(fill = score)) + ggtitle(scores$metric_id[[1]])
-}) %>% patchwork::wrap_plots()
+#
+# split(scores, scores$metric_id) %>% map(function(scores) {
+#   scores %>%
+#     ggplot(aes(dataset_id, paste0(method_id, "_", param_id))) + geom_raster(aes(fill = score)) + ggtitle(scores$metric_id[[1]])
+# }) %>% patchwork::wrap_plots()
