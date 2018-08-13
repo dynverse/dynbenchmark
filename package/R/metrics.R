3fed36ad2555d5758478ab63f7aa327172ff5631
zouter <wouter.saelens@gmail.com>
2018-08-13 11:10:14 +0200

    add metrics characterised and metrics evaluated

--- package/R/metrics.R
+++ package/R/metrics.R
@@ -1,10 +1,10 @@
 #' Metrics used in the characterisation
 #' @export
-characterised_metrics <- dyneval::metrics %>%
+metrics_characterised <- dyneval::metrics %>%
   filter(metric_id %in% c("correlation", "rf_nmse", "lm_nmse", "edge_flip", "featureimp_cor", "F1_branches", "F1_milestones", "harm_mean"))
 
 
 #' Metrics used in the evaluation
 #' @export
-evaluation_metrics <- dyneval::metrics %>%
+metrics_evaluated <- dyneval::metrics %>%
   filter(metric_id %in% c("correlation", "edge_flip", "featureimp_cor", "F1_branches", "harm_mean"))
