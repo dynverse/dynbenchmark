9dab895ab114d43b03c5b4bc2356d74f117d9bf2
Robrecht Cannoodt <rcannood@gmail.com>
2018-08-20 16:28:50 +0200

    rework benchmark scripts

--- scripts/07-benchmark/2-retrieve_results.R
+++ scripts/07-benchmark/2-retrieve_results.R
@@ -11,52 +11,24 @@ experiment("07-benchmark")
 benchmark_fetch_results(TRUE)
 
 # bind results in one data frame (without models)
-output <- benchmark_bind_results(load_models = TRUE)
+execution_output <- benchmark_bind_results(load_models = TRUE)
+design <- read_rds(derived_file("design.rds"))
+methods_info <- design$methods %>% rename_all(function(x) paste0("method_", x)) %>% select(-method_type) %>% left_join(dynmethods::methods %>% select(method_id = id, method_type = type), by = "method_id")
+datasets_info <- design$datasets %>% rename_all(function(x) paste0("dataset_", x))
 
-# load datasets info
-not_list <- function(x) !is.list(x)
-list2env(read_rds(derived_file("config.rds")), environment())
-datasets_info <- map_df(
-  paste0(local_datasets_folder, "/", dataset_ids, ".rds"),
-  ~ read_rds(.) %>% select_if(not_list)
-)
-write_rds(datasets_info, result_file("datasets_info.rds"))
+data <-
+  execution_output %>%
+  left_join(datasets_info, by = "dataset_id") %>%
+  left_join(methods_info, by = "method_id")
 
 # collect relevant trajectory types
 trajtypes <-
-  dynbenchmark::trajectory_types %>%
-  filter(id %in% unique(datasets_info$trajectory_type)) %>%
-  add_row(id = "overall", directedness = "directed", color = "#AAAAAA", background_color = "E6A1A1", .before = 1)
+  dynwrap::trajectory_types %>%
+  filter(id %in% unique(datasets_info$dataset_trajectory_type)) %>%
+  add_row(id = "overall", simplified = "overall", directed = TRUE, directedness = "directed", colour = "#AAAAAA", background_colour = "E6A1A1", ancestors = list(character(0)))
 
-# print dataset errors
-dataset_errors <- outputs %>%
-  filter(is.na(dataset_id)) %>%
-  mutate(error_message = str_sub(error_message, -600, -1)) %>%
-  group_by(method_name, error_message) %>%
-  summarise(n = n()) %>%
-  ungroup()
 
-print(dataset_errors)
-write_tsv(dataset_errors, derived_file("errors_qsub.tsv"))
-
-# print job errors
-job_errors <- outputs %>%
-  filter(error_message != "", !is.na(dataset_id)) %>%
-  mutate(error_message = str_sub(error_message, -600, -1)) %>%
-  group_by(method_name, error_message) %>%
-  summarise(n = n(), example = dataset_id[[1]]) %>%
-  ungroup() %>%
-  arrange(desc(n))
-
-print(job_errors)
-write_tsv(job_errors, derived_file("errors_method.tsv"))
-
-job_errors %>% filter(error_message %in% c("Memory limit exceeded"))
-job_errors %>% filter(error_message %in% c("Time limit exceeded"))
-job_errors %>% group_by(method_name) %>% summarise(n = sum(n), example = example[[1]]) %>% arrange(desc(n))
-
-required_outputs <- length(dataset_ids) * num_repeats
-outputs %>% filter(!is.na(dataset_id)) %>% group_by(method_name) %>% summarise(n = n()) %>% filter(n != required_outputs) %>% mutate(pass = n > .9 * required_outputs)
+data %>% group_by(method_id, error_status) %>% summarise(n = n()) %>% as.data.frame()
 
 ###################################################
 ############### CREATE AGGREGATIONS ###############
@@ -64,163 +36,118 @@ outputs %>% filter(!is.na(dataset_id)) %>% group_by(method_name) %>% summarise(n
 
 
 # filter control methods
-outputs <- outputs %>% filter(!method_short_name %in% c("scorspar", "identity", "shuffle"))
+# data <- data %>% filter(method_type %in% c("algorithm", "algorithm_test"))
 
-# filter disconnected as there is only 1 dataset
-outputs <- outputs %>% filter(dataset_id != "real/blastocyst-monkey_nakamura")
-trajtypes <- trajtypes %>% filter(id != "disconnected_directed_graph")
-
-
-error_message_interpret <- function(error_message) {
-  map_chr(
-    error_message,
-    function(err) {
-      if (grepl("MemoryError", err) | grepl("OOM when allocating", err)) {
-        "Memory limit exceeded"
-      } else {
-        err
-      }
-    }
-  )
+# scaling disabled for now
+# scalesigmoid_trafo <- function (x, remove_errored = TRUE, max_scale = TRUE) {
+#   xn <- x
+#   if (remove_errored) xn <- xn[xn != 0]
+#   if (max_scale) {
+#     y <- (x - mean(xn)) / max(abs(xn - mean(xn))) * 5
+#   } else {
+#     y <- (x - mean(xn)) / var(xn) * 5
+#   }
+#   sigmoid::sigmoid(y)
+# }
+#
+# # previously:
+# # trafo_fun <- percent_rank
+# trafo_fun <- scalesigmoid_trafo
+trafo_fun <- function(x) {
+  ifelse(is.na(x), 0, x)
 }
 
-scalesigmoid_trafo <- function (x, remove_errored = TRUE, max_scale = TRUE) {
-  xn <- x
-  if (remove_errored) xn <- xn[xn != 0]
-  if (max_scale) {
-    y <- (x - mean(xn)) / max(abs(xn - mean(xn))) * 5
-  } else {
-    y <- (x - mean(xn)) / var(xn) * 5
-  }
-  sigmoid::sigmoid(y)
-}
-
-# previously:
-# trafo_fun <- percent_rank
-trafo_fun <- scalesigmoid_trafo
-
-outputs_ind <- outputs %>%
-  left_join(datasets_info %>% select(dataset_id = id, trajectory_type, dataset_source), by = "dataset_id") %>%
-  filter(dataset_source != "toy") %>%
+data <- data %>%
   mutate(
-    rf_mse_inv = 1 - rf_mse,
-    error_message_int = error_message_interpret(error_message),
-    time_method = ifelse(error_message_int == "Time limit exceeded", timeout_per_execution, time_method),
-    pct_errored = (error_message_int != "") + 0,
-    pct_time_exceeded = (error_message_int == "Time limit exceeded") + 0,
-    pct_memory_exceeded = (error_message_int == "Memory limit exceeded") + 0,
-    pct_other_error = pct_errored - pct_time_exceeded - pct_memory_exceeded,
-    prior_str = sapply(prior_df, function(prdf) ifelse(is.null(prdf) || nrow(prdf) == 0, "", paste(prdf$prior_names, collapse = ";"))),
-    trajectory_type_f = factor(trajectory_type, levels = trajtypes$id)
+    time_method = ifelse(error_status != "no_error", 6 * 3600, time_method) %>% log10,
+    max_mem = ifelse(error_status != "no_error", 32 * 10e9, max_mem) %>% log10,
+    pct_errored = (error_status != "no_error") + 0,
+    pct_time_limit = (error_status == "time_limit") + 0,
+    pct_memory_limit = (error_status == "memory_limit") + 0,
+    pct_execution_error = (error_status == "execution_error") + 0,
+    pct_method_error = (error_status == "method_error") + 0,
+    dataset_trajectory_type_f = factor(dataset_trajectory_type, levels = trajtypes$id)
   ) %>%
   group_by(dataset_id) %>%
   mutate(
     norm_correlation = trafo_fun(correlation),
     norm_edge_flip = trafo_fun(edge_flip),
-    norm_rf_mse = trafo_fun(rf_mse_inv),
-    rank_time_method = percent_rank(-time_method)
-    # rank_time_method = 1 - (time_method / max(time_method))
+    norm_featureimp_cor = trafo_fun(featureimp_cor),
+    norm_F1_branches = trafo_fun(F1_branches),
+    rank_time_method = percent_rank(-time_method),
+    rank_max_mem = percent_rank(-max_mem)
   ) %>%
   ungroup()
 
 # aggregate over replicates
-outputs_summrepl <- outputs_ind %>%
-  group_by(method_name, method_short_name, dataset_id, paramset_id, trajectory_type, dataset_source, prior_str, trajectory_type_f) %>%
+data_repl <- data %>%
+  group_by(method_id, method_name, dataset_id, param_id, dataset_trajectory_type, dataset_source, dataset_trajectory_type_f) %>%
   summarise_if(is.numeric, mean) %>%
   ungroup() %>%
   mutate(
-    pct_allerrored = (pct_other_error == 1)+0,
-    pct_stochastic = pct_other_error - pct_allerrored,
-    harm_mean = apply(cbind(norm_correlation, norm_edge_flip, norm_rf_mse), 1, psych::harmonic.mean)
+    pct_method_error_all = (pct_method_error == 1)+0,
+    pct_method_error_stoch = pct_method_error - pct_method_error_all,
+    harm_mean = dyneval::calculate_harmonic_mean(norm_correlation, norm_edge_flip, norm_featureimp_cor, norm_F1_branches)
   )
 
 # process trajtype grouped evaluation
-outputs_summtrajtype <- outputs_summrepl %>%
-  group_by(method_name, method_short_name, dataset_source, paramset_id, trajectory_type, trajectory_type_f) %>%
+data_trajtype <- data_repl %>%
+  group_by(method_id, method_name, param_id, dataset_trajectory_type, dataset_source, dataset_trajectory_type_f) %>%
   mutate(n = n()) %>%
   summarise_if(is.numeric, mean) %>%
   ungroup() %>%
   mutate(
-    harm_mean = apply(cbind(norm_correlation, norm_edge_flip, norm_rf_mse), 1, psych::harmonic.mean)
+    harm_mean = dyneval::calculate_harmonic_mean(norm_correlation, norm_edge_flip, norm_featureimp_cor, norm_F1_branches)
   )
 
 # process overall evaluation
-outputs_summmethod <- outputs_summtrajtype %>%
-  group_by(method_name, method_short_name, dataset_source, paramset_id) %>%
+data_method <- data_trajtype %>%
+  group_by(method_id, method_name, param_id, dataset_source) %>%
   mutate(n = n()) %>%
   summarise_if(is.numeric, mean) %>%
   ungroup() %>%
   mutate(
-    harm_mean = apply(cbind(norm_correlation, norm_edge_flip, norm_rf_mse), 1, psych::harmonic.mean)
+    harm_mean = dyneval::calculate_harmonic_mean(norm_correlation, norm_edge_flip, norm_featureimp_cor, norm_F1_branches)
   )
 
 # adding mean per trajtype
-outputs_summtrajtype_totals <- bind_rows(
-  outputs_summtrajtype,
-  outputs_summtrajtype %>%
-    group_by(method_name, method_short_name, paramset_id, trajectory_type, trajectory_type_f) %>%
+data_trajtype_totals <- bind_rows(
+  data_trajtype,
+  data_trajtype %>%
+    group_by(method_id, method_name, param_id, dataset_trajectory_type, dataset_trajectory_type_f) %>%
     summarise_if(is.numeric, mean) %>%
     ungroup() %>%
     mutate(dataset_source = "mean")
 ) %>%
   mutate(
-    harm_mean = apply(cbind(norm_correlation, norm_edge_flip, norm_rf_mse), 1, psych::harmonic.mean)
+    harm_mean = dyneval::calculate_harmonic_mean(norm_correlation, norm_edge_flip, norm_featureimp_cor, norm_F1_branches)
   )
 
 # adding mean per method
-outputs_summmethod_totals <-
+data_method_totals <-
   bind_rows(
-    outputs_summmethod,
-    outputs_summmethod %>%
-      group_by(method_name, method_short_name, paramset_id) %>%
+    data_method,
+    data_method %>%
+      group_by(method_id, method_name, param_id) %>%
       summarise_if(is.numeric, mean) %>%
       ungroup() %>%
       mutate(dataset_source = "mean")
   ) %>%
   mutate(
-    harm_mean = apply(cbind(norm_correlation, norm_edge_flip, norm_rf_mse), 1, psych::harmonic.mean)
+    harm_mean = dyneval::calculate_harmonic_mean(norm_correlation, norm_edge_flip, norm_featureimp_cor, norm_F1_branches)
   )
 
 # combine all aggregated data frames
-outputs_summtrajtype_totalsx2 <- bind_rows(
-  outputs_summmethod_totals %>% mutate(trajectory_type = "overall"),
-  outputs_summtrajtype_totals
+data_trajtype_totalsx2 <- bind_rows(
+  data_method_totals %>% mutate(dataset_trajectory_type = "overall"),
+  data_trajtype_totals
 ) %>%
-  mutate(trajectory_type_f = factor(trajectory_type, levels = trajtypes$id)) %>%
+  mutate(dataset_trajectory_type_f = factor(dataset_trajectory_type, levels = trajtypes$id)) %>%
   mutate(
-    harm_mean = apply(cbind(norm_correlation, norm_edge_flip, norm_rf_mse), 1, psych::harmonic.mean)
+    harm_mean = dyneval::calculate_harmonic_mean(norm_correlation, norm_edge_flip, norm_featureimp_cor, norm_F1_branches)
   )
 
 # save data structures
 to_save <- environment() %>% as.list()
-to_save <- to_save[c(str_subset(names(to_save), "^outputs_"), "trajtypes")]
-write_rds(to_save, derived_file("outputs_postprocessed.rds"))
-
-# # # Upload ---------------------
-# qsub:::rsync_remote(
-#   remote_dest = "prism",
-#   path_dest = paste0("/group/irc/shared/dynbenchmark/analysis/data/derived_data/", getOption("dynbenchmark_experiment_id"), "/"),
-#   remote_src = "",
-#   path_src = derived_file("config.rds")
-# )
-# qsub:::rsync_remote(
-#   remote_dest = "prism",
-#   path_dest = paste0("/group/irc/shared/dynbenchmark/analysis/data/derived_data/", getOption("dynbenchmark_experiment_id"), "/"),
-#   remote_src = "",
-#   path_src = derived_file("outputs_postprocessed.rds")
-# )
-# method_names <- list.dirs(derived_file("suite"), recursive = FALSE, full.names = FALSE)
-# for (method_name in method_names) {
-#   cat("Syncing method output for ", method_name, "\n", sep = "")
-#   path_dest <- paste0("/group/irc/shared/dynbenchmark/analysis/data/derived_data/", getOption("dynbenchmark_experiment_id"), "/suite/", method_name, "/")
-#
-#   if (length(list.files(paste0(derived_file("suite/"), method_name, "/"), pattern = "output_*")) > 0) {
-#     qsub:::mkdir_remote(path_dest, remote = "prism")
-#     qsub:::rsync_remote(
-#       remote_dest = "prism",
-#       path_dest = path_dest,
-#       remote_src = "",
-#       path_src = paste0(derived_file("suite/"), method_name, "/output_*.rds")
-#     )
-#   }
-# }
+to_save <- to_save[c(str_subset(names(to_save), "^data"), "trajtypes")]
+write_rds(to_save, derived_file("benchmark_results.rds"), compress = "xz")
