5cbb7591c06023e194bed6c0466cbc27afc1f415
zouter <wouter.saelens@gmail.com>
2018-08-30 20:13:09 +0200

    update render results

--- scripts/varia/render/render_results.R
+++ scripts/varia/render/render_results.R
@@ -6,11 +6,11 @@ library(tidyverse)
 
 readme_paths <- c(
   # fs::dir_ls("results", regexp = "README\\.Rmd", recursive = TRUE)
-  fs::dir_ls(result_file("02-metrics"), regexp = "README\\.Rmd", recursive = TRUE)
+  fs::dir_ls(result_file("", experiment = "02-metrics"), regexp = "README\\.Rmd", recursive = TRUE)
 )
 
 walk(readme_paths, function(readme_path) {
-  print(paste0("Processing ", readme_path))
+  print(paste0("Processing ", fs::path_rel(readme_path)))
   rmarkdown::render(
     readme_path,
     output_format = dynbenchmark::github_markdown_nested(html_preview = FALSE),
