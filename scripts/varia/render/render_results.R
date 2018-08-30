fa8759a87cd5ce8138d55e6297f875d11a1a1e0e
zouter <wouter.saelens@gmail.com>
2018-08-30 13:15:35 +0200

    render results

---
+++ scripts/varia/render/render_results.R
@@ -0,0 +1,19 @@
+#' Renders results READMEs
+
+library(dynbenchmark)
+library(fs)
+library(tidyverse)
+
+readme_paths <- c(
+  # fs::dir_ls("results", regexp = "README\\.Rmd", recursive = TRUE)
+  fs::dir_ls(result_file("02-metrics"), regexp = "README\\.Rmd", recursive = TRUE)
+)
+
+walk(readme_paths, function(readme_path) {
+  print(paste0("Processing ", readme_path))
+  rmarkdown::render(
+    readme_path,
+    output_format = dynbenchmark::github_markdown_nested(html_preview = FALSE),
+    quiet = TRUE
+  )
+})
