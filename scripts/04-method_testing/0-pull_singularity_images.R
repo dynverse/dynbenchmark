71988d8d4ee64004d88996aad7cc10501cd0c0c7
Robrecht Cannoodt <rcannood@gmail.com>
2018-09-03 16:46:33 +0200

    update pull script

--- scripts/04-method_testing/0-pull_singularity_images.R
+++ scripts/04-method_testing/0-pull_singularity_images.R
@@ -6,40 +6,29 @@ library(tidyverse)
 experiment("singularity_images")
 
 
-# RUN THIS ON THE LOGIN NODE:
-#
-# dir.create /data/tmp /data/singularity_cache /data/singularity_localcache /data/singularity_tmp
-#
-# Rscript -e 'purrr::map(dynmethods::repo_digests, dynwrap:::.container_pull_image, config = dynwrap::container_singularity(prebuild = FALSE))'
-#
-# rm /data/tmp/* /data/singularity_localcache/* /data/singularity_tmp/*
-# for i in $(seq 1 8); do ssh prismcls0$i 'rm -rf /data/*'; done
-# for i in $(seq 1 8); do rsync -av /data/singularity_cache prismcls0$i:/data; done
-# for i in $(seq 1 8); do ssh prismcls0$i 'dir.create /data/tmp /data/singularity_cache /data/singularity_localcache /data/singularity_tmp'; done
-# for i in $(seq 1 8); do ssh prismcls0$i 'rm -rf /tmp/.singula* /data/tmp/* /data/singularity_tmp/*'; done
+# If nothing is running on the cluster, run this on the login node to clear temporary folders:
+# rm -rf /tmp/* /data/*
+# for i in $(seq 1 8); do ssh prismcls0$i 'rm -rf /tmp/* /data/*'; done
 
-# test with
-# Rscript -e 'dynbenchmark::setup_singularity_methods(); dynwrap::infer_trajectory(dyntoy::generate_dataset(), dynmethods::ti_angle())'
-
-versions <- dynmethods::method_versions
+method_ids <- dynmethods::methods$docker_repository
 
 handle <- qsub::qsub_lapply(
-  X = seq_along(versions),
-  qsub_environment = c("versions"),
+  X = method_ids,
+  qsub_environment = c(),
   qsub_packages = c("tidyverse", "dynmethods", "dynbenchmark"),
   qsub_config = qsub::override_qsub_config(
     max_wall_time = "01:00:00",
     name = "singbuild",
     memory = "10G",
-    num_cores = 2,
+    num_cores = 1,
     wait = FALSE,
     stop_on_error = FALSE
   ),
-  FUN = function(i) {
+  FUN = function(method_id) {
     config <- dynwrap::container_singularity(
       prebuild = TRUE,
       images_folder = derived_file("", experiment_id = "singularity_images")
     )
-    meth <- dynwrap:::.container_pull_image(names(versions)[[i]], config = config)
+    meth <- dynwrap:::.container_pull_image(method_id, config = config)
   }
 )
