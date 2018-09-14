7b72205eabe6d79b574b15b058ebf55ecee0e451
Robrecht Cannoodt <rcannood@gmail.com>
2018-09-14 09:13:08 +0200

    add back legends

--- scripts/08-summary/2-main_figure.R
+++ scripts/08-summary/2-main_figure.R
@@ -49,7 +49,10 @@ data <-
         most_complex = method_most_complex_trajectory_type
       ) %>%
       gather(metric, label, -method_id) %>%
-      mutate(experiment = "method", category = "overall", colour = "black", placeholder = FALSE) # need to set colours
+      mutate(
+        experiment = ifelse(metric == "control_label", "summary", "method"), # control_label is an exception
+        category = "overall", colour = "black", placeholder = FALSE
+      )
   )
 
 
@@ -86,11 +89,11 @@ metric_pos <-
   metric_pos %>%
   mutate(
     xsep = c(0, ifelse(experiment[-1] != experiment[-n()] | category[-1] != category[-n()], .5, .1)),
+    xsep = ifelse(id == "conl", -4, xsep),
     xwidth = case_when(
       id == "name" ~ 6,
       id %in% c("wrty", "mcpx", "topi") ~ 2,
-      # id %in% c("mcpx", "topi") ~ 2,
-      geom == "bar" ~ 4,
+      geom == "bar" | id == "conl" ~ 4,
       TRUE ~ 1
     ),
     show_label = ! name %in% c("Name"),
@@ -114,6 +117,8 @@ header_pos <-
     key = ifelse(level == "experiment", letters[row_number()], ""),
     x = (xmin + xmax) / 2
   )
+
+header_xvals <- header_pos %>% transmute(nam = paste0(level, "_", id), xmin) %>% deframe()
 
 
 # PROCESS CIRCLES DATA
@@ -184,36 +189,28 @@ trajd <- geom_data_processor("trajtype", function(dat) {
 # CREATE LEGENDS
 legy_start <- min(method_pos$ymin)
 
-# leg_topinf <- topinf_types %>%
-#   mutate(x = 1, y = seq_len(n())) %>%
-#   rename(label = name, col = colour)
-# leg_prior <- data_frame(
-#   label = c("id", "#", "id", "#"),
-#   type1 = c("optional_", "optional_", "required_", "required_"),
-#   col = prior_type1cols[type1],
-#   x = 1,
-#   y = seq_along(type1),
-#   explanation = c("optional cell/gene ids", "optional amount", "required cell/gene ids", "required amount")
-# )
-# leg_circles <- data_frame(
-#   r = c(.05, .1, .2, .4, .6, .8, 1)/2,
-#   x = cumsum(c(0, (.05 + r[-1] + r[-length(r)]))),
-#   colbench = colbench_fun(r*2),
-#   colqc = colqc_fun(r*2)
-# )
-#
-# error_leg_df <- error_reasons %>%
-#   rename(fill = colour) %>%
-#   mutate(
-#     rad_start = seq(0, pi, length.out = 5)[-5],
-#     rad_end = seq(0, pi, length.out = 5)[-1],
-#     rad = (rad_start + rad_end) / 2,
-#     colour = rep("black", length(rad)),
-#     lab_x = row_height * sin(rad),
-#     lab_y = row_height * cos(rad),
-#     hjust = rep(0, length(rad)),
-#     vjust = seq(0, 1, length.out = length(rad)+2)[c(-1,-(length(rad)+2))]
-#   )
+leg_circles <- map_df(names(exp_palettes), function(experiment) {
+  data_frame(
+    r = c(.05, .1, .2, .4, .6, .8, 1)/2,
+    x = cumsum(c(0, (.05 + r[-1] + r[-length(r)]))),
+    exp = experiment,
+    pal = exp_palettes[[experiment]],
+    col = scale_viridis_funs[[exp_palettes[[experiment]]]](x)
+  )
+})
+
+error_leg_df <- error_reasons %>%
+  rename(fill = colour) %>%
+  mutate(
+    rad_start = seq(0, pi, length.out = 5)[-5],
+    rad_end = seq(0, pi, length.out = 5)[-1],
+    rad = (rad_start + rad_end) / 2,
+    colour = rep("black", length(rad)),
+    lab_x = row_height * sin(rad),
+    lab_y = row_height * cos(rad),
+    hjust = rep(0, length(rad)),
+    vjust = seq(0, 1, length.out = length(rad)+2)[c(-1,-(length(rad)+2))]
+  )
 
 # Stamp
 stamp <- paste0("Generated on ", Sys.Date())
@@ -246,7 +243,6 @@ g1 <- ggplot() +
   geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = colour), bind_rows(bar_data, invbar_data), colour = "black", size = .25) +
   # RECTANGLES
   # geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = colour), rect_data, colour = "black", size = .25) +
-  # geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), trajd, colour = "black", size = .25) +
   # CIRCLES
   ggforce::geom_circle(aes(x0 = x0, y0 = y0, fill = colour, r = size), circle_data, size = .25) +
   # STARS
@@ -257,34 +253,29 @@ g1 <- ggplot() +
   geom_text(aes(x = x, y = y, label = label, colour = colour), data = text_data, vjust = .5, hjust = 0.5) +
 
   # RESERVE SPACE
-  expand_limits(x = c(max(metric_pos$xmax)+2), y = legy_start - 4.1) +
+  expand_limits(x = c(max(metric_pos$xmax)+3), y = legy_start - 4.1) +
 
-  # # LEGEND: TOPOLOGY
-  # geom_text(aes(3, legy_start - 1, label = "Topology constraints"), data_frame(i = 1), hjust = 0, vjust = 0, fontface = "bold") +
-  # geom_text(aes(3.5, legy_start - 1 - y * .7, label = label, colour = col), leg_topinf, hjust = 0, vjust = 0) +
-  # geom_text(aes(6.5, legy_start - 1 - y * .7, label = explanation), leg_topinf, hjust = 0, vjust = 0) +
-  #
-  # # LEGEND: PRIOR
-  # geom_text(aes(12.5, legy_start - 1, label = "Prior"), data_frame(i = 1), hjust = 0, vjust = 0, fontface = "bold") +
-  # geom_text(aes(13, legy_start - 1 - y * .7, label = label, colour = col), leg_prior, hjust = 0, vjust = 0) +
-  # geom_text(aes(14.5, legy_start - 1- y * .7, label = explanation), leg_prior, hjust = 0, vjust = 0) +
-  #
-  # # LEGEND: BENCHMARK
-  # geom_text(aes(20.5, legy_start - 1, label = "Benchmark score"), data_frame(i = 1), hjust = 0, vjust = 0, fontface = "bold") +
-  # ggforce::geom_circle(aes(x0 = 21.3 + x, y0 = legy_start - 2.3 + r, r = r, fill = colbench), size = .25, leg_circles) +
-  # geom_text(aes(x = 21.3 + x, y = legy_start - 2.3 - .4, label = c("low", "high")), leg_circles %>% slice(c(1, n()))) +
-  #
-  # # LEGEND: PCT ERRORED
-  # geom_text(aes(26, legy_start - 1, label = "Error reason"), data_frame(i = 1), hjust = 0, vjust = 0, fontface = "bold") +
-  # ggforce::geom_arc_bar(aes(x0 = 26.5, y0 = legy_start - 2.5, r0 = 0, r = row_height*.75, start = rad_start, end = rad_end, fill = fill), size = .25, error_leg_df) +
-  # ggforce::geom_arc_bar(aes(x0 = 26.5, y0 = legy_start - 2.5, r0 = 0, r = row_height*.75, start = rad_start, end = rad_end, fill = NA), size = .25, error_leg_df) +
-  # geom_text(aes(x = 26.5 + lab_x + .5, y = legy_start - 2.5 + lab_y, label = label, vjust = vjust, hjust = hjust), error_leg_df) +
-  # geom_segment(aes(x = 26.5, xend = 26.5, y = legy_start - 2.5, yend = legy_start - 2.5 + row_height*.75), data = data_frame(z = 1), size = .25) +
-  #
-  # # LEGEND: QC SCORE
-  # geom_text(aes(34, legy_start - 1, label = "QC score"), data_frame(i = 1), hjust = 0, vjust = 0, fontface = "bold") +
-  # ggforce::geom_circle(aes(x0 = 34.8 + x, y0 = legy_start - 2.3 + r, r = r, fill = colqc), size = .25, leg_circles) +
-  # geom_text(aes(x = 34.8 + x, y = legy_start - 2.3 - .4, label = c("low", "high")), leg_circles %>% slice(c(1, n()))) +
+  # LEGEND: BENCHMARK
+  geom_text(aes(header_xvals[["experiment_benchmark"]], legy_start - 1, label = "Benchmark score"), data_frame(i = 1), hjust = 0, vjust = 0, fontface = "bold") +
+  ggforce::geom_circle(aes(x0 = header_xvals[["experiment_benchmark"]] + .8 + x, y0 = legy_start - 2.3 + r, r = r, fill = col), size = .25, leg_circles %>% filter(exp == "benchmark")) +
+  geom_text(aes(x = header_xvals[["experiment_benchmark"]] + .8 + x, y = legy_start - 2.3 - .4, label = c("low", "high")), leg_circles %>% filter(exp == "benchmark") %>% slice(c(1, n()))) +
+
+  # LEGEND: SCALING
+  geom_text(aes(header_xvals[["experiment_scaling"]], legy_start - 1, label = "Scaling score"), data_frame(i = 1), hjust = 0, vjust = 0, fontface = "bold") +
+  ggforce::geom_circle(aes(x0 = header_xvals[["experiment_scaling"]] + .8 + x, y0 = legy_start - 2.3 + r, r = r, fill = col), size = .25, leg_circles %>% filter(exp == "scaling")) +
+  geom_text(aes(x = header_xvals[["experiment_scaling"]] + .8 + x, y = legy_start - 2.3 - .4, label = c("low", "high")), leg_circles %>% filter(exp == "scaling") %>% slice(c(1, n()))) +
+
+  # LEGEND: QC
+  geom_text(aes(header_xvals[["experiment_qc"]], legy_start - 1, label = "QC score"), data_frame(i = 1), hjust = 0, vjust = 0, fontface = "bold") +
+  ggforce::geom_circle(aes(x0 = header_xvals[["experiment_qc"]] + .8 + x, y0 = legy_start - 2.3 + r, r = r, fill = col), size = .25, leg_circles %>% filter(exp == "qc")) +
+  geom_text(aes(x = header_xvals[["experiment_qc"]] + .8 + x, y = legy_start - 2.3 - .4, label = c("low", "high")), leg_circles %>% filter(exp == "qc") %>% slice(c(1, n()))) +
+
+  # LEGEND: PCT ERRORED
+  geom_text(aes(header_xvals[["category_trajtypes"]], legy_start - 1, label = "Error reason"), data_frame(i = 1), hjust = 0, vjust = 0, fontface = "bold") +
+  ggforce::geom_arc_bar(aes(x0 = header_xvals[["category_trajtypes"]] + .5, y0 = legy_start - 2.5, r0 = 0, r = row_height*.75, start = rad_start, end = rad_end, fill = fill), size = .25, error_leg_df) +
+  ggforce::geom_arc_bar(aes(x0 = header_xvals[["category_trajtypes"]] + .5, y0 = legy_start - 2.5, r0 = 0, r = row_height*.75, start = rad_start, end = rad_end, fill = NA), size = .25, error_leg_df) +
+  geom_text(aes(x = header_xvals[["category_trajtypes"]] + .5 + lab_x + .5, y = legy_start - 2.5 + lab_y, label = label, vjust = vjust, hjust = hjust), error_leg_df) +
+  geom_segment(aes(x = header_xvals[["category_trajtypes"]] + .5, xend = header_xvals[["category_trajtypes"]] + .5, y = legy_start - 2.5, yend = legy_start - 2.5 + row_height*.75), data = data_frame(z = 1), size = .25) +
 
   # GENERATION SENTENCE
   geom_text(aes(1, legy_start - 4, label = stamp), colour = "#cccccc", hjust = 0, vjust = 0) +
