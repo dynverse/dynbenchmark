60f8fdde1a87ec8aa55f277bbdb51377c499a28b
Robrecht Cannoodt <rcannood@gmail.com>
2018-09-14 09:41:38 +0200

    update error reasons

--- package/data-raw/method_characterisation.R
+++ package/data-raw/method_characterisation.R
@@ -4,9 +4,10 @@ topinf_types <- tibble(
   colour = c("#00ab1b", "#edb600", "#cc2400"),
   explanation = c("inferred by algorithm", "determined by parameter", "fixed by algorithm")
 )
+
 error_reasons <- tibble(
-  name = c("pct_memory_exceeded", "pct_time_exceeded", "pct_allerrored", "pct_stochastic"),
-  label = c("Memory limit exceeded", "Time limit exceeded", "Dataset-specific error", "Stochastic error"),
-  colour = RColorBrewer::brewer.pal(4, "Set3")
+  name = c("pct_memory_limit", "pct_time_limit", "pct_method_error_all", "pct_method_error_stoch", "pct_execution_error"),
+  label = c("Memory limit exceeded", "Time limit exceeded", "Dataset-specific error", "Stochastic error", "Execution error"),
+  colour = RColorBrewer::brewer.pal(5, "Set3")
 )
-devtools::use_data(error_reasons, topinf_types, overwrite = TRUE)
+devtools::use_data(error_reasons, topinf_types, overwrite = TRUE, pkg = "package")
