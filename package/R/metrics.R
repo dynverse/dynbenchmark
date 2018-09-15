3b497d272559d456603b3b3d653eed43d9a26e7d
zouter <wouter.saelens@gmail.com>
2018-09-15 16:57:31 +0200

    fix metrics when there is no namespace yet

--- package/R/metrics.R
+++ package/R/metrics.R
@@ -1,10 +1,14 @@
 #' Metrics used in the characterisation
 #' @export
-metrics_characterised <- dyneval::metrics %>%
-  filter(!metric_id %in% c("featureimp_ks", "featureimp_wilcox", "rf_rsq", "lm_rsq", "rf_mse", "lm_mse"))
+metrics_characterised <- dplyr::filter(
+  dyneval::metrics,
+  !metric_id %in% c("featureimp_ks", "featureimp_wilcox", "rf_rsq", "lm_rsq", "rf_mse", "lm_mse")
+)
 
 
 #' Metrics used in the evaluation
 #' @export
-metrics_evaluated <- dyneval::metrics %>%
-  filter(metric_id %in% c("correlation", "him", "featureimp_wcor", "F1_branches", "geom_mean"))
+metrics_characterised <- dplyr::filter(
+  dyneval::metrics,
+  metric_id %in% c("correlation", "him", "featureimp_wcor", "F1_branches", "geom_mean")
+)
