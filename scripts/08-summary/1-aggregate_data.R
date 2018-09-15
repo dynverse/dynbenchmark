d6fc14935d16b5362de6da15a047acadb45dbb55
Robrecht Cannoodt <rcannood@gmail.com>
2018-09-15 22:03:47 +0200

    benchmark experiment tuning

--- scripts/08-summary/1-aggregate_data.R
+++ scripts/08-summary/1-aggregate_data.R
@@ -18,6 +18,9 @@ method_info <-
   select_if(function(x) !all(is.na(x))) %>%
   filter(!method_id %in% c("error", "identity", "random", "shuffle"))
 
+
+
+
 # read QC results
 tool_qc_scores <- read_rds(result_file("tool_qc_scores.rds", experiment_id = "03-methods"))
 tool_qc_scores <- read_rds(result_file("tool_qc_scores.rds", experiment_id = "03-methods"))
@@ -55,35 +58,96 @@ qc_results <-
   )
 rm(tool_qc_scores, tool_qc_category_scores, tool_qc_application_scores, method_qc_overall_scores, method_qc_category_scores, method_qc_application_scores)
 
+
+
+
 # read scaling results
 scaling_results <- read_rds(result_file("scaling.rds", experiment_id = "05-scaling"))
 
+# scaling_exp <- tribble(
+#   ~ experiment, ~ category, ~ metric, ~ lnrow, ~ lncol,
+#   "scalability", "10k features", "100 cells", 2, 4,
+#   "scalability", "10k features", "10k cells", 4, 4,
+#   "scalability", "10k features", "1m cells", 6, 4,
+#   "scalability", "10k cells", "100 features", 4, 2,
+#   "scalability", "10k cells", "10k features", 4, 4,
+#   "scalability", "10k cells", "1m features", 4, 6
+# )
+
+scaling_exp <- tribble(
+  ~ experiment, ~ category, ~ metric, ~ lnrow, ~ lncol,
+  "scalability", "1k features", "1k cells", 3, 3,
+  "scalability", "1k features", "10k cells", 4, 3,
+  "scalability", "1k features", "100k cells", 5, 3,
+  "scalability", "1k cells", "1k features", 3, 3,
+  "scalability", "1k cells", "10k features", 3, 4,
+  "scalability", "1k cells", "100k features", 3, 5
+)
+
 scaling_process <-
   scaling_results$models %>%
-  select(-method_name, -model_time, -model_mem) %>%
-  gather(metric, value, -method_id) %>%
+  select(method_id, model_time) %>%
+  rowwise() %>%
+  do({
+    df <- .
+    exp <- scaling_exp
+    exp$method_id <- df$method_id
+    exp$logtime <- predict(df$model_time, exp)[,1]
+    exp
+  }) %>%
+  ungroup() %>%
   mutate(
-    experiment = "scaling",
-    category = case_when(metric == "pct_errored" ~ "errors", grepl("time", metric) ~ "time", grepl("mem", metric) ~ "memory")
-  ) %>%
-  group_by(category, metric) %>%
-  mutate(value = percent_rank(-value)) %>%
-  ungroup()
-
-scaling_results <- scaling_process %>% {
-  df <- .
-  bind_rows(
-    df,
-    df %>%
-      filter(metric %in% c("time_lpred", "mem_lpred")) %>%
-      group_by(method_id, experiment) %>%
-      summarise(value = dyneval::calculate_geometric_mean(value)) %>%
-      ungroup() %>%
-      mutate(category = "overall", metric = "overall")
+    scaletime = (logtime - log10(10)) / (log10(3600 * 24 * 7) - log10(10)),
+    score = 1 - ifelse(scaletime > 1, 1, ifelse(scaletime < 0, 0, scaletime)),
+    time = 10^logtime,
+    timestr = case_when(
+      time < 1 ~ "<1s",
+      time < 60 ~ paste0(floor(time), "s"),
+      time < 3600 ~ paste0(floor(time / 60), "m"),
+      time < 3600 * 24 ~ paste0(floor(time / 3600), "h"),
+      time < 3600 * 24 * 7 ~ paste0(floor(time / 3600 / 24), "d"),
+      TRUE ~ ">7d"
+    )
   )
-}
 
-rm(scaling_process)
+scaling_agg <- scaling_process %>%
+  group_by(method_id) %>%
+  summarise(
+    score = mean(score)
+  )
+
+scaling_results <- bind_rows(
+  scaling_process %>% transmute(method_id, experiment, category, metric, value = score, label = timestr),
+  scaling_agg %>% transmute(method_id, experiment = "scalability", category = "overall", metric = "overall", value = score)
+)
+
+
+# scaling_process <-
+#   scaling_results$models %>%
+#   select(-method_name, -model_time, -model_mem) %>%
+#   gather(metric, value, -method_id) %>%
+#   mutate(
+#     experiment = "scalability",
+#     category = case_when(metric == "pct_errored" ~ "errors", grepl("time", metric) ~ "time", grepl("mem", metric) ~ "memory")
+#   ) %>%
+#   group_by(category, metric) %>%
+#   mutate(value = percent_rank(-value)) %>%
+#   ungroup()
+#
+# scaling_results <- scaling_process %>% {
+#   df <- .
+#   bind_rows(
+#     df,
+#     df %>%
+#       filter(metric %in% c("time_lpred", "mem_lpred")) %>%
+#       group_by(method_id, experiment) %>%
+#       summarise(value = dyneval::calculate_geometric_mean(value)) %>%
+#       ungroup() %>%
+#       mutate(category = "overall", metric = "overall")
+#   )
+# }
+
+rm(scaling_process, scaling_agg)
 
 
 # read benchmarking results
@@ -126,6 +190,9 @@ benchmark_results <-
 
 rm(data_aggs, bench_overall, bench_trajtypes, bench_sources)
 
+
+
+
 # combine different experiments
 results <-
   bind_rows(qc_results, benchmark_results, scaling_results)
@@ -143,6 +210,10 @@ metric_info <- results %>%
 
 metric_info %>% as.data.frame()
 
+
+
+
+
 # make sure each method has a value for each metric
 results <- full_join(
   results,
@@ -157,13 +228,16 @@ results <- full_join(
 rm(qc_results, benchmark_results, scaling_results)
 
 
+
+
+
 ## CALCULATE FINAL RANKING
 metric_weights <-
   tribble(
     ~experiment, ~category, ~metric, ~weight,
     "benchmark", "overall", "overall", 1,
     "qc", "overall", "overall", 1,
-    "scaling", "overall", "overall", 1
+    "scalability", "overall", "overall", 1
   )
 
 results <-
@@ -178,6 +252,9 @@ results <-
   bind_rows(results)
 
 metric_info <- metric_info %>% add_row(experiment = "summary", metric = "overall", category = "overall")
+
+
+
 
 # write output
 write_rds(lst(method_info, results, metric_info), result_file("results.rds"), compress = "xz")
