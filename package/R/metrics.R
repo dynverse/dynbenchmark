d2fe6038dd8b790c09347f7e5eb1902f3189ba16
zouter <wouter.saelens@gmail.com>
2018-09-16 08:47:49 +0200

    fix metrics

--- package/R/metrics.R
+++ package/R/metrics.R
@@ -8,7 +8,7 @@ metrics_characterised <- dplyr::filter(
 
 #' Metrics used in the evaluation
 #' @export
-metrics_characterised <- dplyr::filter(
+metrics_evaluated <- dplyr::filter(
   dyneval::metrics,
   metric_id %in% c("correlation", "him", "featureimp_wcor", "F1_branches", "geom_mean")
 )
