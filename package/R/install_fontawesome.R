3c40fb6b12505680865b431ba5e767e47934d349
Robrecht Cannoodt <rcannood@gmail.com>
2018-09-17 21:42:41 +0200

    add install fontawesome function

---
+++ package/R/install_fontawesome.R
@@ -0,0 +1,22 @@
+#' @importFrom extrafont fonts font_import
+#' @importFrom utils download.file untip
+install_fontawesome <- function() {
+  if (!"Font Awesome 5 Free" %in% extrafont::fonts()) {
+    # install fontawesome
+    release <- "fontawesome-free-5.3.1-web"
+
+    # download and unzip
+    url <- paste0("https://github.com/FortAwesome/Font-Awesome/releases/download/5.3.1/", release, ".zip")
+    zip_file <- tempfile(fileext = '.zip')
+    utils::download.file(url, destfile = zip_file)
+
+    temp_dir <- tempfile()
+    dir.create(temp_dir)
+
+    utils::unzip(zipfile = zip_file, exdir = temp_dir)
+
+    font_dir <- paste0(temp_dir, "/", release, "/webfonts/")
+    extrafont::font_import(paths = font_dir, pattern = "fa-solid-900\\.ttf", prompt = FALSE)
+    extrafont::font_import(paths = font_dir, pattern = "fa-brands-400\\.ttf", prompt = FALSE)
+  }
+}
