7cadb1e2ece8f125513a7f0343a32e7fca8d5c07
Robrecht Cannoodt <rcannood@gmail.com>
2018-09-17 18:24:01 +0200

    add method statuses tibble

--- package/data-raw/method_status_colours.R
+++ package/data-raw/method_status_colours.R
@@ -1,22 +1,26 @@
 library(tidyverse)
 
-method_status_colours <- c(
-  execution_error = "#9d0549",
-  method_error = "#d51d14",
-  memory_limit = "#f46b21",
-  time_limit = "#f1be21",
-  missing_prior = "#fff600",
-  low_correlation = "#b4ef43",
-  no_error = "#2ECC40"
+method_statuses <- tribble(
+  ~method_status, ~colour, ~symbol,
+  "execution_error", "#9d0549", "",
+  "method_error", "#d51d14", "",
+  "memory_limit", "#f46b21", "",
+  "time_limit", "#f1be21", "",
+  "missing_prior", "#fff600", "",
+  "low_correlation", "#b4ef43", "",
+  "no_error", "#2ECC40", ""
 )
 
-tibble::enframe(method_status_colours) %>%
+method_status_colours <-
+  method_statuses %>% deframe()
+
+method_statuses %>%
   mutate(i = seq_len(n())) %>%
   ggplot() +
-  geom_text(aes(.4, i, label = name), hjust = 1) +
-  geom_tile(aes(1, i, fill = name)) +
+  geom_text(aes(.4, i, label = method_status), hjust = 1) +
+  geom_tile(aes(1, i, fill = method_status)) +
   scale_fill_manual(values = method_status_colours) +
   expand_limits(x = -0) +
   cowplot::theme_nothing()
 
-devtools::use_data(method_status_colours, pkg = "package")
+devtools::use_data(method_statuses, method_status_colours, pkg = "package", overwrite = TRUE)
