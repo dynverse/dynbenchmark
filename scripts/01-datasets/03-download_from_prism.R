e4dbda29fce0fe7987d0aa7570dafb7af55535b4
zouter <wouter.saelens@gmail.com>
2018-09-18 18:44:26 +0200

    make dataset syncing dummy proof

--- scripts/01-datasets/03-download_from_prism.R
+++ scripts/01-datasets/03-download_from_prism.R
@@ -7,9 +7,9 @@ experiment("01-datasets")
 
 qsub::rsync_remote(
   remote_src = TRUE,
-  path_src = derived_file(remote = TRUE),
+  path_src = derived_file(remote = TRUE, experiment = "01-datasets"),
   remote_dest = FALSE,
-  path_dest = derived_file(remote = FALSE),
+  path_dest = derived_file(remote = FALSE, experiment = "01-datasets"),
   verbose = TRUE,
   compress = FALSE
 )
@@ -17,8 +17,8 @@ qsub::rsync_remote(
 # Upload to prism
 # qsub::rsync_remote(
 #   remote_src = FALSE,
-#   path_src = derived_file(remote = FALSE),
+#   path_src = derived_file(remote = FALSE, experiment = "01-datasets"),
 #   remote_dest = TRUE,
-#   path_dest = derived_file(remote = TRUE),
+#   path_dest = derived_file(remote = TRUE, experiment = "01-datasets"),
 #   verbose = TRUE
 # )
