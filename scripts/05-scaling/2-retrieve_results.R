c03988f430f0d171dd62a9fc6e402e6620bea958
Robrecht Cannoodt <rcannood@gmail.com>
2018-09-26 13:57:47 +0200

    update scaling output

--- scripts/05-scaling/2-retrieve_results.R
+++ scripts/05-scaling/2-retrieve_results.R
@@ -142,16 +142,15 @@ write_rds(lst(data, data_pred, models), result_file("scaling.rds"), compress = "
 ##########################################################
 
 scaling_exp <- tribble(
-  ~ experiment, ~ category, ~ metric, ~ lnrow, ~ lncol,
-  "scalability", "10k features", "1k cells", 3, 3,
-  "scalability", "10k features", "10k cells", 4, 3,
-  "scalability", "10k features", "100k cells", 5, 3,
-  "scalability", "10k cells", "1k features", 3, 3,
-  "scalability", "10k cells", "10k features", 3, 4,
-  "scalability", "10k cells", "100k features", 3, 5
+  ~ labnrow, ~ labncol, ~ lnrow, ~ lncol,
+  "cells1k", "features10k", 3, 4,
+  "cells10k", "features1k", 4, 3,
+  "cells10k", "features10k", 4, 4,
+  "cells10k", "features100k", 4, 5,
+  "cells100k", "features10k", 5, 4
 )
 
-scaling_process <-
+scaling_preds <-
   models %>%
   select(method_id, model_time) %>%
   rowwise() %>%
@@ -177,15 +176,15 @@ scaling_process <-
     )
   )
 
-scaling_agg <- scaling_process %>%
+scaling_agg <-
+  scaling_preds %>%
   group_by(method_id) %>%
-  summarise(
-    score = mean(score)
+  summarise_at("score", mean)
+
+scaling_scores <-
+  bind_rows(
+    scaling_preds %>% transmute(method_id, metric = paste0(labnrow, "_", labncol), score),
+    scaling_agg %>% transmute(method_id, metric = "overall", score)
   )
 
-scaling_results <- bind_rows(
-  scaling_process %>% transmute(method_id, experiment, category, metric, value = score, label = timestr),
-  scaling_agg %>% transmute(method_id, experiment = "scalability", category = "overall", metric = "overall", value = score)
-)
-
-write_rds(scaling_results, result_file("scaling_results.rds"), compress = "xz")
+write_rds(lst(scaling_preds, scaling_agg, scaling_scores), result_file("scaling_scores.rds"), compress = "xz")
