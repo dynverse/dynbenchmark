cf9347f9e0a6bcc9a14466c3bcb2b447522b131c
zouter <wouter.saelens@gmail.com>
2018-09-30 21:59:40 +0200

    update scaling figure + add classification of scaling

--- scripts/05-scaling/3-generate_figures.R
+++ scripts/05-scaling/3-generate_figures.R
@@ -1,3 +1,5 @@
+#' Classify the models and generate scalability figures
+
 library(dynbenchmark)
 library(tidyverse)
 library(dynutils)
@@ -23,79 +25,156 @@ axis_scale <- data_frame(lnrow = seq(1, 6), nrow = 10^lnrow)
 method_ids <- unique(data$method_id) %>% setdiff("error")
 
 ##########################################################
+###############        CLASSIFY TIME       ###############
+##########################################################
+
+time_classification_design <- bind_rows(
+  tibble(
+    feature = "n_cells",
+    n_cells = seq(100, 100000, 100),
+    n_features = 10000
+  ),
+  tibble(
+    feature = "n_features",
+    n_features = seq(100, 100000, 100),
+    n_cells = 10000
+  )
+)
+
+method_predict_time <- models %>% select(method_id, predict_time) %>% deframe()
+
+time_classification_timings <- crossing(
+  time_classification_design,
+  method_id = names(method_predict_time)
+) %>%
+  group_by(method_id) %>%
+  mutate(time = first(method_predict_time[method_id])(n_cells, n_features)) %>%
+  ungroup()
+
+
+classify_curve <- function(slope, curvature) {
+  case_when(
+    slope < 0.001 ~ "constant / low slope",
+    curvature < -0.05 ~ "sublinear",
+    curvature < 0.05 ~ "linear",
+    TRUE ~ "superlinear"
+  )
+}
+
+
+time_classification <- time_classification_timings %>%
+  group_by(method_id, feature) %>%
+  summarise(
+    slope = mean(diff(time, differences = 1)),
+    curvature = mean(seq(0, 1, length.out = n()) - scale_minmax(time)),
+    class = classify_curve(slope, curvature)
+  ) %>%
+  ungroup()
+
+##########################################################
 ###############  GENERATE SUMMARY FIGURE   ###############
 ##########################################################
 
-models <-
-  models %>%
-  mutate(
-    score = 1 - dyneval:::calculate_arithmetic_mean(dynutils::scale_minmax(time_lpred), dynutils::scale_minmax(mem_lpred), dynutils::scale_minmax(pct_errored))
-  ) %>%
-  bind_rows(data %>% select(method_id, method_name) %>% distinct() %>% filter(!method_id %in% models$method_id) %>% mutate(score = 0, pct_errored = 1)) %>%
-  arrange(desc(score)) %>%
-  # arrange(method_id) %>%
-  mutate(
-    method_id_f = factor(method_id, levels = method_id),
-    method_name_f = factor(method_name, levels = method_name),
-    y = -as.integer(method_id_f)
-  )
+method_order <- models %>% arrange(-time_lpred) %>% pull(method_id)
+scale_y_methods <- scale_y_continuous("", breaks = seq_along(method_order), labels = label_method(method_order), expand = c(0, 0))
+scale_y_methods_empty <- scale_y_continuous("", breaks = seq_along(method_order) + 0.5, labels = NULL, expand = c(0, 0))
+no_margin_sides <- theme(plot.margin = margin(), axis.ticks.y = element_blank(), axis.title.y = element_blank())
 
-columns <- tribble(
-  ~id, ~name, ~fill,
-  "score", "Score", "overall",
-  "time_lpred", "Prediced\nlog time", "time",
-  "time_intercept", "Time coeff.\nintercept", "time",
-  "time_lnrow", "Time coeff.\n# cells", "time",
-  "time_lncol", "Time coeff.\n# features", "time",
-  "mem_lpred", "Predicted\nlog memory", "memory",
-  "mem_intercept", "Mem coeff.\nintercept", "memory",
-  "mem_lnrow", "Mem coeff.\n# cells", "memory",
-  "mem_lncol", "Mem coeff.\n# features", "memory",
-  "pct_errored", "& errored", "errors"
-) %>% mutate(
-    x = 1.1 * seq_along(id),
-    min = map_dbl(id, ~ min(models[[.]], na.rm = TRUE)),
-    max = map_dbl(id, ~ max(models[[.]], na.rm = TRUE))
-  )
-barplot_data <-
-  map_df(seq_len(nrow(columns)), function(i) {
-    row <- columns %>% extract_row_to_list(i)
+time_limits <- c(log10(0.1), max(models$time_lpred) + 0.5)
+time_breaks <- log10(c(0.1, 1, 60, 60*60, 10^max(time_limits)))
+scale_x_time <- scale_x_continuous(label_long("average_time"), limits = time_limits, breaks = time_breaks, labels = label_time(10^time_breaks), expand = c(0, 0))
 
-    df <- models %>%
-      select(method_id, y, value = !!row$id) %>%
-      mutate(
-        column = row$id,
-        value = (value - row$min) / (row$max - row$min),
-        x = row$x,
-        fill = row$fill
-      )
-  })
+models <- models %>%
+  mutate(method_id = factor(method_id, method_order))
 
-
-g <-
+## Plot average time
+plot_average_time <- models %>%
+  select(method_id, time_lpred) %>%
   ggplot() +
-  geom_text(aes(1, y, label = method_name), models %>% select(-model_time, -model_mem), hjust = 1) +
-  geom_text(aes(x + .5, 2.5, label = name), columns, hjust = .5) +
-  geom_text(aes(x, .5, label = round(min, 2)), columns, hjust = 0) +
-  geom_text(aes(x + 1, .5, label = round(max, 2)), columns, hjust = 1) +
-  geom_segment(aes(y = 0, yend = 0, x = x, xend = x + 1), columns) +
-  geom_rect(aes(ymin = y - .45, ymax = y + .45, xmin = x, xmax = x + value, fill = fill), barplot_data) +
-  theme_clean() +
-  theme(
-    panel.border = element_blank(),
-    axis.ticks = element_blank(),
-    panel.grid = element_blank(),
-    axis.title = element_blank(),
-    axis.text = element_blank(),
-    legend.position = "bottom"
-  ) +
-  expand_limits(x = -.5) +
-  scale_fill_brewer(palette = "Dark2") +
-  labs(
-    title = "Analysis on scalability of TI methods",
-    subtitle = "Summary of linear models trained on execution time and memory in function of dataset size",
-    fill = "Type"
-  )
+  geom_rect(aes(xmin = time_limits[[1]], xmax = time_lpred, ymin = as.numeric(method_id) - 0.4, ymax = as.numeric(method_id) + 0.4), stat = "identity") +
+  geom_text(aes(x = time_lpred, y = as.numeric(method_id), label = label_time(10^time_lpred)), hjust = 0) +
+  scale_y_methods +
+  scale_x_time +
+  theme_pub() +
+  theme(panel.grid.minor.y = element_line(color = "#AAAAAA"), plot.margin = margin())
+
+plot_average_time
+
+## Plot the classification and small multiples
+plotdata_time_classification <- time_classification_timings %>%
+  left_join(time_classification, c("method_id", "feature")) %>%
+  mutate(method_id = factor(method_id, method_order)) %>%
+  mutate(
+    ymin = as.numeric(method_id),
+    xmin = as.numeric(factor(feature))
+  ) %>%
+  group_by(
+    method_id, feature
+  ) %>%
+  mutate(
+    x = xmin + row_number() / n() * 0.95 + 0.025,
+    y = ymin + scale_minmax(time) * 0.9 + 0.05
+  ) %>%
+  ungroup()
+
+plotdata_time_classification_boxes <- plotdata_time_classification %>%
+  group_by(
+    method_id, feature
+  ) %>%
+  summarise(
+    xmin = first(xmin),
+    xmax = xmin + 1,
+    ymin = first(ymin),
+    ymax = ymin + 1
+  ) %>%
+  left_join(time_classification, c("method_id", "feature"))
+
+
+plot_time_classification <- plotdata_time_classification %>%
+  ggplot(aes(group = paste0(method_id, feature))) +
+  geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax, fill = class), data = plotdata_time_classification_boxes, color = NA) +
+  geom_line(aes(x = x, y = y), color = "white", size = 1) +
+  geom_rect(aes(xmin = xmin, xmax = xmax, ymin = ymin, ymax = ymax), data = plotdata_time_classification_boxes, color = "black", fill = NA, size = 0.25) +
+  scale_y_methods_empty +
+  scale_x_continuous("" , expand = c(0, 0), breaks = c(1.5, 2.5), labels = c("Cells", "Features")) +
+  scale_fill_manual("", values = c("constant / low slope" = "#2ECC40", "sublinear" = "#0074D9", "linear" = "#FF851B", "superlinear" = "#FF4136")) +
+  theme_pub() +
+  theme(legend.position = "bottom") +
+  no_margin_sides
+
+plot_time_classification
+
+##
+slope_limits <- c(-2, log10(max(time_classification$slope)))
+slope_breaks <- log10(c(1, 60, 60*60, 10^max(slope_limits)))
+scale_x_slope <- scale_x_continuous(limits = slope_limits, breaks = slope_breaks, labels = label_time(10^slope_breaks), expand = c(0, 0))
+
+plot_slope <- function(feature) {
+  time_classification %>%
+    filter(feature == !!feature) %>%
+    mutate(method_id = factor(method_id, method_order)) %>%
+    mutate(slope = ifelse(slope < 0, 0.000001, slope)) %>%
+    ggplot() +
+    geom_rect(aes(xmin = first(slope_limits), xmax = log10(slope), ymin = as.numeric(method_id) - 0.4, ymax = as.numeric(method_id) + 0.4), stat = "identity") +
+    scale_y_methods_empty +
+    scale_x_slope +
+    labs(x = label_long(paste0("time_increase_per_100_", gsub("n_", "", feature)))) +
+    theme_pub() +
+    no_margin_sides +
+    theme(panel.grid.major.y = element_line(color = "#AAAAAA"))
+}
+
+plot_slope_cells <- plot_slope("n_cells")
+plot_slope_features <- plot_slope("n_features")
+
+g <- patchwork::wrap_plots(
+  plot_average_time,
+  plot_time_classification,
+  plot_slope_cells,
+  plot_slope_features,
+  widths = c(2, 1, 2, 2),
+  nrow = 1
+)
 
 g
 ggsave(result_file("ranking.svg"), g, width = 14, height = 12)
