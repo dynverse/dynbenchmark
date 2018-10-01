22210e8f22e994cf389eb0a99dddaa8c93d22858
Robrecht Cannoodt <rcannood@gmail.com>
2018-10-01 09:20:18 +0200

    rework retrieval script

--- scripts/07b-stability/2-retrieve_results.R
+++ scripts/07b-stability/2-retrieve_results.R
@@ -37,68 +37,38 @@ table(execution_output$method_id, execution_output$error_status)
 ############### JOIN DATA ###############
 #########################################
 
+datasets <- read_rds(derived_file("datasets.rds", "07b-stability"))
+
 raw_data <-
   execution_output %>%
+  rename(did_bs = dataset_id) %>%
+  left_join(datasets %>% select(did_bs = id, dataset_id = orig_dataset_id), by = "did_bs") %>%
   left_join(load_datasets() %>% select(dataset_id = id, dataset_trajectory_type = trajectory_type, dataset_source = source), by = "dataset_id")
 
-# table(raw_data$dataset_source, raw_data$error_status)
-# ###########################################
-# ############### NORM PARAMS ###############
-# ###########################################
-#
-#
-# norm_fun <- "normal"
-# mean_fun <- "geometric"
-# mean_weights <- c("correlation" = 1, "him" = 1, "featureimp_wcor" = 1, "F1_branches" = 1)
-#
-# tmp <- benchmark_aggregate(
-#   data = raw_data %>% filter(error_status == "no_error"),
-#   metrics = metrics,
-#   norm_fun = norm_fun,
-#   mean_fun = mean_fun,
-#   mean_weights = mean_weights,
-#   dataset_source_weights = c("real/gold" = 1, "real/silver" = 1, "synthetic/dyngen" = 1, "synthetic/dyntoy" = 1, "synthetic/prosstt" = 1, "synthetic/splatter" = 1)
-# )
-#
-# dataset_source_weights <-
-#   tmp$data_aggregations %>%
-#   filter(dataset_trajectory_type == "overall") %>%
-#   select(method_id:dataset_source, overall) %>%
-#   spread(dataset_source, overall) %>%
-#   select(-mean) %>%
-#   mutate(compare = `real/gold`) %>%
-#   gather(synth, value, -method_id:-dataset_trajectory_type, -compare) %>%
-#   group_by(synth) %>%
-#   summarise(cor = nacor(compare, value)) %>%
-#   deframe()
-#
-# #########################################
-# ############### SAVE DATA ###############
-# #########################################
-# write_rds(lst(trajtypes, metrics, datasets_info, methods_info, norm_fun, mean_fun, mean_weights, dataset_source_weights), result_file("benchmark_results_input.rds"), compress = "xz")
-# write_rds(lst(raw_data, metrics), result_file("benchmark_results_unnormalised.rds"), compress = "xz")
-#
-#
-# ###################################################
-# ############### CREATE AGGREGATIONS ###############
-# ###################################################
+write_rds(raw_data, result_file("benchmark_results_unnormalised.rds"), compress = "xz")
+
+
+###################################################
+############### CREATE AGGREGATIONS ###############
+###################################################
 benchmark_results_input <- read_rds(result_file("benchmark_results_input.rds", "07-benchmark"))
+stability_params <- read_rds(result_file("params.rds", "07b-stability"))
 
 out <- benchmark_aggregate(
   data = raw_data %>% mutate(method_name = method_id),
-  metrics = metrics,
+  metrics = stability_params$metrics,
   norm_fun = benchmark_results_input$norm_fun,
   mean_fun = benchmark_results_input$mean_fun,
   mean_weights = benchmark_results_input$mean_weights,
   dataset_source_weights = benchmark_results_input$dataset_source_weights
 )
-
+out$data$overall <- ifelse(is.finite(out$data$overall), out$data$overall, 0)
 
 #####################################################
 ############### CALCULATE VARIABILITY ###############
 #####################################################
 
-worst_var <- var(c(rep(0, floor(num_bootstraps / 2)), rep(1, ceiling(num_bootstraps / 2))))
+worst_var <- var(c(rep(0, floor(stability_params$num_bootstraps / 2)), rep(1, ceiling(stability_params$num_bootstraps / 2))))
 
 norm_var <- function(x) {
   maxx <- max(x)
@@ -110,7 +80,7 @@ norm_var <- function(x) {
   (worst_var_this - var(x) ) / worst_var_this
 }
 
-met2 <- c(metrics, "overall")
+met2 <- c(stability_params$metrics, "overall")
 
 out$data_var <-
   out$data %>%
@@ -123,10 +93,10 @@ out$data_var <-
   ungroup() %>%
   gather(metric, value, !!met2) %>%
   mutate(metric = paste0("var_", metric))
-#
+
 # #####################################################
 # ############### SAVE DATA ###############
 # #####################################################
 #
 # write_rds(out, result_file("benchmark_results_normalised.rds"), compress = "xz")
-#
+
