95ee994e74a6891c8dc31220f192d631c3fbae93
Robrecht Cannoodt <rcannood@gmail.com>
2018-10-03 10:16:54 +0200

    wip refactoring of complementarity

--- scripts/10-benchmark_interpretation/04-complementarity.R
+++ scripts/10-benchmark_interpretation/04-complementarity.R
@@ -5,6 +5,7 @@ experiment("10-benchmark_interpretation")
 
 data <- read_rds(result_file("benchmark_results_normalised.rds", "06-benchmark"))$data
 
+trajectory_type_colours <- dynwrap::trajectory_types %>% select(id, colour) %>% deframe()
 
 ##  ............................................................................
 ##  Find a "good-enough" subset of methods                                  ####
@@ -117,70 +118,8 @@ get_top_methods <- function(data_oi, trajectory_types_oi) {
   steps
 }
 
-
-
-
-
 ##  ............................................................................
-##  Plot a single combination of trajectory types                           ####
-# trajectory_types_oi <- c("tree")
-# trajectory_types_oi <- c("linear", "bifurcation", "multifurcation", "tree")
-trajectory_types_oi <- dynwrap::trajectory_types$id
-colour <- dynwrap::trajectory_types$colour[dynwrap::trajectory_types$id == last(trajectory_types_oi)]
-steps <- get_top_methods(data, trajectory_types_oi)
-
-relevant_steps <- steps %>%
-  group_by(step_ix) %>%
-  filter(any(score < 0.9)) %>%
-  ungroup()
-
-relevant_steps_labels <- relevant_steps %>%
-  filter(chosen) %>%
-  mutate(label = ifelse(step_ix > 1, paste0("+ ", label_wrap(label_method(method_id))), label_wrap(label_method(method_id)))) %>%
-  mutate(score_start = lag(score, default = 0))
-
-plot_complementarity_example <-
-  ggplot(relevant_steps, aes(step_ix, score)) +
-  geom_hline(yintercept = 1, linetype = "dashed", color = "#333333", alpha = 0.5) +
-  ggbeeswarm::geom_quasirandom(aes(color = chosen), data = filter(relevant_steps, !chosen), color = "#888888", alpha = 0.5, groupOnX = TRUE) +
-  ggrepel::geom_label_repel(aes(y = score, label = label), data = relevant_steps_labels, angle = 0, label.size = 0, size = 3, lineheight = 0.8, nudge_y = 0.1, direction = "y") +
-    geom_point(data = relevant_steps %>% filter(chosen), color = colour) +
-    scale_x_continuous(label_long("n_methods"), breaks = seq_len(max(relevant_steps$step_ix)), expand = c(0, 0), limits = c(0, max(relevant_steps$step_ix) + 1)) +
-    scale_y_continuous(NULL, limits = c(0, 1.1), breaks = c(0, 0.25, 0.5, 0.75, 1), labels = scales::percent, expand = c(0, 0)) +
-    expand_limits(y = c(0, 0)) +
-    theme_pub() +
-    theme(axis.line.y = element_blank(), plot.title = element_text(face = "plain", size = 11)) +
-    annotate("segment", x = 0, xend = 0, y = 0, yend = 1) +
-    labs(title = "Datasets with a top model\n(at least a performance of 95% of best model)")
-
-plot_complementarity_example
-
-ggsave(result_file("complementarity_example.pdf"), plot_complementarity_example, width = 6, height = 6)
-write_rds(plot_complementarity_example, derived_file("complementarity_example.rds"))
-
-# ymax <- max(relevant_steps$step_ix)
-# ybreaks <- seq_len(ymax)
-# plot_complementarity_example <-
-#   ggplot(relevant_steps, aes(score, -step_ix)) +
-#   geom_vline(xintercept = 1, linetype = "dashed", color = "#333333", alpha = 0.5) +
-#   ggbeeswarm::geom_quasirandom(aes(color = chosen), data = filter(relevant_steps, !chosen), color = "#888888", alpha = 0.5, groupOnX = FALSE) +
-#   ggrepel::geom_label_repel(aes(x = score, label = label), data = relevant_steps_labels, angle = 0, label.size = 0, size = 3, lineheight = 0.8, nudge_y = .5, direction = "y") +
-#   geom_point(data = relevant_steps %>% filter(chosen), color = colour) +
-#   scale_y_continuous(breaks = -ybreaks, labels = ybreaks, expand = c(0, 0), limits = c(-(ymax + 1), 0)) +
-#   scale_x_continuous(limits = c(0, 1.1), breaks = c(0, 0.25, 0.5, 0.75, 1), labels = scales::percent, expand = c(0, 0)) +
-#   expand_limits(x = c(0, 0)) +
-#   theme_pub() +
-#   theme(axis.line.x = element_blank(), plot.title = element_text(face = "plain", size = 14)) +
-#   annotate("segment", x = 0, xend = 1, y = -(ymax + 1), yend = -(ymax + 1)) +
-#   labs(
-#     x = "Likelihood of obtaining a top model",
-#     y = label_long("n_methods")
-#   )
-
-
-
-##  ............................................................................
-##  Plot the complementarity of multiple combinations of trajectory_types   ####
+##  Compute step data                                                       ####
 steps_combinations <- tibble(
   trajectory_types_oi = list(
     dynwrap::trajectory_types$id,
@@ -208,10 +147,55 @@ steps_combinations <- tibble(
   ) %>%
   mutate(steps = map(trajectory_types_oi, get_top_methods, data = data))
 
-
-all_steps <- steps_combinations %>%
+all_steps <-
+  steps_combinations %>%
   unnest(steps)
 
+
+##  ............................................................................
+##  Plot a single combination of trajectory types                           ####
+relevant_steps_ex <-
+  all_steps %>%
+  filter(combination_id == "All trajectory types") %>%
+  group_by(step_ix) %>%
+  filter(any(score < 0.9)) %>%
+  ungroup()
+
+relevant_steps_labels_ex <-
+  relevant_steps_ex %>%
+  filter(chosen) %>%
+  mutate(label = ifelse(step_ix > 1, paste0("+ ", label_wrap(label_method(method_id))), label_wrap(label_method(method_id)))) %>%
+  mutate(score_start = lag(score, default = 0))
+
+ymax <- max(relevant_steps_ex$step_ix)
+ybreaks <- seq_len(ymax)
+plot_complementarity_example <-
+  ggplot(relevant_steps_ex, aes(score, -step_ix)) +
+  geom_segment(aes(x = score, xend = score, y = -(step_ix + .5 + .1), yend = -(step_ix + .5 + .9), colour = most_complex_trajectory_type), data = filter(relevant_steps_ex, chosen), alpha = .5) +
+  ggbeeswarm::geom_quasirandom(aes(color = chosen), data = filter(relevant_steps_ex, !chosen), color = "#888888", alpha = 0.8, size = 1, groupOnX = FALSE) +
+  ggrepel::geom_label_repel(aes(x = score, label = label), data = relevant_steps_labels_ex, angle = 0, label.size = 0, size = 3, lineheight = 0.8, nudge_x = .1, direction = "x") +
+  geom_point(aes(colour = most_complex_trajectory_type), data = relevant_steps_ex %>% filter(chosen)) +
+  scale_x_continuous(limits = c(0, 1.1), breaks = c(0, 0.25, 0.5, 0.75, 1), labels = scales::percent, expand = c(0, 0)) +
+  scale_y_continuous(breaks = -ybreaks, labels = ybreaks, expand = c(0, 0), limits = c(-(ymax + .5), -.5)) +
+  scale_color_manual(values = trajectory_type_colours, guide = FALSE) +
+  # expand_limits(x = c(0, 0)) +
+  theme_pub() +
+  labs(
+    x = "Probability of obtaining a top model",
+    y = label_long("n_methods")
+  ) +
+  lemon::coord_capped_cart(bottom = "both", left = lemon::brackets_vertical())
+
+plot_complementarity_example
+
+ggsave(result_file("complementarity_example.pdf"), plot_complementarity_example, width = 6, height = 6)
+write_rds(plot_complementarity_example, derived_file("complementarity_example.rds"))
+
+
+
+
+##  ............................................................................
+##  Plot the complementarity of multiple combinations of trajectory_types   ####
 relevant_steps <- all_steps %>%
   group_by(step_ix) %>%
   filter(any(round(score, 5) < 1)) %>%
@@ -224,11 +208,10 @@ relevant_steps_labels <- relevant_steps %>%
   mutate(score_start = lag(score, default = 0)) %>%
   ungroup()
 
-trajectory_type_colours <- dynwrap::trajectory_types %>% select(id, colour) %>% deframe()
-
 plot_complementarity_combinations <-
   ggplot(relevant_steps, aes(x = score, xend = score, y = 0, yend = 1)) +
   geom_rect(aes(xmin = score_start, xmax = score, ymin = 0, ymax = 1, fill = factor(step_ix)), data = relevant_steps_labels) +
+  facet_grid(combination_id~., labeller = label_facet(label_short, width = 15), switch = "y") +
   geom_hline(yintercept = 0, color = "#888888") +
   geom_hline(yintercept = 1, color = "#888888") +
   geom_segment(x = 0, xend = 0, y = 0, yend = 1, color = "#888888") +
@@ -249,10 +232,9 @@ plot_complementarity_combinations <-
   ) +
   geom_segment(aes(color = most_complex_trajectory_type), data = relevant_steps %>% filter(chosen)) +
   scale_y_continuous(limits = c(-2, 1), breaks = NULL, expand = c(0, 0)) +
-  scale_x_continuous("Datasets with a top model\n(at least a performance of 95% of best model)", limits = c(0, 1), breaks = c(0, 0.25, 0.5, 0.75, 1), labels = scales::percent, expand = c(0, 0), position = "top") +
+  scale_x_continuous(limits = c(0, 1), breaks = c(0, 0.25, 0.5, 0.75, 1), labels = scales::percent, expand = c(0, 0)) +
   scale_fill_grey(label_long("n_methods"), start = 0.95, end = 0.2, limits = c(1, 2,3,4,5,6), na.value = "#333333") +
   scale_color_manual(values = trajectory_type_colours, guide = FALSE) +
-  facet_grid(combination_id~., labeller = label_facet(label_short, width = 15), switch = "y") +
   theme_pub() +
   theme(
     axis.title.y = element_blank(),
@@ -262,10 +244,11 @@ plot_complementarity_combinations <-
     strip.background.y = element_blank(),
     panel.spacing = unit(0, "cm"),
     legend.position = "bottom",
-    legend.justification = "center",
-    axis.title.x = element_text(size = 11)
+    legend.justification = "center"
   ) +
-  guides(fill = guide_legend(ncol = 6, label.position = "right"))
+  guides(fill = guide_legend(ncol = 6, label.position = "right")) +
+  labs(x = "Probability of obtaining a top model") +
+  lemon::coord_capped_cart(bottom = "both")
 
 plot_complementarity_combinations
 
@@ -275,11 +258,12 @@ write_rds(plot_complementarity_combinations, derived_file("complementarity_combi
 ##  Combined complementarity plot                                           ####
 
 plot_complementarity <- patchwork::wrap_plots(
-  read_rds(derived_file("complementarity_example.rds")) %>% patchwork::wrap_elements(),
-  read_rds(derived_file("complementarity_combinations.rds")) %>% patchwork::wrap_elements(),
+  read_rds(derived_file("complementarity_example.rds")),
+  read_rds(derived_file("complementarity_combinations.rds")),
   nrow = 1,
   widths = c(1, 1.5)
-) + patchwork::plot_annotation(tag_levels = "a")
+) +
+  patchwork::plot_annotation(tag_levels = "a")
 
 plot_complementarity
 
