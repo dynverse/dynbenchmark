0d6090d5296c439f83d44eac13b99887d25c5520
Robrecht Cannoodt <rcannood@gmail.com>
2018-10-03 11:59:13 +0200

    update columns for summary and detailed

--- scripts/08-summary/2a_columns_detailed.R
+++ scripts/08-summary/2a_columns_detailed.R
@@ -1,7 +1,12 @@
 source(scripts_file("2a_columns_all.R", experiment = "08-summary"), local = TRUE)
 
+selected_columns <- c("method_name")
+selected_groups <- c("benchmark_metric", "benchmark_source", "benchmark_trajtype", "scaling_predtime_cells10k", "scaling_predtime_features10k", "stability", "qc_category")
+removed_columns <- c()
+
 column_info <-
-  column_info %>% filter(id == "method_name" | group %in% c("benchmark_metric", "benchmark_source", "benchmark_trajtype", "benchmark_execution", "scaling_predtime_cells10k", "scaling_predtime_features10k", "qc_category"))
+  column_info %>%
+  filter(id %in% selected_columns | group %in% selected_groups, !id %in% removed_columns)
 
 column_groups <-
   column_groups %>% filter(group %in% column_info$group)
