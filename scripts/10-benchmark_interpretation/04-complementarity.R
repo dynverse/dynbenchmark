c72e00d4938e0ac3cf0f13fd6f77ef335a38cb4d
Robrecht Cannoodt <rcannood@gmail.com>
2018-10-04 17:18:44 +0200

    performing some plotmath wizardry to get multiple colours, font styles etc etc

--- scripts/10-benchmark_interpretation/04-complementarity.R
+++ scripts/10-benchmark_interpretation/04-complementarity.R
@@ -161,48 +161,105 @@ all_steps <-
 
 ##  ............................................................................
 ##  Plot a single combination of trajectory types                           ####
+plot1_width <- 7
+plot_height <- 6
+
 relevant_steps_ex <-
   all_steps %>%
   filter(combination_id == "All trajectory types") %>%
   group_by(step_ix) %>%
   filter(any(score < 0.9)) %>%
-  ungroup()
+  ungroup() %>%
+  mutate(y = -step_ix + ifelse(step_ix == 1, .75, 0))
 
 relevant_steps_labels_ex <-
   relevant_steps_ex %>%
   filter(chosen) %>%
-  mutate(label = ifelse(step_ix > 1, paste0("+ ", label_wrap(label_method(method_id))), label_wrap(label_method(method_id)))) %>%
-  mutate(score_start = lag(score, default = 0))
+  mutate(
+    score_start = lag(score, default = 0),
+    label_method = label_method(method_id),
+    label_score = paste0(round(score * 100), "%"),
+    label = case_when(
+      step_ix == 1 ~ paste0('phantom("Running ") * phantom(bold("', label_method, '")) * phantom(" on all datasets")\n"will result in a top model ', label_score, ' of the time"'),
+      step_ix == 2 ~ paste0('"Running both " * bold("', label_method[[1]], '") * " and " * bold("', label_method, '")\n"will result in at least one top model ', label_score, ' of the time"'),
+      step_ix == 3 ~ paste0('bold("', label_method[[1]], '") * ", " * bold("', label_method[[2]], '") * " and " * bold("', label_method, '")\n"\u21B3 \u2265 1 top model ', label_score, ' of the time"'),
+      TRUE ~ paste0('"Add " * bold("', label_method, '")\n"\u21B3 ', label_score, '"')
+    ),
+    label_top = gsub("\\n.*", "", label),
+    label_bot = gsub(".*\\n", "", label)
+  )
 
-ymax <- max(relevant_steps_ex$step_ix)
-ybreaks <- seq_len(ymax)
+source(scripts_file("04-complementarity_helper.R"))
+bracket_range <- relevant_steps_ex %>% filter(step_ix == 1, !chosen) %>% pull(score) %>% range()
+bracket_data <-
+  bracket(0, 1, 0, .3, flip = TRUE) %>%
+  mutate(
+    x = x * diff(bracket_range) + bracket_range[[1]],
+    y = y - .9
+  )
+
 plot_complementarity_example <-
-  ggplot(relevant_steps_ex, aes(score, -step_ix)) +
-  geom_segment(aes(x = score, xend = score, y = -(step_ix + .5 + .1), yend = -(step_ix + .5 + .9), colour = most_complex_trajectory_type), data = filter(relevant_steps_ex, chosen), alpha = .5) +
-  ggbeeswarm::geom_quasirandom(aes(color = chosen), data = filter(relevant_steps_ex, !chosen), color = "#888888", alpha = 0.8, size = 1, groupOnX = FALSE) +
-  ggrepel::geom_label_repel(aes(x = score, label = label), data = relevant_steps_labels_ex, angle = 0, label.size = 0, size = 3, lineheight = 0.8, nudge_x = .1, direction = "x") +
+  ggplot(relevant_steps_ex, aes(score, y)) +
+
+  # labels to the right
+  geom_text(
+    aes(x = 1.25, y = y + .375, label = label_top),
+    relevant_steps_labels_ex %>% mutate(label_top = gsub("phantom\\((\"[^)]*\")\\)", "\\1", label_top)),
+    hjust = 1, lineheight = 1, parse = TRUE, vjust = 1
+  ) +
+  geom_text(
+    aes(x = 1.25, y = y + .375, label = label_top, colour = most_complex_trajectory_type),
+    relevant_steps_labels_ex %>% filter(grepl("phantom\\(bold\\(", label_top)) %>% mutate(label_top = gsub("phantom\\((bold\\(\"[^)]*\"\\))\\)", "\\1", label_top)),
+    hjust = 1, lineheight = 1, parse = TRUE, vjust = 1
+  ) +
+  geom_text(
+    aes(x = 1.25, y = y - .375, label = label_bot),
+    relevant_steps_labels_ex,
+    hjust = 1, lineheight = 1, parse = TRUE, vjust = 0
+  ) +
+
+  # first label line
+  geom_path(aes(x, y), data_frame(x = c(.31, .6), y = c(-.25, -.25))) +
+
+  # bracket and extra text
+  geom_text(aes(x = 1.25, y = -1.125, label = 'phantom("While ") * bold("other methods") * phantom(" perform less well")'), data_frame(x = 1), hjust = 1, vjust = .5, parse = TRUE, colour = "#AAAAAA") +
+  geom_text(aes(x = 1.25, y = -1.125, label = '"While " * phantom(bold("other methods")) * " perform less well"'), data_frame(x = 1), hjust = 1, vjust = .5, parse = TRUE) +
+  geom_path(aes(x, y), bracket_data) +
+  geom_path(aes(x, y), data_frame(x = c(mean(bracket_range), mean(bracket_range), .6), y = c(-.9, -1.125, -1.125))) +
+
+  # red baseline lines
+  geom_segment(aes(x = score, xend = score, y = -(step_ix + .5 + .1), yend = -(step_ix + .5 + .9), colour = most_complex_trajectory_type), data = filter(relevant_steps_ex, chosen)) +
+
+  # points
+  ggbeeswarm::geom_quasirandom(aes(color = chosen), data = filter(relevant_steps_ex, !chosen), color = "#AAAAAA", size = 1, groupOnX = FALSE) +
+
+  # big best point
   geom_point(aes(colour = most_complex_trajectory_type), data = relevant_steps_ex %>% filter(chosen)) +
-  scale_x_continuous(limits = c(0, 1.1), breaks = c(0, 0.25, 0.5, 0.75, 1), labels = scales::percent, expand = c(0, 0)) +
-  scale_y_continuous(breaks = -ybreaks, labels = ybreaks, expand = c(0, 0), limits = c(-(ymax + .5), -.5)) +
+
+  # theme settings
+  scale_x_continuous(limits = c(0, 1.25), breaks = c(0, 0.25, 0.5, 0.75, 1), labels = scales::percent, expand = c(0, 0)) +
+  scale_y_continuous(breaks = relevant_steps_labels_ex$y, labels = relevant_steps_labels_ex$step_ix, expand = c(0, 0), limits = range(relevant_steps_ex$y) + c(-.5, .5)) +
   scale_color_manual(values = trajectory_type_colours, guide = FALSE) +
-  # expand_limits(x = c(0, 0)) +
   theme_pub() +
   labs(
     x = "Likelihood of obtaining a top model",
     y = label_long("n_methods")
   ) +
-  lemon::coord_capped_cart(bottom = "both", left = lemon::brackets_vertical())
+  lemon::coord_capped_cart(bottom = "both", left = lemon::brackets_vertical(length = .04))
 
 plot_complementarity_example
 
-ggsave(result_file("complementarity_example.pdf"), plot_complementarity_example, width = 6, height = 6)
+ggsave(result_file("complementarity_example.pdf"), device = cairo_pdf, plot_complementarity_example, width = plot1_width, height = plot_height)
 write_rds(plot_complementarity_example, derived_file("complementarity_example.rds"))
 
 
+ggplot() +
+  geom_text(aes(x = 1, y = 1, label = "While~other~bold(methods)~perform~less~well"), parse = T)
 
 
 ##  ............................................................................
 ##  Plot the complementarity of multiple combinations of trajectory_types   ####
+plot2_width <- 7
 comb_ypos <-
   steps_combinations %>%
   select(combination_id) %>%
@@ -222,9 +279,11 @@ relevant_steps_labels <-
   relevant_steps %>%
   group_by(combination_id) %>%
   filter(chosen) %>%
-  mutate(label = ifelse(step_ix > 1, paste0("+ ", label_wrap(label_method(method_id))), label_wrap(label_method(method_id)))) %>%
+  mutate(label = label_wrap(label_method(method_id))) %>%
   mutate(score_start = lag(score, default = 0)) %>%
   ungroup()
+
+repel_labels <- relevant_steps_labels %>% filter(step_ix < 3 | (score < 0.9 & step_ix < 7))
 
 plot_complementarity_combinations <-
   ggplot(relevant_steps) +
@@ -232,22 +291,20 @@ plot_complementarity_combinations <-
   geom_rect(aes(xmin = 0, xmax = 1, ymin = y, ymax = y + bar_height), comb_ypos, colour = "#888888", fill = NA) +
   geom_segment(aes(x = score, xend = score, y = y, yend = y + bar_height), color = "black", alpha = 0.1) +
   geom_point(aes(x = score, y = y, color = most_complex_trajectory_type), data = relevant_steps %>% filter(chosen)) +
-  geom_text(aes(-text_width / 2, y + bar_height / 2, label = label_short(combination_id, width = 15)), comb_ypos, hjust = .5) +
+  geom_text(aes(-text_width / 2, y + bar_height / 2, label = label_short(combination_id, width = 15)), comb_ypos, hjust = .5, lineheight = 1) +
   ggrepel::geom_text_repel(
-    aes(
-      x = score,
-      y = y,
-      label = label
-    ),
-    data = relevant_steps_labels %>% filter(step_ix < 3 | (score < 0.9 & step_ix < 7)),
-    nudge_y = -(1 - bar_height)/2,
+    aes(x = score, y = y, label = label),
+    data = repel_labels,
+    nudge_y = -.1,
+    vjust = 1,
     direction = "x",
-    lineheight = 0.8,
+    lineheight = .85,
     min.segment.length = 0,
-    size = 3
+    force = 20,
+    max.iter = 10000
   ) +
   geom_segment(aes(x = score, xend = score, y = y, yend = y + bar_height, color = most_complex_trajectory_type), data = relevant_steps %>% filter(chosen)) +
-  scale_y_continuous(limits = c(.5, max(comb_ypos$y) + .5), breaks = NULL, expand = c(0, 0)) +
+  scale_y_continuous(limits = c(bar_height, max(comb_ypos$y) + .5), breaks = NULL, expand = c(0, 0)) +
   scale_x_continuous(limits = c(-text_width, 1), breaks = c(0, 0.25, 0.5, 0.75, 1), labels = scales::percent, expand = c(0, .03)) +
   scale_fill_grey(label_long("n_methods"), start = 0.95, end = 0.2, limits = c(1, 2,3,4,5,6), na.value = "#333333") +
   scale_color_manual(values = trajectory_type_colours, guide = FALSE) +
@@ -259,29 +316,25 @@ plot_complementarity_combinations <-
     strip.placement.y = "left",
     strip.background.y = element_blank(),
     panel.spacing = unit(0, "cm"),
-    legend.position = "bottom",
+    legend.position = "top",
     legend.justification = "center"
   ) +
   guides(fill = guide_legend(ncol = 6, label.position = "right")) +
   labs(x = "Likelihood of obtaining a top model") +
   lemon::coord_capped_cart(bottom = "both")
 
-plot_complementarity_combinations
-
 write_rds(plot_complementarity_combinations, derived_file("complementarity_combinations.rds"))
 
 ##  ............................................................................
 ##  Combined complementarity plot                                           ####
-
+plot_width <- plot1_width + plot2_width
 plot_complementarity <- patchwork::wrap_plots(
   read_rds(derived_file("complementarity_example.rds")),
   read_rds(derived_file("complementarity_combinations.rds")),
   nrow = 1,
-  widths = c(1, 1.5)
+  widths = c(plot1_width, plot2_width) / plot_width
 ) +
   patchwork::plot_annotation(tag_levels = "a")
 
-plot_complementarity
-
-ggsave(result_file("complementarity.pdf"), plot_complementarity, width = 13, height = 6, device = cairo_pdf)
+ggsave(result_file("complementarity.pdf"), plot_complementarity, width = plot_width, height = plot_height, device = cairo_pdf)
 
