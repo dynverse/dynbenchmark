d8eed96291904cc0544a6c025b0e7af4c5dd7b41
Robrecht Cannoodt <rcannood@gmail.com>
2018-11-12 09:35:34 +0100

    remove offtheshelf labelling columns

--- scripts/08-summary/2a_columns_all.R
+++ scripts/08-summary/2a_columns_all.R
@@ -24,7 +24,6 @@ column_info <- tribble( # tribble_start
   "score_overall",          "scaling_pred_scoretime_overall",               "Scalability",              "bar",        "scaling",        list(width = 4, legend = FALSE),
   "score_overall",          "stability_overall_overall",                    "Stability",                "bar",        "stability",      list(width = 4, legend = FALSE),
   "score_overall",          "qc_overall_overall",                           "Meta-quality",             "bar",        "qc",             list(width = 4, legend = FALSE),
-  "score_overall",          "control_label",                                "",                         "text",       NA,               list(overlay = TRUE),
   "benchmark_metric",       "benchmark_overall_norm_correlation",           "Ordering",                 "funkyrect",  "benchmark",      lst(),
   "benchmark_metric",       "benchmark_overall_norm_him",                   "Topology",                 "funkyrect",  "benchmark",      lst(),
   "benchmark_metric",       "benchmark_overall_norm_featureimp_wcor",       "Features",                 "funkyrect",  "benchmark",      lst(),
@@ -78,8 +77,7 @@ column_info <- tribble( # tribble_start
   "qc_category",            "qc_cat_code_assurance",                        "Code assurance",           "funkyrect",  "qc",             lst(),
   "qc_category",            "qc_cat_code_quality",                          "Code quality",             "funkyrect",  "qc",             lst(),
   "qc_category",            "qc_cat_documentation",                         "Documentation",            "funkyrect",  "qc",             lst(),
-  "qc_category",            "qc_cat_paper",                                 "Paper",                    "funkyrect",  "qc",             lst(),
-  "qc_category",            "control_label",                                "",                         "text",       NA,               list(overlay = TRUE, width = -6)
+  "qc_category",            "qc_cat_paper",                                 "Paper",                    "funkyrect",  "qc",             lst()
 ) # tribble_end
 
 
