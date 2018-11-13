fa9a33b9f5afa8984c01cd7fee6aa8129b529471
Robrecht Cannoodt <rcannood@gmail.com>
2018-11-13 13:13:19 +0100

    remove spaces before citations

--- scripts/varia/render/render_manuscript.R
+++ scripts/varia/render/render_manuscript.R
@@ -11,6 +11,7 @@ fs::dir_create(output_directory)
 httr::set_config(httr::config(http_version = 0)) # avoid http2 framing layer bug
 drive <- googledrive::drive_download(googledrive::as_id("1je6AaelApu2xcSNbYlvcuTzUeUJBOpTUPHz0L9Houfw"), type="text/plain", overwrite=TRUE, path = tempfile())
 system(pritt("sed -i '1s/^.//' {drive$local_path}")) # remove first character, because this is some strange unicode character added by google
+system(pritt("sed -i 's/ *\\(\\[@[^\\]]*\\]\\)/\\1/' {drive$local_path}")) # remote spaces before citations
 system(pritt("cat {drive$local_path} > manuscript/paper.Rmd"))
 
 # render main manuscript
