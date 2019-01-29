62acb266b8438716caad7d66579625cb626134ad
Robrecht Cannoodt <rcannood@gmail.com>
2019-01-29 15:28:24 +0100

    add synthetic dataset rmd for supp notes

--- scripts/12-manuscript/render_supplementary_notes.R
+++ scripts/12-manuscript/render_supplementary_notes.R
@@ -4,7 +4,8 @@ library(dynbenchmark)
 library(tidyverse)
 
 supplementary_note_paths <- c(
-  result_file("README.Rmd", experiment = "02-metrics")
+  result_file("README.Rmd", experiment = "02-metrics"),
+  result_file("README.Rmd", experiment = "01-datasets/02-synthetic")
 )
 
 walk(supplementary_note_paths, function(supplementary_note_path) {
