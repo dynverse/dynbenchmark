d72fae37cf2f44f384614decbbc1d6b8621cbe08
Robrecht Cannoodt <rcannood@gmail.com>
2019-01-30 11:50:13 +0100

    copy supp notes after build

--- scripts/12-manuscript/render_supplementary_notes.R
+++ scripts/12-manuscript/render_supplementary_notes.R
@@ -3,16 +3,23 @@
 library(dynbenchmark)
 library(tidyverse)
 
-supplementary_note_paths <- c(
-  result_file("README.Rmd", experiment = "02-metrics"),
-  result_file("README.Rmd", experiment = "01-datasets/02-synthetic")
-)
+supp_notes <- tribble(
+  ~rmd_path, ~final_path,
+  result_file("README.Rmd", "02-metrics"), result_file("Supplementary Note 1.pdf", "12-benchmark"),
+  result_file("README.Rmd", "01-datasets/02-synthetic"), result_file("Supplementary Note 2.pdf", "12-benchmark")
+) %>%
+  mutate(pdf_path = str_replace_all(rmd_path, "Rmd$", "pdf"))
 
-walk(supplementary_note_paths, function(supplementary_note_path) {
+
+walk(supp_notes$rmd_path, function(supplementary_note_path) {
   print(paste0("Processing ", supplementary_note_path))
   rmarkdown::render(
     supplementary_note_path,
     output_format = dynbenchmark::pdf_supplementary_note(),
     quiet = TRUE
   )
+})
+
+pwalk(supp_notes, function(rmd_path, final_path, pdf_path) {
+  file.copy(pdf_path, final_path, overwrite = TRUE)
 })
